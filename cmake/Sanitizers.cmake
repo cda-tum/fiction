193e054e49daf9a9ebe6381e63eb9e9ca2326f43
Marcel Walter <m_walter@uni-bremen.de>
2021-04-11 15:20:18 +0200

    Test setup for coverage reporting with Codecov

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,12 +1,6 @@
 function(enable_sanitizers project_name)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
-
-    if(ENABLE_COVERAGE)
-      target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
-      target_link_libraries(${project_name} INTERFACE --coverage)
-    endif()
 
     set(SANITIZERS "")
 
