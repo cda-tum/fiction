190c58797abb6b5e894eac59a238125cb02c896b
Marcel Walter <m_walter@uni-bremen.de>
2021-04-14 20:47:37 +0200

    Primary inputs and outputs for tile_based_layouts

--- src/topo/tile_based_layout.hpp
+++ src/topo/tile_based_layout.hpp
@@ -5,7 +5,14 @@
 #ifndef FICTION_TILE_BASED_LAYOUT_HPP
 #define FICTION_TILE_BASED_LAYOUT_HPP
 
+#include <map>
+#include <ostream>
+#include <set>
+#include <vector>
+
+#include <fmt/format.h>
 #include <mockturtle/traits.hpp>
+#include <mockturtle/networks/detail/foreach.hpp>
 
 namespace fiction
 {
@@ -15,6 +22,8 @@ struct tile
     uint64_t z : 2;
     uint64_t y : 31;
     uint64_t x : 31;
+
+    constexpr tile() : z{static_cast<uint64_t>(0ul)}, y{static_cast<uint64_t>(0ul)}, x{static_cast<uint64_t>(0ul)} {}
 
     template <class X, class Y, class Z>
     constexpr tile(X x, Y y, Z z) :
@@ -31,20 +40,47 @@ struct tile
     {
         return x == other.x && y == other.y && z == other.z;
     }
-};
 
-using aspect_ratio = tile;
-
-template <class Ntk>
-struct layout_node : public Ntk::node
-{
-    tile t{0, 0, 0};
-
-    bool operator==(layout_node const& other) const
+    constexpr bool operator!=(const tile& other) const
     {
-        (t.x == other.t.x) && (t.y == other.t.y) && (t.z == other.t.z) && (this->Ntk::node == other.Ntk::node);
+        return !(*this == other);
+    }
+
+    constexpr bool operator<(const tile& other) const
+    {
+        if (z < other.z) return true;
+
+        if (z == other.z)
+        {
+            if (y < other.y) return true;
+
+            if (y == other.y) { return x < other.x; }
+        }
+
+        return false;
+    }
+
+    constexpr bool operator>(const tile& other) const
+    {
+        return other < *this;
+    }
+
+    constexpr bool operator<=(const tile& other) const
+    {
+        return !(*this > other);
+    }
+
+    constexpr bool operator>=(const tile& other) const
+    {
+        return !(*this < other);
     }
 };
+
+std::ostream& operator<<(std::ostream& os, const tile& t)
+{
+    os << fmt::format("({},{},{})", t.x, t.y, t.z);
+    return os;
+}
 
 template <class Ntk>
 class tile_based_layout : public Ntk
@@ -52,26 +88,88 @@ class tile_based_layout : public Ntk
   public:
 #pragma region Types and constructors
 
-    using base_type = tile_based_layout<Ntk>;
-    using storage   = typename Ntk::storage;
-    using node      = layout_node<Ntk>;
-    using signal    = typename Ntk::signal;
+    using base_type    = tile_based_layout<Ntk>;
+    using storage      = typename Ntk::storage;
+    using node         = typename Ntk::node;
+    using signal       = typename Ntk::signal;
+    using aspect_ratio = tile;
 
-    explicit tile_based_layout(const aspect_ratio& aspect_ratio) : Ntk(), aspect_ratio{aspect_ratio}
+    explicit tile_based_layout(const aspect_ratio& aspect_ratio) :
+            Ntk(),
+            ratio{aspect_ratio},
+            l_storage{std::make_shared<layout_storage>()}
     {
         static_assert(mockturtle::is_network_type_v<Ntk>, "Ntk is not a network type");
     }
-
-    void f() {}
 
 #pragma endregion
 
 #pragma region Primary I / O and constants
 
+    signal create_pi(const tile& t, std::string const& name = std::string())
+    {
+        static_assert(mockturtle::has_create_pi_v<Ntk>, "Ntk does not implement the create_pi function");
+
+        auto s = Ntk::create_pi(name);
+        assign_node(t, Ntk::get_node(s));
+        l_storage->inputs.emplace_back(t);
+
+        return s;
+    }
+
+    void create_po(const tile& t, signal const& f, std::string const& name = std::string())
+    {
+        static_assert(mockturtle::has_create_po_v<Ntk>, "Ntk does not implement the create_po function");
+
+        Ntk::create_po(f, name);
+        assign_signal(t, f);
+        l_storage->outputs.emplace_back(t);
+    }
+
+#pragma endregion
+
+
+#pragma region tile iterators
+
+    template <typename Fn>
+    void foreach_pi_tile(Fn&& fn) const
+    {
+        mockturtle::detail::foreach_element(l_storage->inputs.cbegin(), l_storage->inputs.cend(), fn);
+    }
+
+    template <typename Fn>
+    void foreach_po_tile(Fn&& fn) const
+    {
+        mockturtle::detail::foreach_element(l_storage->outputs.cbegin(), l_storage->outputs.cend(), fn);
+    }
+
 #pragma endregion
 
   private:
-    fiction::aspect_ratio aspect_ratio;
+    aspect_ratio ratio;
+
+    struct layout_storage
+    {
+        std::vector<tile> inputs{}, outputs{};
+
+        std::map<tile, node> tile_node_map{};
+        std::map<node, tile> node_tile_map{};
+
+        std::map<tile, std::set<signal>> tile_signal_map{};
+    };
+
+    std::shared_ptr<layout_storage> l_storage;
+
+    void assign_node(const tile& t, const node& n)
+    {
+        l_storage->tile_node_map[t] = n;
+        l_storage->node_tile_map[n] = t;
+    }
+
+    void assign_signal(const tile& t, const signal& s)
+    {
+        l_storage->tile_signal_map[t].insert(s);
+    }
 };
 
 }  // namespace fiction
--- test/tests/tile_based_layout.cpp
+++ test/tests/tile_based_layout.cpp
@@ -4,49 +4,72 @@
 
 #include "catch.hpp"
 
-#include <tile_based_layout.hpp>
-
+#include <iostream>
 #include <mockturtle/networks/aig.hpp>
 #include <mockturtle/networks/mig.hpp>
 #include <mockturtle/traits.hpp>
+#include <tile_based_layout.hpp>
 
 using namespace fiction;
 
 TEST_CASE("Tiles", "[tile-based")
 {
+    auto t0 = tile{0, 0, 0};
+
+    CHECK(t0 == tile{0, 0});
+
     auto t1 = tile{1, 2, 0};
     auto t2 = tile{1, 2};
 
+    CHECK(t0 < t1);
+    CHECK(t1 > t0);
+    CHECK(t1 >= t0);
+    CHECK(t0 <= t1);
+    CHECK(t1 == t2);
+    CHECK(t2 == t1);
+
+    t1.z += static_cast<uint64_t>(4ul);
+
     CHECK(t1 == t2);
 
-    t1.z += 4;
+    t1.y += static_cast<uint64_t>(2147483648ul);
+
+    CHECK(t1 == t2);
+
+    t1.x += static_cast<uint64_t>(2147483648ul);
 
     CHECK(t1 == t2);
 
     t1.x++;
 
-    CHECK(!(t1 == t2));
+    CHECK(t1 != t2);
+    CHECK(t1 > t2);
+    CHECK(t1 >= t2);
+    CHECK(t2 < t1);
+    CHECK(t2 <= t1);
 }
 
-TEST_CASE("AIG layout creation and constant usage", "[tile-based]")
+template<class Ntk>
+static void creation_and_constants()
 {
-    // adapted from mockturtle/test/networks/aig.cpp
+    // adapted from mockturtle/test/networks/*.cpp
 
-    using aig_layout = tile_based_layout<mockturtle::aig_network>;
-    aig_layout layout{aspect_ratio{2, 2, 1}};
+    CHECK(mockturtle::is_network_type_v<Ntk>);
+
+    Ntk layout{typename Ntk::aspect_ratio{2, 2, 1}};
 
     CHECK(layout.size() == 1);
-    CHECK(mockturtle::has_get_constant_v<aig_layout>);
-//    CHECK(mockturtle::has_is_constant_v<aig_layout>);
-    CHECK(mockturtle::has_get_node_v<aig_layout>);
-    CHECK(mockturtle::has_is_complemented_v<aig_layout>);
+    CHECK(mockturtle::has_get_constant_v<Ntk>);
+    CHECK(mockturtle::has_is_constant_v<Ntk>);
+    CHECK(mockturtle::has_get_node_v<Ntk>);
+    CHECK(mockturtle::has_is_complemented_v<Ntk>);
 
     const auto c0 = layout.get_constant(false);
     CHECK(layout.is_constant(layout.get_node(c0)));
     CHECK(!layout.is_pi(layout.get_node(c0)));
 
     CHECK(layout.size() == 1);
-    CHECK(std::is_same_v<std::decay_t<decltype(c0)>, aig_layout::signal>);
+    CHECK(std::is_same_v<std::decay_t<decltype(c0)>, typename Ntk::signal>);
     CHECK(layout.get_node(c0) == 0);
     CHECK(!layout.is_complemented(c0));
 
@@ -65,39 +88,147 @@ TEST_CASE("AIG layout creation and constant usage", "[tile-based]")
     CHECK(c0 == +c0);
 }
 
-TEST_CASE("MIG layout creation and constant usage", "[tile-based]")
+TEST_CASE("Layout creation and usage of constants", "[tile-based]")
 {
-    // adapted from mockturtle/test/networks/mig.cpp
+    SECTION("AIG layouts")
+    {
+        using aig_layout = tile_based_layout<mockturtle::aig_network>;
 
-    using mig_layout = tile_based_layout<mockturtle::mig_network>;
-    mig_layout layout{aspect_ratio{2, 2, 1}};
+        creation_and_constants<aig_layout>();
+    }
 
-    CHECK(layout.size() == 1);
-    CHECK(mockturtle::has_get_constant_v<mig_layout>);
-//    CHECK(mockturtle::has_is_constant_v<mig_layout>);
-    CHECK(mockturtle::has_get_node_v<mig_layout>);
-    CHECK(mockturtle::has_is_complemented_v<mig_layout>);
+    SECTION("MIG layouts")
+    {
+        using mig_layout = tile_based_layout<mockturtle::mig_network>;
+
+        creation_and_constants<mig_layout>();
+    }
+}
+
+template <class Ntk>
+static void creation_and_primary_inputs()
+{
+    // adapted from mockturtle/test/networks/*.cpp
+
+    Ntk layout{typename Ntk::aspect_ratio{2, 2, 1}};
+
+    auto a = layout.create_pi({0, 0});
+
+    CHECK(layout.size() == 2);
+    CHECK(layout.num_pis() == 1);
+    CHECK(layout.num_gates() == 0);
+
+    CHECK(std::is_same_v<std::decay_t<decltype(a)>, typename Ntk::signal>);
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 0);
+
+    a = !a;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 1);
+
+    a = +a;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 0);
+
+    a = +a;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 0);
+
+    a = -a;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 1);
+
+    a = -a;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 1);
+
+    a = a ^ true;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 0);
+
+    a = a ^ true;
+
+    CHECK(a.index == 1);
+    CHECK(a.complement == 1);
+}
+
+TEST_CASE("Layout creation and usage of primary inputs", "[tile-based]")
+{
+    SECTION("AIG layouts")
+    {
+        using aig_layout = tile_based_layout<mockturtle::aig_network>;
+
+        creation_and_primary_inputs<aig_layout>();
+    }
+
+    SECTION("MIG layouts")
+    {
+        using mig_layout = tile_based_layout<mockturtle::mig_network>;
+
+        creation_and_primary_inputs<mig_layout>();
+    }
+}
+
+template <class Ntk>
+static void creation_and_primary_outputs()
+{
+    // adapted from mockturtle/test/networks/*.cpp
+
+    Ntk layout{typename Ntk::aspect_ratio{2, 2, 1}};
 
     const auto c0 = layout.get_constant(false);
-    CHECK(layout.is_constant(layout.get_node(c0)));
-    CHECK(!layout.is_pi(layout.get_node(c0)));
+    const auto x1 = layout.create_pi({0, 0});
 
-    CHECK(layout.size() == 1);
-    CHECK(std::is_same_v<std::decay_t<decltype(c0)>, mig_layout::signal>);
-    CHECK(layout.get_node(c0) == 0);
-    CHECK(!layout.is_complemented(c0));
+    CHECK(layout.size() == 2);
+    CHECK(layout.num_pis() == 1);
+    CHECK(layout.num_pos() == 0);
 
-    const auto c1 = layout.get_constant(true);
+    layout.create_po({1, 0}, c0);
+    layout.create_po({0, 1}, x1);
+    layout.create_po({1, 1}, !x1);
 
-    CHECK(layout.get_node(c1) == 0);
-    CHECK(layout.is_complemented(c1));
+    CHECK(layout.size() == 2);
+    CHECK(layout.num_pos() == 3);
 
-    CHECK(c0 != c1);
-    CHECK(c0 == !c1);
-    CHECK((!c0) == c1);
-    CHECK((!c0) != !c1);
-    CHECK(-c0 == c1);
-    CHECK(-c1 == c1);
-    CHECK(c0 == +c1);
-    CHECK(c0 == +c0);
+    layout.foreach_po([&](auto s, auto i) {
+        switch (i)
+            {
+                case 0: CHECK(s == c0); break;
+                case 1: CHECK(s == x1); break;
+                case 2: CHECK(s == !x1); break;
+            }
+    });
+
+    layout.foreach_po_tile([&](auto t, auto i) {
+        switch (i)
+            {
+                case 0: CHECK(t == tile{1, 0}); break;
+                case 1: CHECK(t == tile{0, 1}); break;
+                case 2: CHECK(t == tile{1, 1}); break;
+            }
+    });
+}
+
+TEST_CASE("Layout creation and usage of primary outputs", "[tile-based]")
+{
+    SECTION("AIG layouts")
+    {
+        using aig_layout = tile_based_layout<mockturtle::aig_network>;
+
+        creation_and_primary_outputs<aig_layout>();
+    }
+
+    SECTION("MIG layouts")
+    {
+        using mig_layout = tile_based_layout<mockturtle::mig_network>;
+
+        creation_and_primary_outputs<mig_layout>();
+    }
 }
