cc74517947d2557884832fedd1d002e8615606e3
Marcel Walter <m_walter@uni-bremen.de>
2021-05-14 10:33:02 +0200

    Adjusted CMake for --coverage include in project_options

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -2,6 +2,12 @@ function(enable_sanitizers project_name)
 
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
 
+    option(ENABLE_COVERAGE "Enable coverage reporting for gcc/clang" FALSE)
+    if (ENABLE_COVERAGE)
+        target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
+        target_link_libraries(${project_name} INTERFACE --coverage)
+    endif ()
+
     set(SANITIZERS "")
 
     option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" FALSE)
