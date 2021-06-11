dfb997f3357dd3b09f75ded0643bd29f91937335
Marcel Walter <m_walter@uni-bremen.de>
2021-06-11 15:52:20 +0200

    Fixed DOT description in technology library

--- include/fiction/technology/technology_library.hpp
+++ include/fiction/technology/technology_library.hpp
@@ -14,17 +14,17 @@ constexpr const char* GATE_AND2 = "GATE   and2    1 O=(ab);         PIN * NONINV
 constexpr const char* GATE_OR2  = "GATE   or2     1 O={ab};         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n";
 constexpr const char* GATE_XOR2 = "GATE   xor2    1 O=[ab];         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n";
 
-constexpr const char* GATE_MAJ3 = "GATE   maj3    1 O=<abc>;        PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   maj3    1 O=(ab);         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
+constexpr const char* GATE_MAJ3 = "GATE   maj3    1 O=<abc>;        PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                  "GATE   maj3    1 O=(ab);         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
                                   "GATE   maj3    1 O={ab};         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n";
 
-constexpr const char* GATE_DOT  = "GATE   dot     1 O=[a{c(ab)}];   PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=c;            PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=(!b!c);       PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=[ac];         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=(!ac);        PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=(a!b);        PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"\
-                                  "GATE   dot     1 O=!a;           PIN * INV    1 999 1.0 1.0 1.0 1.0\n";
+constexpr const char* GATE_DOT = "GATE   dot     1 O=[a{c(ab)}];    PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=a;             PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=(!a!b);        PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=[ab];          PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=(!ab);         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=(a!b);         PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+                                 "GATE   dot     1 O=!a;            PIN * INV    1 999 1.0 1.0 1.0 1.0\n";
 
 constexpr const char* GATE_BUF  = "GATE   buf     1 O=a;            PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n";
 constexpr const char* GATE_ZERO = "GATE   zero    0 O=0;\n";
