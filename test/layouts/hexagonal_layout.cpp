13ea2b11600595058f15d8cf5c80c79bcf9b380e
Marcel Walter <marcelwalter.mw@gmail.com>
2021-09-15 18:31:58 +0200

    Created hex files

---
+++ test/layouts/hexagonal_layout.cpp
@@ -0,0 +1,18 @@
+//
+// Created by marcel on 15.09.21.
+//
+
+#include "catch.hpp"
+
+#include <fiction/layouts/hexagonal_layout.hpp>
+#include <fiction/traits.hpp>
+
+using namespace fiction;
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wuseless-cast"
+#pragma GCC diagnostic ignored "-Wconversion"
+#endif
+
+TEST_CASE("Traits", "[hexagonal]") {}
