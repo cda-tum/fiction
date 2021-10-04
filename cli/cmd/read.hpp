422fa33992471ad3fd2a4503aa53079f3753cbd1
Marcel Walter <marcelwalter.mw@gmail.com>
2021-10-04 19:44:23 +0200

    read_fqca_layout in command 'read'

--- cli/cmd/read.hpp
+++ cli/cmd/read.hpp
@@ -6,10 +6,13 @@
 #define FICTION_READ_HPP
 
 #include <fiction/io/network_reader.hpp>
+#include <fiction/io/read_fqca_layout.hpp>
 #include <fiction/types.hpp>
 
 #include <alice/alice.hpp>
+#include <fmt/format.h>
 
+#include <memory>
 #include <string>
 
 namespace alice
@@ -18,6 +21,8 @@ namespace alice
  * Shorthand for all read commands. Chooses the proper function by the file ending.
  *
  * Currently parses Verilog, AIGER, and BLIF using the lorina parsers.
+ *
+ * Parses FQCA via a custom reader function.
  *
  * For more information see: https://github.com/hriener/lorina
  */
@@ -30,14 +35,17 @@ class read_command : public command
      * @param e alice::environment that specifies stores etc.
      */
     explicit read_command(const environment::ptr& e) :
-            command(e, "Reads one Verilog or AIGER file or a directory of those and creates logic network objects "
-                       "which will be put into the respective store. In a directory, only files with extension "
-                       "'.v' and '.aig' are considered.")
+            command(e, "Reads a file or a directory of files and creates logic network or FCN layout objects "
+                       "which will be put into the respective store. Current supported file types are:\n"
+                       "Logic networks: Verilog, AIGER, BLIF.\n"
+                       "Cell-level layouts: FQCA.\n"
+                       "In a directory, only files with extension '.v', '.aig', '.blif' are considered.")
     {
         add_option("filename", filename, "Filename or directory")->required();
-        add_flag("--aig,-a", "Parse networks as AIG");
-        add_flag("--mig,-m", "Parse networks as MIG");
-        add_flag("--top,-t", "Parse networks as Topology network");
+        add_flag("--aig,-a", "Parse file as AIG");
+        add_flag("--mig,-m", "Parse file as MIG");
+        add_flag("--top,-t", "Parse file as Topology network");
+        add_flag("--qca,-q", "Parse file as QCA cell-level layout");
         add_flag("--sort,-s", sort, "Sort networks in given directory by vertex count prior to storing them");
     }
 
@@ -52,9 +60,9 @@ class read_command : public command
             for (const auto& ln : reader.get_networks(sort)) store<fiction::logic_network_t>().extend() = ln;
         };
 
-        if (!is_set("aig") && !is_set("mig") && !is_set("top"))
+        if (!is_set("aig") && !is_set("mig") && !is_set("top") && !is_set("qca"))
         {
-            env->out() << "[e] at least one network type must be specified" << std::endl;
+            env->out() << "[e] at least one network or layout type must be specified" << std::endl;
         }
         else
         {
@@ -78,10 +86,35 @@ class read_command : public command
 
                     store_nets(reader);
                 }
+                if (is_set("qca"))
+                {
+                    try
+                    {
+                        store<fiction::cell_layout_t>().extend() = std::make_shared<fiction::qca_cell_clk_lyt>(
+                            fiction::read_fqca_layout<fiction::qca_cell_clk_lyt>(filename));
+                    }
+                    catch (const fiction::unsupported_character_exception& e)
+                    {
+                        env->out() << fmt::format("character '{}' is not supported as a cell designator", e.which())
+                                   << std::endl;
+                    }
+                    catch (const fiction::undefined_cell_label_exception& e)
+                    {
+                        env->out()
+                            << fmt::format(
+                                   "cell label '{}' was used in the cell definition section but never defined above",
+                                   e.which())
+                            << std::endl;
+                    }
+                    catch (const fiction::unrecognized_cell_definition_exception& e)
+                    {
+                        env->out() << fmt::format("unsupported cell definition in line {}", e.where()) << std::endl;
+                    }
+                }
             }
             catch (...)
             {
-                env->out() << "[e] no networks were read" << std::endl;
+                env->out() << "[e] no networks or layouts were read" << std::endl;
             }
         }
 
