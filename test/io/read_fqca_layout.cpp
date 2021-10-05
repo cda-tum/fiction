7ce6fc865ef41c9fd4f52ff10808444819d43b7d
Marcel Walter <marcelwalter.mw@gmail.com>
2021-10-05 10:39:33 +0200

    Exception tests

--- test/io/read_fqca_layout.cpp
+++ test/io/read_fqca_layout.cpp
@@ -118,3 +118,120 @@ TEST_CASE("Read single-layer AND gate", "[fqca]")
     CHECK(layout.get_cell_name({2, 4}) == "b");
     CHECK(layout.get_cell_name({4, 2}) == "f");
 }
+
+TEST_CASE("Exceptions", "[fqca]")
+{
+    using qca_layout = cell_level_layout<qca_technology, clocked_layout<cartesian_layout<coord_t>>>;
+
+    SECTION("unsupported_character_exception")
+    {
+        static constexpr const char* fqca_layout =
+            "[ AND ]\n"
+            "\n"
+            "= = = = = \n"
+            "    -     \n"
+            "    0     \n"
+            "_ 0 0 0 b \n"  // cell (0,2) uses an underscore as id, which is not allowed
+            "    0     \n"
+            "    c     \n"
+            "= = = = = \n"
+            "\n"
+            "\n"
+            "$\n"
+            "\n"
+            "_:\n"
+            "- input\n"
+            "- label = \"a\"\n"
+            "- clock = 0\n"
+            "\n"
+            "b:\n"
+            "- output\n"
+            "- label = \"f\"\n"
+            "- clock = 1\n"
+            "\n"
+            "c:\n"
+            "- input\n"
+            "- label = \"b\"\n"
+            "- clock = 0\n"
+            "\n";
+
+        std::istringstream layout_stream{fqca_layout};
+
+        CHECK_THROWS_AS(read_fqca_layout<qca_layout>(layout_stream), unsupported_character_exception);
+    }
+    SECTION("undefined_cell_label_exception")
+    {
+        static constexpr const char* fqca_layout = "[ AND ]\n"
+                                                   "\n"
+                                                   "= = = = = \n"
+                                                   "    -     \n"
+                                                   "    0     \n"
+                                                   "a 0 0 0 b \n"
+                                                   "    0     \n"
+                                                   "    c     \n"
+                                                   "= = = = = \n"
+                                                   "\n"
+                                                   "\n"
+                                                   "$\n"
+                                                   "\n"
+                                                   "a:\n"
+                                                   "- input\n"
+                                                   "- label = \"a\"\n"
+                                                   "- clock = 0\n"
+                                                   "\n"
+                                                   "b:\n"
+                                                   "- output\n"
+                                                   "- label = \"f\"\n"
+                                                   "- clock = 1\n"
+                                                   "\n"
+                                                   "c:\n"
+                                                   "- input\n"
+                                                   "- label = \"b\"\n"
+                                                   "- clock = 0\n"
+                                                   "\n"
+                                                   "d:\n"  // id 'd' does not appear in the layout definition
+                                                   "- label = \"unused\"\n"
+                                                   "- clock = 1\n";
+
+        std::istringstream layout_stream{fqca_layout};
+
+        CHECK_THROWS_AS(read_fqca_layout<qca_layout>(layout_stream), undefined_cell_label_exception);
+    }
+    SECTION("unrecognized_cell_definition_exception")
+    {
+        static constexpr const char* fqca_layout =
+            "[ AND ]\n"
+            "\n"
+            "= = = = = \n"
+            "    -     \n"
+            "    0     \n"
+            "a 0 0 0 b \n"
+            "    0     \n"
+            "    c     \n"
+            "= = = = = \n"
+            "\n"
+            "\n"
+            "$\n"
+            "\n"
+            "a:\n"
+            "- input\n"
+            "- label = \"a\"\n"
+            "- clock = 0\n"
+            "\n"
+            "b:\n"
+            "- output\n"
+            "- label = \"f\"\n"
+            "- clock = 1\n"
+            "\n"
+            "c:\n"
+            "- input\n"
+            "- label = \"b\"\n"
+            "- clock = 0\n"
+            "- unrecognized = 3.14\n"  // cell 'c' uses an unrecognized definition
+            "\n";
+
+        std::istringstream layout_stream{fqca_layout};
+
+        CHECK_THROWS_AS(read_fqca_layout<qca_layout>(layout_stream), unrecognized_cell_definition_exception);
+    }
+}
