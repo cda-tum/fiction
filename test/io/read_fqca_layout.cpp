38890dc7e8f01cf15e051b48006a212d22267190
Marcel Walter <marcelwalter.mw@gmail.com>
2021-10-07 14:59:52 +0200

    Test case for reading multi-layered layouts from fqca files

--- test/io/read_fqca_layout.cpp
+++ test/io/read_fqca_layout.cpp
@@ -143,6 +143,113 @@ TEST_CASE("Read single-layer AND gate", "[fqca]")
     }
 }
 
+TEST_CASE("Read multi-layer wire crossing", "[fqca]")
+{
+    static constexpr const char* fqca_layout = "[ Wire crossing ]\n"
+                                               "\n"
+                                               "= = = = = \n"
+                                               "    a     \n"
+                                               "          \n"
+                                               "b 0 0 0 c \n"
+                                               "          \n"
+                                               "    d     \n"
+                                               "= = = = = \n"
+                                               "    0     \n"
+                                               "          \n"
+                                               "          \n"
+                                               "          \n"
+                                               "    0     \n"
+                                               "= = = = = \n"
+                                               "    0     \n"
+                                               "    0     \n"
+                                               "    0     \n"
+                                               "    0     \n"
+                                               "    0     \n"
+                                               "= = = = = \n"
+                                               "\n"
+                                               "\n"
+                                               "$\n"
+                                               "\n"
+                                               "a:\n"
+                                               "- input\n"
+                                               "- label = \"a\"\n"
+                                               "- clock = 0\n"
+                                               "\n"
+                                               "b:\n"
+                                               "- input\n"
+                                               "- label = \"b\"\n"
+                                               "- clock = 0\n"
+                                               "\n"
+                                               "c:\n"
+                                               "- output\n"
+                                               "- label = \"b'\"\n"
+                                               "- clock = 0\n"
+                                               "\n"
+                                               "d:\n"
+                                               "- output\n"
+                                               "- label = \"a'\"\n"
+                                               "- clock = 0\n"
+                                               "\n";
+
+    std::istringstream layout_stream{fqca_layout};
+
+    using qca_layout = cell_level_layout<qca_technology, clocked_layout<cartesian_layout<cube::coord_t>>>;
+
+    const auto layout = read_fqca_layout<qca_layout>(layout_stream);
+
+    CHECK(layout.x() == 4);
+    CHECK(layout.y() == 4);
+    CHECK(layout.z() == 2);
+
+    CHECK(layout.get_cell_name({2, 0, 0}) == "a");
+    CHECK(layout.get_cell_name({0, 2, 0}) == "b");
+    CHECK(layout.get_cell_name({4, 2, 0}) == "b'");
+    CHECK(layout.get_cell_name({2, 4, 0}) == "a'");
+
+    CHECK(layout.num_cells() == 14);
+    CHECK(layout.num_pis() == 2);
+    CHECK(layout.num_pos() == 2);
+
+    CHECK(layout.is_pi({2, 0, 0}));
+    CHECK(layout.is_pi({0, 2, 0}));
+    CHECK(layout.is_po({4, 2, 0}));
+    CHECK(layout.is_po({2, 4, 0}));
+
+    CHECK(layout.get_cell_type({0, 2, 0}) == qca_technology::cell_type::INPUT);
+    CHECK(layout.get_cell_type({2, 0, 0}) == qca_technology::cell_type::INPUT);
+    CHECK(layout.get_cell_type({2, 4, 0}) == qca_technology::cell_type::OUTPUT);
+    CHECK(layout.get_cell_type({4, 2, 0}) == qca_technology::cell_type::OUTPUT);
+    CHECK(layout.get_cell_type({1, 2, 0}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 2, 0}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({3, 2, 0}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({1, 2, 0}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({3, 2, 0}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 0, 1}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 0, 2}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 1, 2}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 2, 2}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 3, 2}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 4, 2}) == qca_technology::cell_type::NORMAL);
+    CHECK(layout.get_cell_type({2, 4, 1}) == qca_technology::cell_type::NORMAL);
+
+    CHECK(layout.get_cell_mode({0, 2, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({1, 2, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({2, 2, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({3, 2, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({4, 2, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({2, 0, 0}) == qca_technology::cell_mode::NORMAL);
+    CHECK(layout.get_cell_mode({2, 4, 0}) == qca_technology::cell_mode::NORMAL);
+
+    CHECK(layout.get_cell_mode({2, 0, 1}) == qca_technology::cell_mode::VERTICAL);
+    CHECK(layout.get_cell_mode({2, 4, 1}) == qca_technology::cell_mode::VERTICAL);
+
+    CHECK(layout.get_cell_mode({2, 0, 2}) == qca_technology::cell_mode::CROSSOVER);
+    CHECK(layout.get_cell_mode({2, 1, 2}) == qca_technology::cell_mode::CROSSOVER);
+    CHECK(layout.get_cell_mode({2, 2, 2}) == qca_technology::cell_mode::CROSSOVER);
+    CHECK(layout.get_cell_mode({2, 3, 2}) == qca_technology::cell_mode::CROSSOVER);
+    CHECK(layout.get_cell_mode({2, 4, 2}) == qca_technology::cell_mode::CROSSOVER);
+}
+
 TEST_CASE("Parsing of unsupported features", "[fqca]")
 {
     static constexpr const char* fqca_layout = "[ AND ]\n"
