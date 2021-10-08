994fd604e6c0d343b2fe2a5793fe38a12ea6f692
Marcel Walter <marcelwalter.mw@gmail.com>
2021-10-08 14:20:04 +0200

    First stub of the exact port

--- test/algorithms/exact.cpp
+++ test/algorithms/exact.cpp
@@ -2,7 +2,7 @@
 // Created by marcel on 08.10.21.
 //
 
-#if (MUGEN)
+#if (FICTION_Z3_SOLVER)
 
 #include "catch.hpp"
 #include "utils/blueprints/network_blueprints.hpp"
@@ -17,6 +17,7 @@
 #include <fiction/layouts/gate_level_layout.hpp>
 #include <fiction/layouts/tile_based_layout.hpp>
 #include <fiction/technology/qca_one_library.hpp>
+#include <fiction/traits.hpp>
 
 #include <mockturtle/networks/aig.hpp>
 #include <mockturtle/networks/mig.hpp>
@@ -28,50 +29,34 @@
 
 using namespace fiction;
 
-std::vector<exact_physical_design_params> configurations() noexcept
+template <typename Lyt>
+std::vector<exact_physical_design_params<Lyt>> configurations() noexcept
 {
-    exact_physical_design_params twoddwave_config{};
+    exact_physical_design_params<Lyt> twoddwave_config{};
 
-    twoddwave_config.scheme       = std::make_shared<clocking_scheme<coord_t>>(twoddwave_4_clocking);
-    twoddwave_config.enable_and   = true;
-    twoddwave_config.enable_not   = true;
-    twoddwave_config.enable_or    = true;
-    twoddwave_config.enable_wires = true;
+    twoddwave_config.scheme       = std::make_shared<clocking_scheme<coordinate<Lyt>>>(twoddwave_4_clocking<Lyt>());
     twoddwave_config.crossings    = true;
 
-    exact_physical_design_params use_config{};
+    exact_physical_design_params<Lyt> use_config{};
 
-    use_config.scheme       = std::make_shared<clocking_scheme<coord_t>>(use_4_clocking);
-    use_config.enable_and   = true;
-    use_config.enable_not   = true;
-    use_config.enable_or    = true;
-    use_config.enable_wires = true;
+    use_config.scheme       = std::make_shared<clocking_scheme<coordinate<Lyt>>>(use_4_clocking<Lyt>());
     use_config.crossings    = true;
 
-    exact_physical_design_params res_config{};
+    exact_physical_design_params<Lyt> res_config{};
 
-    res_config.scheme       = std::make_shared<clocking_scheme<coord_t>>(res_4_clocking);
-    res_config.enable_and   = true;
-    res_config.enable_not   = true;
-    res_config.enable_or    = true;
-    res_config.enable_maj   = true;
-    res_config.enable_wires = true;
+    res_config.scheme       = std::make_shared<clocking_scheme<coordinate<Lyt>>>(res_4_clocking<Lyt>());
     res_config.crossings    = true;
 
-    exact_physical_design_params async_config{};
+    exact_physical_design_params<Lyt> async_config{};
 
-    async_config.scheme       = std::make_shared<clocking_scheme<coord_t>>(twoddwave_4_clocking);
-    async_config.enable_and   = true;
-    async_config.enable_not   = true;
-    async_config.enable_or    = true;
-    async_config.enable_wires = true;
+    async_config.scheme       = std::make_shared<clocking_scheme<coordinate<Lyt>>>(twoddwave_4_clocking<Lyt>());
     async_config.crossings    = true;
     async_config.num_threads  = 2ul;
 
     return {{twoddwave_config, use_config, res_config, async_config}};
 }
 
-void check_stats(const exact_physical_design_params& st) noexcept
+void check_stats(const exact_physical_design_stats& st) noexcept
 {
     CHECK(std::chrono::duration_cast<std::chrono::milliseconds>(st.time_total).count() > 0);
     CHECK(st.x_size > 0);
@@ -81,7 +66,7 @@ void check_stats(const exact_physical_design_params& st) noexcept
 }
 
 template <typename Lyt, typename Ntk>
-Lyt generate_layout(const Ntk& ntk, const exact_physical_design_params& ps)
+Lyt generate_layout(const Ntk& ntk, const exact_physical_design_params<Lyt>& ps)
 {
     exact_physical_design_stats stats{};
 
@@ -98,16 +83,16 @@ Lyt generate_layout(const Ntk& ntk, const exact_physical_design_params& ps)
 template <typename Lyt>
 void apply_gate_library(const Lyt& lyt)
 {
-    using cell_layout = cell_level_layout<qca_technology, clocked_layout<cartesian_layout<coord_t>>>;
+    using cell_layout = cell_level_layout<qca_technology, clocked_layout<cartesian_layout<cartesian::ucoord_t>>>;
     CHECK_NOTHROW(apply_gate_library<cell_layout, qca_one_library>(lyt));
 }
 
 template <typename Ntk>
 void check_all(const Ntk& ntk)
 {
-    using gate_layout = gate_level_layout<clocked_layout<tile_based_layout<cartesian_layout<coord_t>>>>;
+    using gate_layout = gate_level_layout<clocked_layout<tile_based_layout<cartesian_layout<cartesian::ucoord_t>>>>;
 
-    for (const auto& ps : configurations())
+    for (const auto& ps : configurations<gate_layout>())
     {
         const auto layout = generate_layout<gate_layout>(ntk, ps);
 
@@ -122,30 +107,25 @@ TEST_CASE("Exact physical design", "[exact]")
     check_all(blueprints::maj1_network<mockturtle::mig_network>());
     check_all(blueprints::constant_gate_input_maj_network<mockturtle::mig_network>());
     check_all(blueprints::multi_output_and_network<mockturtle::aig_network>());
-    check_all(blueprints::half_adder_network<mockturtle::aig_network>());
+//    check_all(blueprints::half_adder_network<mockturtle::aig_network>());
     check_all(blueprints::se_coloring_corner_case_network<mockturtle::aig_network>());
 }
 
-TEST_CASE("Timeout", "[exact]")
-{
-    using gate_layout = gate_level_layout<clocked_layout<tile_based_layout<cartesian_layout<coord_t>>>>;
+//TEST_CASE("Timeout", "[exact]")
+//{
+//    using gate_layout = gate_level_layout<clocked_layout<tile_based_layout<cartesian_layout<cartesian::ucoord_t>>>>;
+//
+//    exact_physical_design_params<gate_layout> timeout_config{};
+//
+//    timeout_config.scheme  = std::make_shared<clocking_scheme<coordinate<Lyt>>>(use_4_clocking);
+//    timeout_config.timeout = 1u;  // allow only one second to find a solution; this will fail (and is tested for)
+//
+//    const auto half_adder = blueprints::half_adder_network<mockturtle::aig_network>();
+//
+//    const auto layout = exact<gate_layout>(half_adder, timeout_config);
+//
+//    // since a half adder cannot be placed and routed in just one second, layout should not have a value
+//    CHECK(!layout.has_value());
+//}
 
-    exact_physical_design_params timeout_config{};
-
-    timeout_config.scheme       = std::make_shared<clocking_scheme<coord_t>>(use_4_clocking);
-    timeout_config.enable_and   = true;
-    timeout_config.enable_not   = true;
-    timeout_config.enable_or    = true;
-    timeout_config.enable_wires = true;
-    timeout_config.crossings    = true;
-    timeout_config.timeout      = 1u;  // allow only one second to find a solution; this will fail (and is tested for)
-
-    const auto half_adder = blueprints::half_adder_network<mockturtle::aig_network>();
-
-    const auto layout = exact<gate_layout>(half_adder, timeout_config);
-
-    // since a half adder cannot be placed and routed in just one second, layout should not have a value
-    CHECK(!layout.has_value());
-}
-
-#endif  // MUGEN
+#endif  // FICTION_Z3_SOLVER
