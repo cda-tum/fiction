a61d19ff85424b6340aaae63eea29b537a745c63
Marcel Walter <marcelwalter.mw@gmail.com>
2021-10-12 15:34:47 +0200

    Async test with 4 threads

--- test/algorithms/one_pass_synthesis.cpp
+++ test/algorithms/one_pass_synthesis.cpp
@@ -68,7 +68,7 @@ std::vector<one_pass_synthesis_params> configurations() noexcept
     async_config.enable_or    = true;
     async_config.enable_wires = true;
     async_config.crossings    = true;
-    async_config.num_threads  = 5ul;
+    async_config.num_threads  = 4ul;
 #endif
 
     return {{twoddwave_config, use_config, res_config
