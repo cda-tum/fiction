7510e74dd34a62d0a71d9850bcfecf814db67885
Marcel Walter <marcelwalter.mw@gmail.com>
2021-11-08 10:28:15 +0100

    Added row-based clocking scheme, i.e., a 90° rotated columnar scheme

--- test/layouts/clocking_scheme.cpp
+++ test/layouts/clocking_scheme.cpp
@@ -265,6 +265,138 @@ TEST_CASE("4-phase columnar clocking", "[clocking-scheme]")
     CHECK(columnar4({3 + 4, 3 + 4}) == 3);
 }
 
+TEST_CASE("3-phase row clocking", "[clocking-scheme]")
+{
+    using clk_lyt = clocked_layout<cartesian_layout<cartesian::ucoord_t>>;
+
+    const auto row3 = row_clocking<clk_lyt>(num_clks::THREE);
+
+    CHECK(row3.num_clocks == 3u);
+    CHECK(row3.max_in_degree == 3u);
+    CHECK(row3.max_out_degree == 2u);
+    CHECK(row3.is_regular());
+
+    CHECK(row3({0, 0}) == 0);
+    CHECK(row3({0, 1}) == 1);
+    CHECK(row3({0, 2}) == 2);
+    CHECK(row3({1, 0}) == 0);
+    CHECK(row3({1, 1}) == 1);
+    CHECK(row3({1, 2}) == 2);
+    CHECK(row3({2, 0}) == 0);
+    CHECK(row3({2, 1}) == 1);
+    CHECK(row3({2, 2}) == 2);
+
+    CHECK(row3({0 + 3, 0}) == 0);
+    CHECK(row3({0 + 3, 1}) == 1);
+    CHECK(row3({0 + 3, 2}) == 2);
+    CHECK(row3({1 + 3, 0}) == 0);
+    CHECK(row3({1 + 3, 1}) == 1);
+    CHECK(row3({1 + 3, 2}) == 2);
+    CHECK(row3({2 + 3, 0}) == 0);
+    CHECK(row3({2 + 3, 1}) == 1);
+    CHECK(row3({2 + 3, 2}) == 2);
+
+    CHECK(row3({0, 0 + 3}) == 0);
+    CHECK(row3({0, 1 + 3}) == 1);
+    CHECK(row3({0, 2 + 3}) == 2);
+    CHECK(row3({1, 0 + 3}) == 0);
+    CHECK(row3({1, 1 + 3}) == 1);
+    CHECK(row3({1, 2 + 3}) == 2);
+    CHECK(row3({2, 0 + 3}) == 0);
+    CHECK(row3({2, 1 + 3}) == 1);
+    CHECK(row3({2, 2 + 3}) == 2);
+
+    CHECK(row3({0 + 3, 0 + 3}) == 0);
+    CHECK(row3({0 + 3, 1 + 3}) == 1);
+    CHECK(row3({0 + 3, 2 + 3}) == 2);
+    CHECK(row3({1 + 3, 0 + 3}) == 0);
+    CHECK(row3({1 + 3, 1 + 3}) == 1);
+    CHECK(row3({1 + 3, 2 + 3}) == 2);
+    CHECK(row3({2 + 3, 0 + 3}) == 0);
+    CHECK(row3({2 + 3, 1 + 3}) == 1);
+    CHECK(row3({2 + 3, 2 + 3}) == 2);
+}
+
+TEST_CASE("4-phase row clocking", "[clocking-scheme]")
+{
+    using clk_lyt = clocked_layout<cartesian_layout<cartesian::ucoord_t>>;
+
+    const auto row4 = row_clocking<clk_lyt>(num_clks::FOUR);
+
+    CHECK(row4.num_clocks == 4u);
+    CHECK(row4.max_in_degree == 3u);
+    CHECK(row4.max_out_degree == 2u);
+    CHECK(row4.is_regular());
+
+    CHECK(row4({0, 0}) == 0);
+    CHECK(row4({0, 1}) == 1);
+    CHECK(row4({0, 2}) == 2);
+    CHECK(row4({0, 3}) == 3);
+    CHECK(row4({1, 0}) == 0);
+    CHECK(row4({1, 1}) == 1);
+    CHECK(row4({1, 2}) == 2);
+    CHECK(row4({1, 3}) == 3);
+    CHECK(row4({2, 0}) == 0);
+    CHECK(row4({2, 1}) == 1);
+    CHECK(row4({2, 2}) == 2);
+    CHECK(row4({2, 3}) == 3);
+    CHECK(row4({3, 0}) == 0);
+    CHECK(row4({3, 1}) == 1);
+    CHECK(row4({3, 2}) == 2);
+    CHECK(row4({3, 3}) == 3);
+
+    CHECK(row4({0 + 4, 0}) == 0);
+    CHECK(row4({0 + 4, 1}) == 1);
+    CHECK(row4({0 + 4, 2}) == 2);
+    CHECK(row4({0 + 4, 3}) == 3);
+    CHECK(row4({1 + 4, 0}) == 0);
+    CHECK(row4({1 + 4, 1}) == 1);
+    CHECK(row4({1 + 4, 2}) == 2);
+    CHECK(row4({1 + 4, 3}) == 3);
+    CHECK(row4({2 + 4, 0}) == 0);
+    CHECK(row4({2 + 4, 1}) == 1);
+    CHECK(row4({2 + 4, 2}) == 2);
+    CHECK(row4({2 + 4, 3}) == 3);
+    CHECK(row4({3 + 4, 0}) == 0);
+    CHECK(row4({3 + 4, 1}) == 1);
+    CHECK(row4({3 + 4, 2}) == 2);
+    CHECK(row4({3 + 4, 3}) == 3);
+
+    CHECK(row4({0, 0 + 4}) == 0);
+    CHECK(row4({0, 1 + 4}) == 1);
+    CHECK(row4({0, 2 + 4}) == 2);
+    CHECK(row4({0, 3 + 4}) == 3);
+    CHECK(row4({1, 0 + 4}) == 0);
+    CHECK(row4({1, 1 + 4}) == 1);
+    CHECK(row4({1, 2 + 4}) == 2);
+    CHECK(row4({1, 3 + 4}) == 3);
+    CHECK(row4({2, 0 + 4}) == 0);
+    CHECK(row4({2, 1 + 4}) == 1);
+    CHECK(row4({2, 2 + 4}) == 2);
+    CHECK(row4({2, 3 + 4}) == 3);
+    CHECK(row4({3, 0 + 4}) == 0);
+    CHECK(row4({3, 1 + 4}) == 1);
+    CHECK(row4({3, 2 + 4}) == 2);
+    CHECK(row4({3, 3 + 4}) == 3);
+
+    CHECK(row4({0 + 4, 0 + 4}) == 0);
+    CHECK(row4({0 + 4, 1 + 4}) == 1);
+    CHECK(row4({0 + 4, 2 + 4}) == 2);
+    CHECK(row4({0 + 4, 3 + 4}) == 3);
+    CHECK(row4({1 + 4, 0 + 4}) == 0);
+    CHECK(row4({1 + 4, 1 + 4}) == 1);
+    CHECK(row4({1 + 4, 2 + 4}) == 2);
+    CHECK(row4({1 + 4, 3 + 4}) == 3);
+    CHECK(row4({2 + 4, 0 + 4}) == 0);
+    CHECK(row4({2 + 4, 1 + 4}) == 1);
+    CHECK(row4({2 + 4, 2 + 4}) == 2);
+    CHECK(row4({2 + 4, 3 + 4}) == 3);
+    CHECK(row4({3 + 4, 0 + 4}) == 0);
+    CHECK(row4({3 + 4, 1 + 4}) == 1);
+    CHECK(row4({3 + 4, 2 + 4}) == 2);
+    CHECK(row4({3 + 4, 3 + 4}) == 3);
+}
+
 TEST_CASE("3-phase 2DDWave", "[clocking-scheme]")
 {
     using clk_lyt = clocked_layout<cartesian_layout<cartesian::ucoord_t>>;
@@ -1709,6 +1841,7 @@ TEST_CASE("Clocking lookup", "[clocking-scheme]")
 
     check({"open", "OPEN", "oPeN", "OpEn"}, clock_name::open);
     check({"columnar", "COLUMNAR", "CoLumNar", "COLUMnar"}, clock_name::columnar);
+    check({"row", "ROW", "RoW", "rOw"}, clock_name::row);
     check({"2DDwave", "2DdWaVe", "2ddwave", "2DDWAVE", "2DDWave"}, clock_name::twoddwave);
     check({"2DDwavehex", "2DdWaVeHeX", "2ddwavehex", "2DDWAVEHEX", "2DDWaveHex"}, clock_name::twoddwave);
     check({"use", "USE", "uSe", "UsE"}, clock_name::use);
@@ -1717,11 +1850,12 @@ TEST_CASE("Clocking lookup", "[clocking-scheme]")
     check({"bancs", "BANCS", "BaNCs", "banCS"}, clock_name::bancs);
 
     CHECK(!get_clocking_scheme<clk_lyt>("").has_value());
+    CHECK(!get_clocking_scheme<clk_lyt>("Column").has_value());
+    CHECK(!get_clocking_scheme<clk_lyt>("Rows").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("TwoDDWave").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("2DDWave6").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("SUE").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("ERS").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("EPS").has_value());
     CHECK(!get_clocking_scheme<clk_lyt>("BNCS").has_value());
-    CHECK(!get_clocking_scheme<clk_lyt>("Column").has_value());
 }
