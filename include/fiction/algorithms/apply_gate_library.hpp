62aeb6845ba7271e0fb07b77304b13766a509eca
Marcel Walter <marcelwalter.mw@gmail.com>
2021-11-19 23:10:19 +0100

    Hard-coded mirrored Bestagon gates. Dot offset seems to work now. No guarantee though.

--- include/fiction/algorithms/apply_gate_library.hpp
+++ include/fiction/algorithms/apply_gate_library.hpp
@@ -65,14 +65,13 @@ class apply_gate_library_impl
                         if constexpr (has_pointy_top_hex_orientation_v<GateLyt>)
                         {
                             // vertical distance between pointy top hexagons is height * 3/4
-                            c = {t.x * GateLibrary::gate_x_size(),
-                                 static_cast<decltype(c.y)>((t.y * (GateLibrary::gate_y_size() * 3 / 4))),
-                                 t.z};
+                            c = {t.x * GateLibrary::gate_x_size() + 1,
+                                 static_cast<decltype(c.y)>(t.y * (GateLibrary::gate_y_size() * 3 / 4)), t.z};
                         }
                         else if constexpr (has_flat_top_hex_orientation_v<GateLyt>)
                         {
                             // horizontal distance between flat top hexagons is width * 3/4
-                            c = {static_cast<decltype(c.x)>((t.x * (GateLibrary::gate_x_size() * 3 / 4))),
+                            c = {static_cast<decltype(c.x)>(t.x * (GateLibrary::gate_x_size() * 3 / 4)),
                                  t.y * (GateLibrary::gate_y_size()), t.z};
                         }
 
@@ -81,8 +80,8 @@ class apply_gate_library_impl
                             if (gate_lyt.is_in_odd_row(t))
                             {
                                 // odd rows are shifted in by width / 2
-                                c.x += static_cast<decltype(c.x)>(
-                                    std::ceil(GateLibrary::gate_x_size() / static_cast<decltype(c.x)>(2)) + 1);
+                                c.x +=
+                                    static_cast<decltype(c.x)>(static_cast<double>(GateLibrary::gate_x_size()) / 2.0);
                             }
                         }
                         else if constexpr (has_even_row_hex_arrangement_v<GateLyt>)
@@ -90,8 +89,8 @@ class apply_gate_library_impl
                             if (gate_lyt.is_in_even_row(t))
                             {
                                 // even rows are shifted in by width / 2
-                                c.x += static_cast<decltype(c.x)>(
-                                    std::ceil(GateLibrary::gate_x_size() / static_cast<decltype(c.x)>(2)) + 1);
+                                c.x +=
+                                    static_cast<decltype(c.x)>(static_cast<double>(GateLibrary::gate_x_size()) / 2.0);
                             }
                         }
                         else if constexpr (has_odd_column_hex_arrangement_v<GateLyt>)
@@ -99,8 +98,8 @@ class apply_gate_library_impl
                             if (gate_lyt.is_in_odd_column(t))
                             {
                                 // odd columns are shifted in by height / 2
-                                c.y += static_cast<decltype(c.y)>(
-                                    std::ceil(GateLibrary::gate_y_size() / static_cast<decltype(c.y)>(2)) + 1);
+                                c.y +=
+                                    static_cast<decltype(c.y)>(static_cast<double>(GateLibrary::gate_y_size()) / 2.0);
                             }
                         }
                         else if constexpr (has_even_column_hex_arrangement_v<GateLyt>)
@@ -108,8 +107,8 @@ class apply_gate_library_impl
                             if (gate_lyt.is_in_even_column(t))
                             {
                                 // even columns are shifted in by height / 2
-                                c.y += static_cast<decltype(c.y)>(
-                                    std::ceil(GateLibrary::gate_y_size() / static_cast<decltype(c.y)>(2)) + 1);
+                                c.y +=
+                                    static_cast<decltype(c.y)>(static_cast<double>(GateLibrary::gate_y_size()) / 2.0);
                             }
                         }
                     }
