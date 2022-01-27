e41dfbb96aa882a3281471180c6eb9a850394395
Marcel Walter <marcelwalter.mw@gmail.com>
2022-01-27 11:25:55 +0100

    Code reuse from fiction/utils/name_utils.hpp

--- cli/cmd/logic/simulate.hpp
+++ cli/cmd/logic/simulate.hpp
@@ -6,6 +6,7 @@
 #define FICTION_CMD_SIMULATE_HPP
 
 #include <fiction/types.hpp>
+#include <fiction/utils/name_utils.hpp>
 
 #include <alice/alice.hpp>
 #include <kitty/print.hpp>
@@ -130,21 +131,7 @@ class simulate_command : public command
     template <typename NtkOrLytVariant>
     void perform_simulation(const NtkOrLytVariant& network_or_layout_variant)
     {
-        const auto get_name = [](auto&& ntk_or_lyt_ptr) -> std::string
-        {
-            using NtkOrLyt = typename std::decay_t<decltype(ntk_or_lyt_ptr)>::element_type;
-
-            if constexpr (mockturtle::has_get_network_name_v<NtkOrLyt>)
-            {
-                return ntk_or_lyt_ptr->get_network_name();
-            }
-            else if constexpr (fiction::has_get_layout_name_v<NtkOrLyt>)
-            {
-                return ntk_or_lyt_ptr->get_layout_name();
-            }
-
-            return {};
-        };
+        const auto get_name = [](auto&& ntk_or_lyt_ptr) -> std::string { return fiction::get_name(*ntk_or_lyt_ptr); };
 
         const auto store_po_names = [this](auto&& ntk_or_lyt_ptr)
         {
