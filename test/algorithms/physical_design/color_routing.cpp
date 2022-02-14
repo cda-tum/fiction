8bb3671b9025b106f1cfaf029c5bf915016e1a6c
Marcel Walter <marcelwalter.mw@gmail.com>
2022-02-14 16:23:18 +0100

    test: Use clear_routing in the color_routing test case

--- test/algorithms/physical_design/color_routing.cpp
+++ test/algorithms/physical_design/color_routing.cpp
@@ -8,6 +8,7 @@
 
 #include <fiction/algorithms/physical_design/color_routing.hpp>
 #include <fiction/types.hpp>
+#include <fiction/utils/routing_utils.hpp>
 
 using namespace fiction;
 
@@ -27,8 +28,8 @@ TEST_CASE("Simple wire connection", "[color-routing]")
     auto spec_layout = blueprints::straight_wire_gate_layout<cart_gate_clk_lyt>();
     auto impl_layout = blueprints::straight_wire_gate_layout<cart_gate_clk_lyt>();
 
-    // remove the wire from the implementation
-    impl_layout.clear_tile({1, 1});
+    // remove the wire routing from the implementation
+    clear_routing(impl_layout);
 
     // color routing should be able to recreate the removed wire
     const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{{{0, 1}, {2, 1}}};
@@ -52,16 +53,13 @@ TEST_CASE("Two paths wire connections", "[color-routing]")
     auto spec_layout = blueprints::unbalanced_and_layout<cart_gate_clk_lyt>();
     auto impl_layout = blueprints::unbalanced_and_layout<cart_gate_clk_lyt>();
 
-    // remove the wire from the implementation
-    impl_layout.clear_tile({1, 2});
-    impl_layout.clear_tile({2, 2});
-    impl_layout.clear_tile({2, 1});
-
-    // rip-up the AND gate's remaining connection
-    impl_layout.move_node(impl_layout.get_node({2, 0}), {2, 0}, {});
+    // remove the wire routing from the implementation
+    clear_routing(impl_layout);
 
     // color routing should be able to recreate the removed wire
-    const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{{{0, 2}, {2, 0}}, {{1, 0}, {2, 0}}};
+    const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{{{0, 2}, {2, 0}},
+                                                                       {{1, 0}, {2, 0}},
+                                                                       {{2, 0}, {3, 0}}};
 
     color_routing_params ps{};
 
@@ -82,16 +80,8 @@ TEST_CASE("Three paths wire connections", "[color-routing]")
     auto spec_layout = blueprints::three_wire_paths_gate_layout<cart_gate_clk_lyt>();
     auto impl_layout = blueprints::three_wire_paths_gate_layout<cart_gate_clk_lyt>();
 
-    // remove the wire from the implementation
-    impl_layout.clear_tile({1, 0});
-    impl_layout.clear_tile({1, 2});
-    impl_layout.clear_tile({1, 4});
-    impl_layout.clear_tile({2, 0});
-    impl_layout.clear_tile({2, 2});
-    impl_layout.clear_tile({2, 4});
-    impl_layout.clear_tile({3, 0});
-    impl_layout.clear_tile({3, 2});
-    impl_layout.clear_tile({3, 4});
+    // remove the wire routing from the implementation
+    clear_routing(impl_layout);
 
     // color routing should be able to recreate the removed wire
     const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{{{0, 0}, {4, 0}},
@@ -117,11 +107,8 @@ TEST_CASE("Direct gate connections", "[color-routing]")
     auto spec_layout = blueprints::xor_maj_gate_layout<cart_gate_clk_lyt>();
     auto impl_layout = blueprints::xor_maj_gate_layout<cart_gate_clk_lyt>();
 
-    // rip-up all connections
-    impl_layout.move_node(impl_layout.get_node({2, 1}), {2, 1}, {});
-    impl_layout.move_node(impl_layout.get_node({1, 0}), {1, 0}, {});
-    impl_layout.move_node(impl_layout.get_node({2, 2}), {2, 2}, {});
-    impl_layout.move_node(impl_layout.get_node({0, 0}), {0, 0}, {});
+    // remove the wire routing from the implementation
+    clear_routing(impl_layout);
 
     // color routing should be able to recreate the removed wire
     const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{
@@ -147,21 +134,8 @@ TEST_CASE("Partial routing", "[color-routing]")
     auto spec_layout = blueprints::use_and_gate_layout<cart_gate_clk_lyt>();
     auto impl_layout = blueprints::use_and_gate_layout<cart_gate_clk_lyt>();
 
-    // remove the wire from the implementation
-    impl_layout.clear_tile({0, 0});
-    impl_layout.clear_tile({1, 0});
-    impl_layout.clear_tile({1, 1});
-    impl_layout.clear_tile({2, 3});
-    impl_layout.clear_tile({1, 3});
-    impl_layout.clear_tile({0, 3});
-    impl_layout.clear_tile({0, 2});
-    impl_layout.clear_tile({2, 2});
-
-    // rip-up all connections
-    impl_layout.move_node(impl_layout.get_node({0, 1}), {0, 1}, {});
-    impl_layout.move_node(impl_layout.get_node({3, 3}), {3, 3}, {});
-    impl_layout.move_node(impl_layout.get_node({1, 2}), {1, 2}, {});
-    impl_layout.move_node(impl_layout.get_node({3, 2}), {3, 2}, {});
+    // remove the wire routing from the implementation
+    clear_routing(impl_layout);
 
     // color routing should be able to recreate the removed wire
     const std::vector<routing_objective<cart_gate_clk_lyt>> objectives{
