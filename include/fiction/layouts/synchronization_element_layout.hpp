0f548d3a756838b6eb7536b2fe60f337985807e7
ClangFormat <ClangFormat@users.noreply.github.com>
2022-12-06 10:48:40 +0000

    :art: ClangFormat changes
    Signed-off-by: ClangFormat <ClangFormat@users.noreply.github.com>

--- include/fiction/layouts/synchronization_element_layout.hpp
+++ include/fiction/layouts/synchronization_element_layout.hpp
@@ -317,7 +317,7 @@ class synchronization_element_layout : public ClockedLayout
     [[nodiscard]] typename ClockedLayout::degree_t degree(const clock_zone<ClockedLayout>& cz) const noexcept
     {
         const auto clock_degree = static_cast<typename ClockedLayout::degree_t>(ClockedLayout::degree(cz));
-        const auto se_degree = static_cast<typename ClockedLayout::degree_t>(in_degree(cz) + out_degree(cz));
+        const auto se_degree    = static_cast<typename ClockedLayout::degree_t>(in_degree(cz) + out_degree(cz));
         return se_degree > clock_degree ? clock_degree : se_degree;
     }
 
