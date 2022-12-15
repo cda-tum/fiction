fde6f16551595490ade5a0f9026a59be7cbe4a7f
Marcel Walter <marcel.walter@tum.de>
2022-12-15 20:54:46 +0100

    :sparkles: Enabled A* parameters

--- bindings/pyfiction/include/pyfiction/algorithms/path_finding/a_star.hpp
+++ bindings/pyfiction/include/pyfiction/algorithms/path_finding/a_star.hpp
@@ -26,12 +26,15 @@ void a_star(pybind11::module& m)
 
     m.def(
         "a_star",
-        [](const Lyt& lyt, const fiction::coordinate<Lyt>& source, const fiction::coordinate<Lyt>& target)
+        [](const Lyt& lyt, const fiction::coordinate<Lyt>& source, const fiction::coordinate<Lyt>& target,
+           const fiction::a_star_params& params)
         {
             return static_cast<std::vector<fiction::coordinate<Lyt>>>(
-                fiction::a_star<fiction::layout_coordinate_path<Lyt>, Lyt>(lyt, {source, target}));
+                fiction::a_star<fiction::layout_coordinate_path<Lyt>, Lyt>(lyt, {source, target},
+                                                                           fiction::manhattan_distance_functor<Lyt>(),
+                                                                           fiction::unit_cost_functor<Lyt>(), params));
         },
-        "layout"_a, "source"_a, "target"_a);
+        "layout"_a, "source"_a, "target"_a, "params"_a = fiction::a_star_params{});
 
     m.def("a_star_distance", &fiction::a_star_distance<Lyt, double>, "layout"_a, "source"_a, "target"_a);
 }
@@ -40,6 +43,14 @@ void a_star(pybind11::module& m)
 
 inline void a_star(pybind11::module& m)
 {
+    namespace py = pybind11;
+
+    py::class_<fiction::a_star_params>(m, "a_star_params")
+        .def(py::init<>())
+        .def_readwrite("crossings", &fiction::a_star_params::crossings)
+
+        ;
+
     detail::a_star<py_cartesian_clocked_layout>(m);
     detail::a_star<py_cartesian_gate_layout>(m);
     detail::a_star<py_hexagonal_clocked_layout>(m);
