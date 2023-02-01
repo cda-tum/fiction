f0b88ab8ebdfc0fc30018d4a916d090d50ad16f6
Marcel Walter <marcel.walter@tum.de>
2023-02-01 14:53:09 +0100

    :art: Code formatting

--- include/fiction/technology/sidb_charge_state.hpp
+++ include/fiction/technology/sidb_charge_state.hpp
@@ -5,11 +5,16 @@
 #ifndef FICTION_SIDB_CHARGE_STATE_HPP
 #define FICTION_SIDB_CHARGE_STATE_HPP
 
+#include <cassert>
+#include <cstdint>
 #include <sstream>
+#include <vector>
+
 namespace fiction
 {
+
 /**
- * Possible SiDB charges.
+ * Charge states of SiDBs.
  */
 enum class sidb_charge_state
 {
@@ -18,64 +23,105 @@ enum class sidb_charge_state
     NEUTRAL,
     NEGATIVE
 };
-
 /**
- * Converts the charge state into int (-1,0,1).
+ * Converts the charge state into an integer (`-1`, `0`, `1`).
  *
- * @param cs sidb charge state.
- * @return int representing the SiDB's charge state.
+ * @param cs SiDB charge state.
+ * @return Integer representing the SiDB's charge state.
  */
-[[nodiscard]] constexpr int charge_state_to_sign(const sidb_charge_state& cs) noexcept
+[[nodiscard]] constexpr int8_t charge_state_to_sign(const sidb_charge_state& cs) noexcept
 {
     switch (cs)
     {
-        case sidb_charge_state::POSITIVE: return +1;
-        case sidb_charge_state::NEGATIVE: return -1;
-        default: return 0;
+        case sidb_charge_state::POSITIVE:
+        {
+            return +1;
+        }
+        case sidb_charge_state::NEGATIVE:
+        {
+            return -1;
+        }
+        default:
+        {
+            return 0;
+        }
     }
 }
-
 /**
- * Converts the charge state (-1,0,1) into enum.
+ * Converts a charge state (-1,0,1) into an enum.
  *
- * @param sg charge state as integer (-1,0,1).
+ * @param sg Charge state as integer (-1,0,1).
  * @return sidb_charge_state.
  */
-[[nodiscard]] constexpr sidb_charge_state sign_to_charge_state(const int& sg) noexcept
+[[nodiscard]] constexpr sidb_charge_state sign_to_charge_state(const int8_t sg) noexcept
 {
+    assert((sg == -1) || (sg == 0) || (sg == 1) && "Invalid charge state.");
+
     switch (sg)
     {
-        case 1: return sidb_charge_state::POSITIVE;
-        case -1: return sidb_charge_state::NEGATIVE;
-        case 0: return sidb_charge_state::NEUTRAL;
-        default: return sidb_charge_state::NONE;
+        case 1:
+        {
+            return sidb_charge_state::POSITIVE;
+        }
+        case -1:
+        {
+            return sidb_charge_state::NEGATIVE;
+        }
+        case 0:
+        {
+            return sidb_charge_state::NEUTRAL;
+        }
+        default:
+        {
+            return sidb_charge_state::NONE;
+        }
     }
 }
-
 /**
- * Converts the vector of charges (enums) to a string (-101...).
+ * Converts a vector of charge states to a string representation (`"-101..."`).
  *
- * @param charge_distribution vector of the SiDBs' charge states.
- * @return string of the charge states.
+ * @param charge_distribution A vector of SiDBs charge states.
+ * @return A string representation of the charge states.
  */
 [[nodiscard]] std::string
 charge_configuration_to_string(const std::vector<sidb_charge_state>& charge_distribution) noexcept
 {
-    std::ostringstream config_str;
+    std::stringstream config_str{};
+
     for (const auto& cs : charge_distribution)
     {
         if (cs == sidb_charge_state::NONE)
         {
             continue;
         }
+
         switch (cs)
         {
-            case sidb_charge_state::NEGATIVE: config_str << "-"; break;
-            case sidb_charge_state::NEUTRAL: config_str << "0"; break;
-            case sidb_charge_state::POSITIVE: config_str << "+"; break;
-            case sidb_charge_state::NONE:; break;
+            case sidb_charge_state::NEGATIVE:
+            {
+                config_str << "-";
+
+                break;
+            }
+            case sidb_charge_state::NEUTRAL:
+            {
+                config_str << "0";
+
+                break;
+            }
+            case sidb_charge_state::POSITIVE:
+            {
+                config_str << "+";
+
+                break;
+            }
+            case sidb_charge_state::NONE:
+            {
+                break;
+            }
         }
     }
+
     return config_str.str();
 }
 
