fb002ce6dd20f8d8a4f1d360fdf727a174c566d0
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2023-02-06 13:47:36 +0100

    :white_check_mark: occupation function with tests

--- include/fiction/algorithms/simulation/sidb/occupation_function_pi.hpp
+++ include/fiction/algorithms/simulation/sidb/occupation_function_pi.hpp
@@ -7,22 +7,47 @@
 
 #include <map>
 #include <cmath>
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <numeric>
 
-double occupation_propability_pi(std::map<double, uint64_t>& energy_deg, const double &temp, uint64_t &state_number)
-{
-    static_assert((energy_deg < state_number), "Lyt is not a cell-level layout");
+namespace fiction {
+    double occupation_propability_pi(const std::map<double, uint64_t> &energy_deg, const double &temp,
+                                     const uint64_t &state_number) {
 
-    double part_func = 0;
-    for (const auto& it : energy_deg)
-    {
-        part_func += static_cast<double>(it.second) * std::exp(-it.first * 12000 / temp);
+        assert(!energy_deg.empty() && "map is empty");
+        assert(!(energy_deg.size() < state_number) && "state_number is too large");
+
+        if (temp == 0) {
+            auto it = energy_deg.begin();
+            for (uint64_t i = 0; i < state_number; i++) {
+                it++;
+            }
+
+            if (it != energy_deg.begin()) {
+                return 0;
+            }
+            return 1;
+
+        }
+
+        double part_func = std::accumulate(energy_deg.begin(), energy_deg.end(), 0.0,
+                                           [&](double sum, const auto &it) {
+                                               return sum + static_cast<double>(it.second) *
+                                                            std::exp(-it.first * 12000 / temp);
+                                           });
+
+
+        auto it = energy_deg.begin();
+        for (uint64_t i = 0; i < state_number; i++) {
+            it++;
+        }
+
+        return static_cast<double>(it->second) * std::exp(-it->first * 12000 / temp) / part_func;
+
     }
 
-    auto it = std::advance(energy_deg.begin(), state_number);
-
-    return static_cast<double>(it->second) * std::exp(-it->first * 12000 / temp) / part_func;
-
-};
-
+} // namespace fiction
 
 #endif  // FICTION_OCCUPATION_FUNCTION_HPP
--- test/algorithms/simulation/sidb/occupation_function_pi.cpp
+++ test/algorithms/simulation/sidb/occupation_function_pi.cpp
@@ -3,77 +3,34 @@
 //
 
 #include <catch2/catch_template_test_macros.hpp>
-
-#include <fiction/algorithms/simulation/sidb/energy_distribution.hpp>
-#include <fiction/algorithms/simulation/sidb/quicksim.hpp>
-#include <fiction/layouts/cartesian_layout.hpp>
-#include <fiction/layouts/cell_level_layout.hpp>
-#include <fiction/layouts/clocked_layout.hpp>
-#include <fiction/layouts/hexagonal_layout.hpp>
-#include <fiction/technology/cell_technologies.hpp>
+#include <fiction/algorithms/simulation/sidb/occupation_function_pi.hpp>
 
 using namespace fiction;
 
-TEMPLATE_TEST_CASE(
-"Test minimum energy function", "[minimum energy]",
-(cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>),
-(cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, odd_row_hex>>>),
-(cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, even_row_hex>>>),
-(cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, odd_column_hex>>>),
-(cell_level_layout<sidb_technology, clocked_layout<hexagonal_layout<siqad::coord_t, even_column_hex>>>))
+TEST_CASE("occupation function for simple energy distribution", "[occupation_function_pi]")
 {
-SECTION("layout with no SiDB placed")
-{
-TestType lyt{{10, 10}};
 
-std::vector<charge_distribution_surface<TestType>> all_lyts{};
-charge_distribution_surface                        charge_layout{lyt};
 
-CHECK(minimum_energy(all_lyts) == std::numeric_limits<double>::max());
+    SECTION("no states")
+    {
+        std::map<double, uint64_t> test_map{};
+    }
 
-all_lyts.push_back(charge_layout);
+    SECTION("a few states with degeneracy")
+    {
+        std::map<double, uint64_t> test_map{};
+        test_map.insert(std::make_pair(0, 1));
+        test_map.insert(std::make_pair(0.2, 3));
+        test_map.insert(std::make_pair(0.4, 4));
+        test_map.insert(std::make_pair(0.5, 6));
+        test_map.insert(std::make_pair(1.5, 1));
+        test_map.insert(std::make_pair(1.7, 0));
 
-CHECK(std::abs(minimum_energy(all_lyts) - 0) < 0.00000001);
-}
+        CHECK(occupation_propability_pi(test_map, 10, 3) > 0);
+        CHECK(occupation_propability_pi(test_map, 10, 5) == 0);
+        CHECK(occupation_propability_pi(test_map, 0, 0) == 1);
 
-SECTION("layout with one SiDB placed")
-{
-TestType lyt{{10, 10}};
-lyt.assign_cell_type({0, 0}, TestType::cell_type::NORMAL);
-std::vector<charge_distribution_surface<TestType>> all_lyts{};
-charge_distribution_surface                        charge_layout{lyt};
-
-CHECK(minimum_energy(all_lyts) == std::numeric_limits<double>::max());
-
-all_lyts.push_back(charge_layout);
-
-CHECK(std::abs(minimum_energy(all_lyts) - 0) < 0.00000001);
-}
-
-SECTION("layout with three SiDBs placed")
-{
-TestType lyt{{10, 10}};
-lyt.assign_cell_type({0, 0}, TestType::cell_type::NORMAL);
-lyt.assign_cell_type({10, 10}, TestType::cell_type::NORMAL);
-lyt.assign_cell_type({9, 9}, TestType::cell_type::NORMAL);
-
-std::vector<charge_distribution_surface<TestType>> all_lyts{};
-charge_distribution_surface                        charge_layout_first{lyt};
-
-CHECK(minimum_energy(all_lyts) == std::numeric_limits<double>::max());
-
-charge_layout_first.assign_charge_state({0, 0}, sidb_charge_state::NEUTRAL);
-charge_layout_first.update_local_potential();
-charge_layout_first.recompute_system_energy();
-all_lyts.push_back(charge_layout_first);
-
-charge_distribution_surface charge_layout_second{lyt};
-charge_layout_second.assign_charge_state({10, 10}, sidb_charge_state::NEUTRAL);
-charge_layout_second.assign_charge_state({9, 9}, sidb_charge_state::NEUTRAL);
-charge_layout_second.update_local_potential();
-charge_layout_second.recompute_system_energy();
-all_lyts.push_back(charge_layout_second);
-
-CHECK(std::abs(minimum_energy(all_lyts) - 0) < 0.00000001);
-}
+        test_map.insert(std::make_pair(0, 2));
+        CHECK(occupation_propability_pi(test_map, 0, 0) == 1);
+    }
 }
