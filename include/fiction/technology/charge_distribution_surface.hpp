934e5bd31a74600cc7b3f65b99abc10431d7d4ce
Marcel Walter <marcel.walter@tum.de>
2023-03-14 13:49:56 +0100

    :art: Improved structure of the `validity_check` function

--- include/fiction/technology/charge_distribution_surface.hpp
+++ include/fiction/technology/charge_distribution_surface.hpp
@@ -571,42 +571,41 @@ class charge_distribution_surface<Lyt, false> : public Lyt
      */
     void validity_check() noexcept
     {
-        uint64_t population_stability_not_fulfilled_counter = 0;
-        uint64_t for_loop_counter                           = 0;
+        uint64_t for_loop_counter = 0;
 
-        for (const auto& it : strg->loc_pot)  // this for-loop checks if the "population stability" is fulfilled.
+        for (const auto& it : strg->loc_pot)  // this for-loop checks if the "population stability" is fulfilled
         {
-            bool valid = (((strg->cell_charge[for_loop_counter] == sidb_charge_state::NEGATIVE) &&
-                           ((-it + strg->phys_params.mu) < physical_constants::POP_STABILITY_ERR)) ||
-                          ((strg->cell_charge[for_loop_counter] == sidb_charge_state::POSITIVE) &&
-                           ((-it + strg->phys_params.mu_p) > -physical_constants::POP_STABILITY_ERR)) ||
-                          ((strg->cell_charge[for_loop_counter] == sidb_charge_state::NEUTRAL) &&
-                           ((-it + strg->phys_params.mu) > -physical_constants::POP_STABILITY_ERR) &&
-                           (-it + strg->phys_params.mu_p) < physical_constants::POP_STABILITY_ERR));
-            for_loop_counter += 1;
+            const bool valid = (((strg->cell_charge[for_loop_counter] == sidb_charge_state::NEGATIVE) &&
+                                 ((-it + strg->phys_params.mu) < physical_constants::POP_STABILITY_ERR)) ||
+                                ((strg->cell_charge[for_loop_counter] == sidb_charge_state::POSITIVE) &&
+                                 ((-it + strg->phys_params.mu_p) > -physical_constants::POP_STABILITY_ERR)) ||
+                                ((strg->cell_charge[for_loop_counter] == sidb_charge_state::NEUTRAL) &&
+                                 ((-it + strg->phys_params.mu) > -physical_constants::POP_STABILITY_ERR) &&
+                                 (-it + strg->phys_params.mu_p) < physical_constants::POP_STABILITY_ERR));
+
+            ++for_loop_counter;
+
             if (!valid)
             {
                 strg->validity = false;  // if at least one SiDB does not fulfill the population stability, the validity
-                                         // of the given charge distribution is set to "false".
-                population_stability_not_fulfilled_counter += 1;
-                break;
+                                         // of the given charge distribution is set to "false"
+
+                return;
             }
         }
 
-        if ((population_stability_not_fulfilled_counter == 0) &&
-            (for_loop_counter >
-             0))  // if population stability is fulfilled for all SiDBs, the "configuration stability" is checked.
+        // if population stability is fulfilled for all SiDBs, the "configuration stability" is checked next
+        if (for_loop_counter > 0)
         {
-            const auto hop_del =
-                [this](const uint64_t c1, const uint64_t c2)  // energy change when charge hops between two SiDBs.
+            const auto hop_delta =
+                [this](const uint64_t c1, const uint64_t c2)  // energy change when charge hops between two SiDBs
             {
                 const int dn_i = (strg->cell_charge[c1] == sidb_charge_state::NEGATIVE) ? 1 : -1;
                 const int dn_j = -dn_i;
 
-                return strg->loc_pot[c1] * dn_i + strg->loc_pot[c2] * dn_j - strg->pot_mat[c1][c2] * 1;
+                return strg->loc_pot[c1] * dn_i + strg->loc_pot[c2] * dn_j - strg->pot_mat[c1][c2];
             };
 
-            uint64_t hop_counter = 0;
             for (uint64_t i = 0u; i < strg->loc_pot.size(); ++i)
             {
                 if (strg->cell_charge[i] == sidb_charge_state::POSITIVE)  // we do nothing with SiDB+
@@ -616,26 +615,26 @@ class charge_distribution_surface<Lyt, false> : public Lyt
 
                 for (uint64_t j = 0u; j < strg->loc_pot.size(); j++)
                 {
-                    if (hop_counter == 1)
+                    if (i == j)
                     {
-                        break;
+                        continue;
                     }
 
-                    if (const auto e_del = hop_del(i, j);
+                    if (const auto e_del = hop_delta(i, j);
                         (charge_state_to_sign(strg->cell_charge[j]) > charge_state_to_sign(strg->cell_charge[i])) &&
                         (e_del < -physical_constants::POP_STABILITY_ERR))  // Checks if energetically favored hops
                                                                            // exist between two SiDBs.
                     {
-                        hop_counter = 1;
+                        strg->validity = false;
 
-                        break;
+                        return;
                     }
                 }
             }
 
             // If there is no jump that leads to a decrease in the potential energy of the system, the given charge
             // distribution satisfies metastability.
-            strg->validity = hop_counter == 0;
+            strg->validity = true;
         }
     }
     /**
