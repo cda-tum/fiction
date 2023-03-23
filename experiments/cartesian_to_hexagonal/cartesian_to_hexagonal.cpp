80ba8a49e84612f4e1abd3b9414c6699b11b0c9b
Simon Hofmann <simon.t.hofmann@tum.de>
2023-03-23 08:56:26 +0100

    cartesian to hexagonal mapping

---
+++ experiments/cartesian_to_hexagonal/cartesian_to_hexagonal.cpp
@@ -0,0 +1,257 @@
+#include "experiment_setup.hpp"
+
+#include <fiction/algorithms/physical_design/apply_gate_library.hpp>  // layout conversion to cell-level
+// #include <fiction/algorithms/physical_design/exact.hpp>               // SMT-based physical design of FCN layouts
+#include <fiction/algorithms/physical_design/orthogonal.hpp>
+#include <fiction/algorithms/properties/critical_path_length_and_throughput.hpp>  // critical path and throughput calculations
+#include <fiction/io/write_sqd_layout.hpp>                    // writer for SiQAD files (physical simulation)
+#include <fiction/networks/technology_network.hpp>            // technology-mapped network type
+#include <fiction/technology/area.hpp>                        // area requirement calculations
+#include <fiction/technology/cell_technologies.hpp>           // cell implementations
+// #include <fiction/technology/sidb_bestagon_library.hpp>       // a pre-defined SiDB gate library
+#include <fiction/technology/technology_mapping_library.hpp>  // pre-defined gate types for technology mapping
+#include <fiction/traits.hpp>                                 // traits for type-checking
+#include <fiction/types.hpp>                                  // pre-defined types suitable for the FCN domain
+
+#include <fmt/format.h>                                        // output formatting
+#include <lorina/genlib.hpp>                                   // Genlib file parsing
+#include <lorina/lorina.hpp>                                   // Verilog/BLIF/AIGER/... file parsing
+#include <mockturtle/algorithms/cut_rewriting.hpp>             // logic optimization with cut rewriting
+#include <mockturtle/algorithms/equivalence_checking.hpp>      // equivalence checking
+#include <mockturtle/algorithms/mapper.hpp>                    // Technology mapping on the logic level
+#include <mockturtle/algorithms/miter.hpp>                     // miter structure
+#include <mockturtle/algorithms/node_resynthesis/xag_npn.hpp>  // NPN databases for cut rewriting of XAGs and AIGs
+#include <mockturtle/io/genlib_reader.hpp>                     // call-backs to read Genlib files into gate libraries
+#include <mockturtle/io/verilog_reader.hpp>                    // call-backs to read Verilog files into networks
+#include <mockturtle/io/aiger_reader.hpp>
+#include <mockturtle/networks/klut.hpp>                        // k-LUT network
+#include <mockturtle/networks/xag.hpp>                         // XOR-AND-inverter graphs
+#include <mockturtle/networks/aig.hpp>
+#include <mockturtle/utils/tech_library.hpp>                   // technology library utils
+#include <mockturtle/views/depth_view.hpp>                     // to determine network levels
+
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+
+fiction::tile<fiction::gate_level_layout<fiction::clocked_layout<fiction::tile_based_layout<fiction::cartesian_layout<fiction::offset::ucoord_t>>>>> to_hex(int old_x, int old_y, int height, int z) {
+        int y = old_x + old_y;
+        int x = old_x + std::ceil(std::floor(height / 2) - y / 2);
+        fiction::tile<fiction::gate_level_layout<fiction::clocked_layout<fiction::tile_based_layout<fiction::cartesian_layout<fiction::offset::ucoord_t>>>>> hex{x, y, z};
+        return hex;
+    }
+
+int main()  // NOLINT
+{
+    using gate_lyt = fiction::gate_level_layout<fiction::clocked_layout<fiction::tile_based_layout<fiction::cartesian_layout<fiction::offset::ucoord_t>>>>;
+    using hex_lyt = fiction::hex_even_row_gate_clk_lyt;
+    using cell_lyt = fiction::sidb_cell_clk_lyt;
+
+    const std::string layouts_folder = fmt::format("{}/bestagon/layouts", EXPERIMENTS_PATH);
+
+    experiments::experiment<std::string, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t,
+                            uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint32_t, uint32_t, uint64_t, uint64_t, double, double, bool,
+                            uint64_t, double>
+        bestagon_exp{"cartesian_to_hexagonal",
+                     "benchmark",
+                     "inputs",
+                     "outputs",
+                     "initial nodes",
+                     "initial depth",
+                     "nodes after optimization",
+                     "depth after optimization",
+                     "nodes after mapping",
+                     "depth after mapping",
+                     "ortho layout width (in tiles)",
+                     "ortho layout height (in tiles)",
+                     "ortho layout area (in tiles)",
+                     "mapped layout width (in tiles)",
+                     "mapped layout height (in tiles)",
+                     "mapped layout area (in tiles)",
+                     "gates",
+                     "wires",
+                     "critical path",
+                     "throughput",
+                     "runtime ortho (in sec)",
+                     "runtime mapping (in sec)",
+                     "equivalent",
+                     "SiDB dots",
+                     "layout area in nm²"};
+
+    // instantiate a complete XAG NPN database for node re-synthesis
+    mockturtle::xag_npn_resynthesis<mockturtle::xag_network,                    // the input network type
+                                    mockturtle::xag_network,                    // the database network type
+                                    mockturtle::xag_npn_db_kind::xag_complete>  // the kind of database to use
+        resynthesis_function{};
+
+    // parameters for cut rewriting
+    mockturtle::cut_rewriting_params cut_params{};
+    cut_params.cut_enumeration_ps.cut_size = 4;
+
+    // instantiate a technology mapping library
+    std::stringstream library_stream{};
+    library_stream << fiction::GATE_ZERO << fiction::GATE_ONE << fiction::GATE_BUF << fiction::GATE_INV
+                   << fiction::GATE_AND2 << fiction::GATE_NAND2 << fiction::GATE_OR2 << fiction::GATE_NOR2
+                   << fiction::GATE_XOR2 << fiction::GATE_XNOR2;
+
+    std::vector<mockturtle::gate> gates{};
+
+    // parameters for technology mapping
+    mockturtle::map_params map_params{};
+
+    const auto read_genlib_result = lorina::read_genlib(library_stream, mockturtle::genlib_reader{gates});
+    assert(read_genlib_result == lorina::return_code::success);
+    mockturtle::tech_library<2> gate_lib{gates};
+
+    // parameters for SMT-based physical design
+    // fiction::orthogonal_physical_design_params<gate_lyt> orthogonal_params{};
+    fiction::orthogonal_physical_design_stats orthogonal_stats{};
+
+    static constexpr const uint64_t bench_select = fiction_experiments::all & ~fiction_experiments::log2 &
+                                                   ~fiction_experiments::sqrt & ~fiction_experiments::multiplier;
+
+    for (const auto& benchmark : fiction_experiments::all_benchmarks(bench_select))
+    {
+        fmt::print("[i] processing {}\n", benchmark);
+        mockturtle::xag_network xag{};
+
+        const auto read_verilog_result =
+            lorina::read_verilog(fiction_experiments::benchmark_path(benchmark), mockturtle::verilog_reader(xag));
+        assert(read_verilog_result == lorina::return_code::success);
+
+        // compute depth
+        mockturtle::depth_view depth_xag{xag};
+
+        // rewrite network cuts using the given re-synthesis function
+        const auto cut_xag = mockturtle::cut_rewriting(xag, resynthesis_function, cut_params);
+        // compute depth
+        mockturtle::depth_view depth_cut_xag{cut_xag};
+
+        // perform technology mapping
+        const auto mapped_network = mockturtle::map(cut_xag, gate_lib, map_params);
+        // compute depth
+        mockturtle::depth_view depth_mapped_network{mapped_network};
+
+        // perform layout generation with an SMT-based exact algorithm
+        const auto gate_level_layout = fiction::orthogonal<gate_lyt>(mapped_network, {}, &orthogonal_stats);
+
+        // compute critical path and throughput
+        fiction::critical_path_length_and_throughput_stats cp_tp_stats{};
+        fiction::critical_path_length_and_throughput(gate_level_layout, &cp_tp_stats);
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+        // get width & height of cartesian layout
+        int layout_width = gate_level_layout.x() + 1;
+        int layout_height = gate_level_layout.y() + 1;
+
+        // calculate max width & height of hexagonal layout
+        int hex_height = to_hex(layout_width - 1, layout_height - 1, layout_height, 0).y;
+        int hex_width = to_hex(layout_width - 1, 0, layout_height, 0).x;
+
+        // instantiate hexagonal layout
+        hex_lyt hex_layout{{hex_width, hex_height}, "ROW"};
+
+        // iterate through cartesian layout diagonally
+        for (int k = 0; k < layout_width + layout_height - 1; k++) {
+            for (int x = 0; x < k + 1; x++) {
+                int y = k - x;
+                if (y < layout_height && x < layout_width) {
+                    for (int z = 0; z < 2; z++) {
+                        fiction::tile<gate_lyt> node{x, y, z};                          // old coordinate
+                        fiction::tile<gate_lyt> hex{to_hex(x, y, layout_height, z)};    // new coordinate
+                        if (gate_level_layout.is_empty_tile(node)) {
+                            ;
+                        }
+                        else {
+                            if (gate_level_layout.is_pi_tile(node)) {
+                                hex_layout.create_pi(gate_level_layout.get_input_name(gate_level_layout.get_node({x, y, z})), hex);
+                            }
+                            else if (gate_level_layout.is_po_tile(node)) {
+                                hex_layout.create_po(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 gate_level_layout.get_output_name(gate_level_layout.get_node(node)), hex);
+                            }
+                            else if (gate_level_layout.is_wire(gate_level_layout.get_node(node))) {
+                                hex_layout.create_buf(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex);
+                            }
+                            else if (gate_level_layout.is_inv(gate_level_layout.get_node(node))) {
+                                hex_layout.create_not(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex);
+                            }
+                            else if (gate_level_layout.is_and(gate_level_layout.get_node(node))) {
+                                hex_layout.create_and(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                            else if (gate_level_layout.is_nand(gate_level_layout.get_node(node))) {
+                                hex_layout.create_nand(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                            else if (gate_level_layout.is_or(gate_level_layout.get_node(node))) {
+                                hex_layout.create_or(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                            else if (gate_level_layout.is_nor(gate_level_layout.get_node(node))) {
+                                hex_layout.create_nor(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                            else if (gate_level_layout.is_xor(gate_level_layout.get_node(node))) {
+                                hex_layout.create_xor(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                            else if (gate_level_layout.is_xnor(gate_level_layout.get_node(node))) {
+                                hex_layout.create_xnor(hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[0].x, gate_level_layout.incoming_data_flow(node)[0].y, layout_height, gate_level_layout.incoming_data_flow(node)[0].z))),
+                                 hex_layout.make_signal(hex_layout.get_node(to_hex(gate_level_layout.incoming_data_flow(node)[1].x, gate_level_layout.incoming_data_flow(node)[1].y, layout_height, gate_level_layout.incoming_data_flow(node)[1].z))),
+                                  hex);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+        // check equivalence
+        // const auto miter = mockturtle::miter<fiction::technology_network>(mapped_network, hex_layout);
+        // const auto eq    = mockturtle::equivalence_checking(*miter);
+        // assert(eq.has_value());
+
+        // apply gate library
+        // const auto cell_level_layout = fiction::apply_gate_library<cell_lyt, fiction::sidb_bestagon_library>(hex_layout);
+
+        // compute area
+        // fiction::area_stats                            area_stats{};
+        // fiction::area_params<fiction::sidb_technology> area_ps{};
+        // fiction::area(cell_level_layout, area_ps, &area_stats);
+
+
+        // log results
+        bestagon_exp(benchmark, xag.num_pis(), xag.num_pos(), xag.num_gates(), depth_xag.depth(),
+                     cut_xag.num_gates(), depth_cut_xag.depth(), mapped_network.num_gates(),
+                     depth_mapped_network.depth(), gate_level_layout.x() + 1, gate_level_layout.y() + 1,
+                     (gate_level_layout.x() + 1) * (gate_level_layout.y() + 1), hex_layout.x() + 1, hex_layout.y() + 1,
+                     (hex_layout.x() + 1) * (hex_layout.y() + 1),gate_level_layout.num_gates(),
+                     gate_level_layout.num_wires(), cp_tp_stats.critical_path_length, cp_tp_stats.throughput,
+                     mockturtle::to_seconds(orthogonal_stats.time_total),
+                     std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() /1000000.0, 0,
+                     0, 0);
+
+        bestagon_exp.save();
+        bestagon_exp.table();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+
---
+++ experiments/cartesian_to_hexagonal/experiment_setup.hpp
@@ -0,0 +1,189 @@
+//
+// Created by marcel on 16.11.21.
+//
+
+#ifndef FICTION_EXPERIMENT_SETUP_HPP
+#define FICTION_EXPERIMENT_SETUP_HPP
+
+#include <fmt/format.h>
+
+#include <array>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include <experiments.hpp>  // mockturtle's experiments setup
+
+namespace fiction_experiments
+{
+
+// NOLINTBEGIN(readability-identifier-naming)
+// clang-format off
+
+/* Trindade et al. 2016 benchmarks */
+inline constexpr uint64_t mux21            = 0b0000000000000000000000000000000000000000000000000000000000000001;
+inline constexpr uint64_t xor2_t           = 0b0000000000000000000000000000000000000000000000000000000000000010;
+inline constexpr uint64_t xnor2            = 0b0000000000000000000000000000000000000000000000000000000000000100;
+inline constexpr uint64_t par_gen          = 0b0000000000000000000000000000000000000000000000000000000000001000;
+inline constexpr uint64_t half_adder       = 0b0000000000000000000000000000000000000000000000000000000000010000;
+inline constexpr uint64_t full_adder       = 0b0000000000000000000000000000000000000000000000000000000000100000;
+inline constexpr uint64_t par_check        = 0b0000000000000000000000000000000000000000000000000000000001000000;
+inline constexpr uint64_t trindade16       = 0b0000000000000000000000000000000000000000000000000000000001111111;
+
+/* Fontes et al. 2018 benchmarks */
+inline constexpr uint64_t xor2_f           = 0b0000000000000000000000000000000000000000000000000000000010000000;
+inline constexpr uint64_t one_bit_add_aoig = 0b0000000000000000000000000000000000000000000000000000000100000000;
+inline constexpr uint64_t t                = 0b0000000000000000000000000000000000000000000000000000001000000000;
+inline constexpr uint64_t t_5              = 0b0000000000000000000000000000000000000000000000000000010000000000;
+inline constexpr uint64_t c17              = 0b0000000000000000000000000000000000000000000000000000100000000000;
+inline constexpr uint64_t b1_r2            = 0b0000000000000000000000000000000000000000000000000001000000000000;
+inline constexpr uint64_t majority         = 0b0000000000000000000000000000000000000000000000000010000000000000;
+inline constexpr uint64_t majority_5_r1    = 0b0000000000000000000000000000000000000000000000000100000000000000;
+inline constexpr uint64_t newtag           = 0b0000000000000000000000000000000000000000000000001000000000000000;
+inline constexpr uint64_t clpl             = 0b0000000000000000000000000000000000000000000000010000000000000000;
+inline constexpr uint64_t xor5_r1          = 0b0000000000000000000000000000000000000000000000100000000000000000;
+inline constexpr uint64_t one_bit_add_maj  = 0b0000000000000000000000000000000000000000000001000000000000000000;
+inline constexpr uint64_t cm82a_5          = 0b0000000000000000000000000000000000000000000010000000000000000000;
+inline constexpr uint64_t two_bit_add_maj  = 0b0000000000000000000000000000000000000000000100000000000000000000;
+inline constexpr uint64_t xor5_maj         = 0b0000000000000000000000000000000000000000001000000000000000000000;
+inline constexpr uint64_t parity           = 0b0000000000000000000000000000000000000000010000000000000000000000;
+inline constexpr uint64_t fontes18         = 0b0000000000000000000000000000000000000000011111111111111110000000;
+
+/* ISCAS benchmarks */
+inline constexpr uint64_t c17_i            = 0b0000000000000000000000000000000000000000100000000000000000000000;
+inline constexpr uint64_t c432             = 0b0000000000000000000000000000000000000001000000000000000000000000;
+inline constexpr uint64_t c499             = 0b0000000000000000000000000000000000000010000000000000000000000000;
+inline constexpr uint64_t c880             = 0b0000000000000000000000000000000000000100000000000000000000000000;
+inline constexpr uint64_t c1355            = 0b0000000000000000000000000000000000001000000000000000000000000000;
+inline constexpr uint64_t c1908            = 0b0000000000000000000000000000000000010000000000000000000000000000;
+inline constexpr uint64_t c2670            = 0b0000000000000000000000000000000000100000000000000000000000000000;
+inline constexpr uint64_t c3540            = 0b0000000000000000000000000000000001000000000000000000000000000000;
+inline constexpr uint64_t c5315            = 0b0000000000000000000000000000000010000000000000000000000000000000;
+inline constexpr uint64_t c6288            = 0b0000000000000000000000000000000100000000000000000000000000000000;
+inline constexpr uint64_t c7552            = 0b0000000000000000000000000000001000000000000000000000000000000000;
+inline constexpr uint64_t iscas85          = 0b0000000000000000000000000000001111111111100000000000000000000000;
+
+/* EPFL benchmarks */
+inline constexpr uint64_t adder            = 0b0000000000000000000000000000010000000000000000000000000000000000;
+inline constexpr uint64_t bar              = 0b0000000000000000000000000000100000000000000000000000000000000000;
+inline constexpr uint64_t max              = 0b0000000000000000000000000001000000000000000000000000000000000000;
+inline constexpr uint64_t sin              = 0b0000000000000000000000000010000000000000000000000000000000000000;
+inline constexpr uint64_t cavlc            = 0b0000000000000000000000000100000000000000000000000000000000000000;
+inline constexpr uint64_t ctrl             = 0b0000000000000000000000001000000000000000000000000000000000000000;
+inline constexpr uint64_t dec              = 0b0000000000000000000000010000000000000000000000000000000000000000;
+inline constexpr uint64_t i2c              = 0b0000000000000000000000100000000000000000000000000000000000000000;
+inline constexpr uint64_t int2float        = 0b0000000000000000000001000000000000000000000000000000000000000000;
+inline constexpr uint64_t priority         = 0b0000000000000000000010000000000000000000000000000000000000000000;
+inline constexpr uint64_t router           = 0b0000000000000000000100000000000000000000000000000000000000000000;
+inline constexpr uint64_t arbiter          = 0b0000000000000000001000000000000000000000000000000000000000000000;
+inline constexpr uint64_t voter            = 0b0000000000000000010000000000000000000000000000000000000000000000;
+inline constexpr uint64_t square           = 0b0000000000000000100000000000000000000000000000000000000000000000;
+inline constexpr uint64_t log2             = 0b0000000000000001000000000000000000000000000000000000000000000000;
+inline constexpr uint64_t sqrt             = 0b0000000000000010000000000000000000000000000000000000000000000000;
+inline constexpr uint64_t multiplier       = 0b0000000000000100000000000000000000000000000000000000000000000000;
+inline constexpr uint64_t epfl             = 0b0000000000000111111111111111110000000000000000000000000000000000;
+
+inline constexpr uint64_t all              = 0b000000000000011111111111111111111111111111111111111111111111111;
+
+/* extra functions */
+
+
+static constexpr const std::array benchmarks {
+    "trindade16/mux21", "trindade16/xor2", "trindade16/xnor2", "trindade16/par_gen",
+    "trindade16/HA", "trindade16/FA", "trindade16/par_check",
+
+    "fontes18/xor", "fontes18/1bitAdderAOIG", "fontes18/t", "fontes18/t_5", "fontes18/c17", "fontes18/b1_r2",
+    "fontes18/majority", "fontes18/majority_5_r1", "fontes18/newtag", "fontes18/clpl", "fontes18/xor5_r1",
+    "fontes18/1bitAdderMaj", "fontes18/cm82a_5", "fontes18/2bitAdderMaj", "fontes18/xor5Maj", "fontes18/parity",
+
+    "iscas85/c17", "iscas85/c432", "iscas85/c499", "iscas85/c880", "iscas85/c1355", "iscas85/c1908", "iscas85/c2670",
+    "iscas85/c3540", "iscas85/c5315", "iscas85/c6288", "iscas85/c7552",
+
+    "epfl/adder", "epfl/bar", "epfl/max", "epfl/sin", "epfl/cavlc", "epfl/ctrl", "epfl/dec", "epfl/i2c",
+    "epfl/int2float", "epfl/priority", "epfl/router", "epfl/arbiter", "epfl/voter", "epfl/square", "epfl/log2",
+    "epfl/sqrt", "epfl/multiplier",
+    };
+
+// clang-format on
+// NOLINTEND(readability-identifier-naming)
+
+std::vector<std::string> trindade16_benchmarks(uint64_t selection = trindade16)
+{
+    std::vector<std::string> result{};
+    for (uint64_t i = 0u; i < 7u; ++i)
+    {
+        if (((selection >> i) & 1u) != 0u)
+        {
+            result.emplace_back(benchmarks.at(i));
+        }
+    }
+
+    return result;
+}
+
+std::vector<std::string> fontes18_benchmarks(uint64_t selection = fontes18)
+{
+    std::vector<std::string> result{};
+    for (uint64_t i = 7u; i < 23u; ++i)
+    {
+        if (((selection >> i) & 1u) != 0u)
+        {
+            result.emplace_back(benchmarks.at(i));
+        }
+    }
+
+    return result;
+}
+
+std::vector<std::string> iscas85_benchmarks(uint64_t selection = iscas85)
+{
+    std::vector<std::string> result{};
+    for (uint64_t i = 23u; i < 34u; ++i)
+    {
+        if (((selection >> i) & 1u) != 0u)
+        {
+            result.emplace_back(benchmarks.at(i));
+        }
+    }
+
+    return result;
+}
+
+std::vector<std::string> epfl_benchmarks(uint64_t selection = epfl)
+{
+    std::vector<std::string> result{};
+    for (uint64_t i = 34u; i < 51u; ++i)
+    {
+        if (((selection >> i) & 1u) != 0u)
+        {
+            result.emplace_back(benchmarks.at(i));
+        }
+    }
+
+    return result;
+}
+
+
+
+std::vector<std::string> all_benchmarks(uint64_t selection = all)
+{
+    std::vector<std::string> result{};
+    for (uint64_t i = 0u; i < 51u; ++i)
+    {
+        if (((selection >> i) & 1u) != 0u)
+        {
+            result.emplace_back(benchmarks.at(i));
+        }
+    }
+
+    return result;
+}
+
+std::string benchmark_path(const std::string& benchmark_name, const std::string& benchmark_folder = "../benchmarks")
+{
+    return fmt::format("{}{}/{}.v", EXPERIMENTS_PATH, benchmark_folder, benchmark_name);
+}
+
+}  // namespace fiction_experiments
+
+#endif  // FICTION_EXPERIMENT_SETUP_HPP
