3c4ece54be2580ae78d61c5cf0cd33144d8e1924
hibenj <benjamin.hien@web.de>
2023-04-11 17:48:32 +0200

    Input_Sort updated

--- test/algorithms/physical_design/ortho_ordering_network.cpp
+++ test/algorithms/physical_design/ortho_ordering_network.cpp
@@ -1,3 +1,14 @@
 //
 // Created by benjamin on 11.04.23.
 //
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <fiction/algorithms/physical_design/ortho_ordering_network.hpp>
+
+#include <iostream>
+
+TEST_CASE("TEST", "[ordering]")
+{
+    std::cout<<"Ausgabe: "<<"fkt"<<std::endl;
+}
