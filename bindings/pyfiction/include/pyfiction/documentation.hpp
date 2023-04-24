524872685ded2d03db4bcda6a443f91df05bf36d
Simon Hofmann <simon.t.hofmann@tum.de>
2023-04-24 16:12:15 +0200

    fix documentation for cartesian layout

--- bindings/pyfiction/include/pyfiction/documentation.hpp
+++ bindings/pyfiction/include/pyfiction/documentation.hpp
@@ -3,4 +3,28 @@
 
 #include "pyfiction/pybind11_mkdoc_docstrings.hpp"
 
+static const char *__doc_fiction_cartesian_layout_overridden =
+R"doc(A layout type that utilizes offset coordinates to represent a
+Cartesian grid. Its faces are organized in the following way:
+
+.. code-block:: text
+
+    +-------+-------+-------+-------+
+    |       |       |       |       |
+    | (0,0) | (1,0) | (2,0) | (3,0) |
+    |       |       |       |       |
+    +-------+-------+-------+-------+
+    |       |       |       |       |
+    | (0,1) | (1,1) | (2,1) | (3,1) |
+    |       |       |       |       |
+    +-------+-------+-------+-------+
+    |       |       |       |       |
+    | (0,2) | (1,2) | (2,2) | (3,2) |
+    |       |       |       |       |
+    +-------+-------+-------+-------+
+
+Template parameter ``OffsetCoordinateType``:
+    The coordinate implementation to be used. Offset coordinates are
+    required.)doc";
+
 #endif  // FICTION_DOCSTRINGS_HPP
