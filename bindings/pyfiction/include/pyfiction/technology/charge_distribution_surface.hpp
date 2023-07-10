443c616d0c26f1299edee1447e722be986b3ba25
Marcel Walter <marcel.walter@tum.de>
2023-07-11 00:15:11 +0200

    :sparkles: Added additionally cell-level layout features to pyfiction's charge_distribution_surface

--- bindings/pyfiction/include/pyfiction/technology/charge_distribution_surface.hpp
+++ bindings/pyfiction/include/pyfiction/technology/charge_distribution_surface.hpp
@@ -79,6 +79,56 @@ inline void charge_distribution_surface(pybind11::module& m)
             [](py_charge_distribution_surface& lyt, const py_siqad_coordinate& dimension) { lyt.resize(dimension); },
             "dimension"_a, DOC(fiction_cartesian_layout_resize))
 
+        .def(
+            "get_cell_type",
+            [](const py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c)
+            { return lyt.get_cell_type(c); },
+            "c"_a, DOC(fiction_cell_level_layout_get_cell_type))
+        .def(
+            "is_empty_cell",
+            [](const py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c)
+            { return lyt.is_empty_cell(c); },
+            "c"_a, DOC(fiction_cell_level_layout_is_empty_cell))
+        .def(
+            "assign_cell_name",
+            [](py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c,
+               const std::string& n) { lyt.assign_cell_name(c, n); },
+            "c"_a, "n"_a, DOC(fiction_cell_level_layout_assign_cell_name))
+        .def(
+            "get_cell_name",
+            [](const py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c)
+            { return lyt.get_cell_name(c); },
+            "c"_a, DOC(fiction_cell_level_layout_get_cell_name))
+        .def(
+            "set_layout_name",
+            [](py_charge_distribution_surface& lyt, const std::string& name) { lyt.set_layout_name(name); }, "name"_a,
+            DOC(fiction_cell_level_layout_set_layout_name))
+        .def(
+            "get_layout_name", [](const py_charge_distribution_surface& lyt) { return lyt.get_layout_name(); },
+            DOC(fiction_cell_level_layout_get_layout_name))
+        .def(
+            "num_cells", [](const py_charge_distribution_surface& lyt) { return lyt.num_cells(); },
+            DOC(fiction_cell_level_layout_num_cells))
+        .def(
+            "is_empty", [](const py_charge_distribution_surface& lyt) { return lyt.is_empty(); },
+            DOC(fiction_cell_level_layout_is_empty))
+        .def(
+            "num_pis", [](const py_charge_distribution_surface& lyt) { return lyt.num_pis(); },
+            DOC(fiction_cell_level_layout_num_pis))
+        .def(
+            "num_pos", [](const py_charge_distribution_surface& lyt) { return lyt.num_pos(); },
+            DOC(fiction_cell_level_layout_num_pos))
+        .def(
+            "is_pi",
+            [](const py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c)
+            { return lyt.is_pi(c); },
+            "c"_a, DOC(fiction_cell_level_layout_is_pi))
+        .def(
+            "is_po",
+            [](const py_charge_distribution_surface& lyt, const fiction::cell<py_charge_distribution_surface>& c)
+            { return lyt.is_po(c); },
+            "c"_a, DOC(fiction_cell_level_layout_is_po))
+
         .def("set_physical_parameters", &py_charge_distribution_surface::set_physical_parameters, "params"_a)
         .def("get_phys_params", &py_charge_distribution_surface::get_phys_params)
         .def("get_all_sidb_locations_in_nm", &py_charge_distribution_surface::get_all_sidb_locations_in_nm)
@@ -135,6 +185,31 @@ inline void charge_distribution_surface(pybind11::module& m)
 
         .def("adjacent_search", &py_charge_distribution_surface::adjacent_search, "alpha"_a, "negative_indices"_a)
 
+        .def("cells",
+             [](const py_charge_distribution_surface& lyt)
+             {
+                 std::vector<fiction::coordinate<py_charge_distribution_surface>> cells{};
+                 cells.reserve(lyt.num_cells());
+                 lyt.foreach_cell([&cells](const auto& c) { cells.push_back(c); });
+                 return cells;
+             })
+        .def("pis",
+             [](const py_charge_distribution_surface& lyt)
+             {
+                 std::vector<fiction::coordinate<py_charge_distribution_surface>> pis{};
+                 pis.reserve(lyt.num_pis());
+                 lyt.foreach_pi([&pis](const auto& c) { pis.push_back(c); });
+                 return pis;
+             })
+        .def("pos",
+             [](const py_charge_distribution_surface& lyt)
+             {
+                 std::vector<fiction::coordinate<py_charge_distribution_surface>> pos{};
+                 pos.reserve(lyt.num_pos());
+                 lyt.foreach_po([&pos](const auto& c) { pos.push_back(c); });
+                 return pos;
+             })
+
         .def(
             "is_within_bounds",
             [](const py_charge_distribution_surface& lyt, const py_siqad_coordinate& c)
@@ -142,15 +217,13 @@ inline void charge_distribution_surface(pybind11::module& m)
             "c"_a, DOC(fiction_cartesian_layout_is_within_bounds))
 
         .def(
-            "coordinates",
+            "bounding_box_2d",
             [](const py_charge_distribution_surface& lyt)
             {
-                std::vector<fiction::coordinate<py_charge_distribution_surface>> coords{};
-                coords.reserve(lyt.area() * (lyt.z() + 1));
-                lyt.foreach_coordinate([&coords](const auto& c) { coords.push_back(c); });
-                return coords;
+                const auto bb = fiction::bounding_box_2d<py_charge_distribution_surface>(lyt);
+                return std::make_pair(bb.get_min(), bb.get_max());
             },
-            DOC(fiction_cartesian_layout_coordinates))
+            DOC(fiction_bounding_box_2d_overridden))
 
         ;
 }
