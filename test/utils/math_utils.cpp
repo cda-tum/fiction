2c1d62131115c18466c8d185c10dddca85b47327
Jan Drewniok <97012901+Drewniok@users.noreply.github.com>
2023-07-26 10:48:29 +0200

    ➖  Remove ``unit`` library due to increasing simulation runtimes.  (#258)
    * :heavy_minus_sign: Remove ``unit`` library due to increasing simulation runtimes.
    
    * :art: implement review suggestions.
    
    * :white_check_mark: add a few more tests.
    
    * :white_check_mark: add a few more tests.

--- test/utils/math_utils.cpp
+++ test/utils/math_utils.cpp
@@ -3,12 +3,8 @@
 //
 
 #include <catch2/catch_template_test_macros.hpp>
-#include <catch2/catch_test_macros.hpp>
 
 #include <fiction/utils/math_utils.hpp>
-
-#include <cstdint>
-#include <limits>
 
 using namespace fiction;
 
@@ -50,74 +46,6 @@ TEST_CASE("round_to_n_decimal_places should round an input number to n decimal p
         CHECK(round_to_n_decimal_places(0.0000001, 6) == 0);
         CHECK(round_to_n_decimal_places(-0.000001, 6) == -0.000001);
         CHECK(round_to_n_decimal_places(-0.0000001, 6) == 0);
-    }
-
-    SECTION("Round Kelvin")
-    {
-        const auto value_positive = 3.145926_K;
-        CHECK(round_to_n_decimal_places(value_positive, 0) == 3_K);
-        CHECK(round_to_n_decimal_places(value_positive, 1) == 3.1_K);
-        CHECK(round_to_n_decimal_places(value_positive, 2) == 3.15_K);
-        CHECK(round_to_n_decimal_places(value_positive, 3) == 3.146_K);
-        CHECK(round_to_n_decimal_places(value_positive, 4) == 3.1459_K);
-        CHECK(round_to_n_decimal_places(value_positive, 5) == 3.14593_K);
-        CHECK(round_to_n_decimal_places(value_positive, 6) == 3.145926_K);
-        CHECK(round_to_n_decimal_places(1.005_K, 2) == 1.0_K);
-        CHECK(round_to_n_decimal_places(0.000001_K, 6) == 0.000001_K);
-        CHECK(round_to_n_decimal_places(0.0000001_K, 6) == 0_K);
-        CHECK(round_to_n_decimal_places(-0.000001_K, 6) == -0.000001_K);
-        CHECK(round_to_n_decimal_places(-0.0000001_K, 6) == 0_K);
-    }
-
-    SECTION("Round length")
-    {
-        const auto value_positive = 3.145926_nm;
-        CHECK(round_to_n_decimal_places(value_positive, 0) == 3_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 1) == 3.1_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 2) == 3.15_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 3) == 3.146_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 4) == 3.1459_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 5) == 3.14593_nm);
-        CHECK(round_to_n_decimal_places(value_positive, 6) == 3.145926_nm);
-        CHECK(round_to_n_decimal_places(1.005_nm, 2) == 1.0_nm);
-        CHECK(round_to_n_decimal_places(0.000001_nm, 6) == 0.000001_nm);
-        CHECK(round_to_n_decimal_places(0.0000001_nm, 6) == 0_nm);
-        CHECK(round_to_n_decimal_places(-0.000001_nm, 6) == -0.000001_nm);
-        CHECK(round_to_n_decimal_places(-0.0000001_nm, 6) == 0_nm);
-    }
-
-    SECTION("Round voltage")
-    {
-        const auto value_positive = 3.145926_V;
-        CHECK(round_to_n_decimal_places(value_positive, 0) == 3_V);
-        CHECK(round_to_n_decimal_places(value_positive, 1) == 3.1_V);
-        CHECK(round_to_n_decimal_places(value_positive, 2) == 3.15_V);
-        CHECK(round_to_n_decimal_places(value_positive, 3) == 3.146_V);
-        CHECK(round_to_n_decimal_places(value_positive, 4) == 3.1459_V);
-        CHECK(round_to_n_decimal_places(value_positive, 5) == 3.14593_V);
-        CHECK(round_to_n_decimal_places(value_positive, 6) == 3.145926_V);
-        CHECK(round_to_n_decimal_places(1.005_V, 2) == 1.0_V);
-        CHECK(round_to_n_decimal_places(0.000001_V, 6) == 0.000001_V);
-        CHECK(round_to_n_decimal_places(0.0000001_V, 6) == 0_V);
-        CHECK(round_to_n_decimal_places(-0.000001_V, 6) == -0.000001_V);
-        CHECK(round_to_n_decimal_places(-0.0000001_V, 6) == 0_V);
-    }
-
-    SECTION("Round energy")
-    {
-        const auto value_positive = 3.145926_eV;
-        CHECK(round_to_n_decimal_places(value_positive, 0) == 3_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 1) == 3.1_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 2) == 3.15_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 3) == 3.146_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 4) == 3.1459_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 5) == 3.14593_eV);
-        CHECK(round_to_n_decimal_places(value_positive, 6) == 3.145926_eV);
-        CHECK(round_to_n_decimal_places(1.005_eV, 2) == 1.0_eV);
-        CHECK(round_to_n_decimal_places(0.000001_eV, 6) == 0.000001_eV);
-        CHECK(round_to_n_decimal_places(0.0000001_eV, 6) == 0_eV);
-        CHECK(round_to_n_decimal_places(-0.000001_eV, 6) == -0.000001_eV);
-        CHECK(round_to_n_decimal_places(-0.0000001_eV, 6) == 0_eV);
     }
 }
 
