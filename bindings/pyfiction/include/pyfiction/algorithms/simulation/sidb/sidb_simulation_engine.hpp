240bafc5876d25c94cf7ae823f3f66213521d268
Marcel Walter <marcel.walter@tum.de>
2023-11-21 15:54:27 +0100

    :memo: Added missing docstrings

--- bindings/pyfiction/include/pyfiction/algorithms/simulation/sidb/sidb_simulation_engine.hpp
+++ bindings/pyfiction/include/pyfiction/algorithms/simulation/sidb/sidb_simulation_engine.hpp
@@ -18,23 +18,20 @@ inline void sidb_simulation_engine(pybind11::module& m)
 {
     namespace py = pybind11;
 
-    py::enum_<fiction::sidb_simulation_engine>(m, "sidb_simulation_engine")
-        //, DOC(fiction_sidb_simulation_engine))
-        .value("EXGS", fiction::sidb_simulation_engine::EXGS)
-        //, DOC(fiction_sidb_simulation_engine_EXGS))
-        .value("QUICKSIM", fiction::sidb_simulation_engine::QUICKSIM)
-        //, DOC(fiction_sidb_simulation_engine_QUICKSIM))
-        .value("QUICKEXACT", fiction::sidb_simulation_engine::QUICKEXACT)
-        //, DOC(fiction_sidb_simulation_engine_QUICKEXACT))
+    py::enum_<fiction::sidb_simulation_engine>(m, "sidb_simulation_engine", DOC(fiction_sidb_simulation_engine))
+        .value("EXGS", fiction::sidb_simulation_engine::EXGS, DOC(fiction_sidb_simulation_engine_EXGS))
+        .value("QUICKSIM", fiction::sidb_simulation_engine::QUICKSIM, DOC(fiction_sidb_simulation_engine_QUICKSIM))
+        .value("QUICKEXACT", fiction::sidb_simulation_engine::QUICKEXACT,
+               DOC(fiction_sidb_simulation_engine_QUICKEXACT))
 
         ;
 
-    py::enum_<fiction::exhaustive_sidb_simulation_engine>(m, "exhaustive_sidb_simulation_engine")
-        //, DOC(fiction_exhaustive_sidb_simulation_engine))
-        .value("EXGS", fiction::exhaustive_sidb_simulation_engine::EXGS)
-        //, DOC(fiction_exhaustive_sidb_simulation_engine_EXGS))
-        .value("QUICKEXACT", fiction::exhaustive_sidb_simulation_engine::QUICKEXACT)
-        //, DOC(fiction_exhaustive_sidb_simulation_engine_QUICKEXACT))
+    py::enum_<fiction::exhaustive_sidb_simulation_engine>(m, "exhaustive_sidb_simulation_engine",
+                                                          DOC(fiction_exhaustive_sidb_simulation_engine))
+        .value("EXGS", fiction::exhaustive_sidb_simulation_engine::EXGS,
+               DOC(fiction_exhaustive_sidb_simulation_engine_EXGS))
+        .value("QUICKEXACT", fiction::exhaustive_sidb_simulation_engine::QUICKEXACT,
+               DOC(fiction_exhaustive_sidb_simulation_engine_QUICKEXACT))
 
         ;
 }
