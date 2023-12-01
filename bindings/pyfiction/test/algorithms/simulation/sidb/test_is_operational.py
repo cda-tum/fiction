bc523d8a027b986233e16114fb836c6fa93b5f4c
Drewniok <jan.drewniok.research@gmail.com>
2023-12-01 15:04:32 +0100

    :bug: convert siqad to offset coordinates.

--- bindings/pyfiction/test/algorithms/simulation/sidb/test_is_operational.py
+++ bindings/pyfiction/test/algorithms/simulation/sidb/test_is_operational.py
@@ -6,26 +6,26 @@ import os
 class TestIsOperational(unittest.TestCase):
 
     def test_is_operational(self):
-        lyt = sidb_layout((7, 0))
+        lyt = sidb_layout()
 
         lyt = charge_distribution_surface(lyt)
 
-        lyt.assign_cell_type((0, 0, 1), sidb_technology.cell_type.INPUT)
-        lyt.assign_cell_type((2, 1, 1), sidb_technology.cell_type.INPUT)
+        lyt.assign_cell_type((0, 1), sidb_technology.cell_type.INPUT)
+        lyt.assign_cell_type((2, 3), sidb_technology.cell_type.INPUT)
 
-        lyt.assign_cell_type((20, 0, 1), sidb_technology.cell_type.INPUT)
-        lyt.assign_cell_type((19, 1, 1), sidb_technology.cell_type.INPUT)
+        lyt.assign_cell_type((20, 1), sidb_technology.cell_type.INPUT)
+        lyt.assign_cell_type((19, 3), sidb_technology.cell_type.INPUT)
 
-        lyt.assign_cell_type((4, 2, 1), sidb_technology.cell_type.NORMAL)
-        lyt.assign_cell_type((6, 3, 1), sidb_technology.cell_type.NORMAL)
+        lyt.assign_cell_type((4, 5), sidb_technology.cell_type.NORMAL)
+        lyt.assign_cell_type((6, 7), sidb_technology.cell_type.NORMAL)
 
-        lyt.assign_cell_type((14, 3, 1), sidb_technology.cell_type.NORMAL)
-        lyt.assign_cell_type((16, 2, 1), sidb_technology.cell_type.NORMAL)
+        lyt.assign_cell_type((14, 7), sidb_technology.cell_type.NORMAL)
+        lyt.assign_cell_type((16, 5), sidb_technology.cell_type.NORMAL)
 
-        lyt.assign_cell_type((10, 6, 0), sidb_technology.cell_type.OUTPUT)
-        lyt.assign_cell_type((10, 7, 0), sidb_technology.cell_type.OUTPUT)
+        lyt.assign_cell_type((10, 12, 0), sidb_technology.cell_type.OUTPUT)
+        lyt.assign_cell_type((10, 14, 0), sidb_technology.cell_type.OUTPUT)
 
-        lyt.assign_cell_type((10, 9, 1), sidb_technology.cell_type.NORMAL)
+        lyt.assign_cell_type((10, 19), sidb_technology.cell_type.NORMAL)
 
         params = is_operational_params()
         params.simulation_parameters = sidb_simulation_parameters(2, -0.28)
