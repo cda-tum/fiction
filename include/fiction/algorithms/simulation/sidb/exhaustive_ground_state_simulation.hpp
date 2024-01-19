689c7bd05981c0b954fc92b99852dacf6da98517
ClangFormat <ClangFormat@users.noreply.github.com>
2024-01-19 14:39:25 +0000

    :art: ClangFormat changes
    Signed-off-by: ClangFormat <ClangFormat@users.noreply.github.com>

--- include/fiction/algorithms/simulation/sidb/exhaustive_ground_state_simulation.hpp
+++ include/fiction/algorithms/simulation/sidb/exhaustive_ground_state_simulation.hpp
@@ -46,7 +46,7 @@ exhaustive_ground_state_simulation(const Lyt&                        lyt,
     static_assert(has_sidb_technology_v<Lyt>, "Lyt is not an SiDB layout");
 
     sidb_simulation_result<Lyt> simulation_result{};
-    simulation_result.algorithm_name      = "ExGS";
+    simulation_result.algorithm_name        = "ExGS";
     simulation_result.simulation_parameters = params;
     mockturtle::stopwatch<>::duration time_counter{};
     {
