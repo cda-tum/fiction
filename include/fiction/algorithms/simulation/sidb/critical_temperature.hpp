8a633696c758c6589d57a8ce52943502a0155991
Drewniok <jan.drewniok.research@gmail.com>
2024-01-31 17:34:05 +0100

    :art: add runtime.

--- include/fiction/algorithms/simulation/sidb/critical_temperature.hpp
+++ include/fiction/algorithms/simulation/sidb/critical_temperature.hpp
@@ -176,6 +176,7 @@ class critical_temperature_impl
     template <typename TT>
     void gate_based_simulation(const std::vector<TT>& spec) noexcept
     {
+        mockturtle::stopwatch stop{stats.time_total};
         if (layout.is_empty())
         {
             stats.critical_temperature = 0.0;
@@ -238,6 +239,7 @@ class critical_temperature_impl
      */
     void non_gate_based_simulation() noexcept
     {
+        mockturtle::stopwatch stop{stats.time_total};
         sidb_simulation_result<Lyt> simulation_results{};
 
         if (params.engine == critical_temperature_params::simulation_engine::EXACT)
@@ -468,9 +470,9 @@ double critical_temperature_gate_based(const Lyt& lyt, const std::vector<TT>& sp
 
     assert(!spec.empty());
     // all elements in tts must have the same number of variables
-    assert(std::adjacent_find(spec.cbegin(), spec.cend(), [
-                              (const auto& a, const auto& b)
-                              { return a.num_vars() != b.num_vars(); }) == spec.cend());
+    assert(std::adjacent_find(
+               spec.cbegin(), spec.cend(), (const auto& a, const auto& b) { return a.num_vars() != b.num_vars(); }) ==
+           spec.cend());
 
     critical_temperature_stats<Lyt> st{};
 
