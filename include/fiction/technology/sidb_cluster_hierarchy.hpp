af61160770233a73e2ffe47b04b9c00cd5391d38
Willem Lambooy <willem.lambooy@gmail.com>
2024-02-04 13:36:05 +0100

    :sparkles: Hierarchical SiDB clustering using ALGLIB

---
+++ include/fiction/technology/sidb_cluster_hierarchy.hpp
@@ -0,0 +1,128 @@
+//
+// Created by Willem Lambooy on 04/02/2024.
+//
+
+#ifndef FICTION_SIDB_CLUSTER_HIERARCHY_HPP
+#define FICTION_SIDB_CLUSTER_HIERARCHY_HPP
+
+#include "fiction/technology/charge_distribution_surface.hpp"
+
+#include <array>
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <set>
+
+#include <dataanalysis.h>
+#include <linalg.h>
+
+namespace fiction
+{
+
+/**
+ * An enumeration of cluster linkage methods. The chosen method defines how clusters are merged in the agglomerative
+ * clustering procedure, by, e.g., defining an inter-cluster distance to minimize for the cluster to merge.
+ */
+enum class sidb_cluster_hierarchy_linkage_method
+{
+    /*
+     * Complete linkage takes the maximum distance between nodes in a cluster.
+     */
+    COMPLETE = 0,
+    /*
+     * Single linkage takes the minimum distance between nodes in a cluster.
+     */
+    SINGLE,
+    /*
+     * Unweighted average linkage takes the average distance between nodes in a cluster, disregarding the number of
+     * elements in a node.
+     */
+    UNWEIGHTED_AVERAGE,
+    /*
+     * Weighted average linkage takes the average distance between nodes in a cluster, weighing in the number of
+     * elements in a node.
+     */
+    WEIGHTED_AVERAGE,
+    /*
+     * Known as Ward's method, this type of linkage merges clusters based on a minimum variance measure.
+     */
+    MINIMUM_VARIANCE
+};
+
+struct sidb_cluster_hierarchy_node
+{
+    std::set<uint64_t>                                          c;
+    std::array<std::unique_ptr<sidb_cluster_hierarchy_node>, 2> sub;
+
+    sidb_cluster_hierarchy_node(const std::set<uint64_t>&                                   c_,
+                                std::array<std::unique_ptr<sidb_cluster_hierarchy_node>, 2> sub_) :
+            c{c_},
+            sub{std::move(sub_)}
+    {}
+};
+
+template <typename Lyt>
+sidb_cluster_hierarchy_node sidb_cluster_hierarchy(
+    Lyt&                                  lyt,
+    sidb_cluster_hierarchy_linkage_method linkage_method = sidb_cluster_hierarchy_linkage_method::MINIMUM_VARIANCE)
+{
+    if (lyt.num_cells() == 0)
+    {
+        return sidb_cluster_hierarchy_node{{}, {nullptr, nullptr}};
+    }
+
+    charge_distribution_surface<Lyt> charge_lyt{lyt};
+
+    alglib::real_2d_array d;
+    d.setlength(static_cast<alglib::ae_int_t>(charge_lyt.num_cells()), 2);
+    for (uint64_t i = 0; i < charge_lyt.num_cells(); ++i)
+    {
+        const auto [x, y] = charge_lyt.get_all_sidb_locations_in_nm()[i];
+
+        d[static_cast<int>(i)][0] = x;
+        d[static_cast<int>(i)][1] = y;
+    }
+
+    alglib::clusterizerstate s;
+    clusterizercreate(s);
+    clusterizersetpoints(s, d, 2);                               // Assign data and specify L2 norm (Euclidean metric)
+    clusterizersetahcalgo(s, static_cast<int>(linkage_method));  // Set the linkage method (default: Ward's method)
+    alglib::ahcreport rep;
+    clusterizerrunahc(s, rep);
+
+    std::map<uint64_t, std::unique_ptr<sidb_cluster_hierarchy_node>> nodes{};
+
+    // build hierarchy from N - 1 merges represented in rep.z
+    for (int i = 0; i < rep.npoints - 1; ++i)
+    {
+        const std::array<uint64_t, 2> cs = {static_cast<uint64_t>(rep.z[i][0]), static_cast<uint64_t>(rep.z[i][1])};
+        for (uint8_t c = 0; c < 2; ++c)
+        {
+            // create leaf nodes
+            if (cs[c] < charge_lyt.num_cells())
+            {
+                nodes[cs[c]] = std::make_unique<sidb_cluster_hierarchy_node>(
+                    std::set{cs[c]}, std::array<std::unique_ptr<sidb_cluster_hierarchy_node>, 2>{nullptr, nullptr});
+            }
+        }
+
+        // rep.z assigns each new cluster to N + i
+        const uint64_t new_n = charge_lyt.num_cells() + static_cast<uint64_t>(i);
+
+        std::set<uint64_t> set_union{};
+        std::set_union(nodes.at(cs[0])->c.cbegin(), nodes.at(cs[0])->c.cend(), nodes.at(cs[1])->c.cbegin(),
+                       nodes.at(cs[1])->c.cend(), std::inserter(set_union, set_union.begin()));
+
+        nodes[new_n] = std::make_unique<sidb_cluster_hierarchy_node>(
+            set_union, std::array<std::unique_ptr<sidb_cluster_hierarchy_node>, 2>{std::move(nodes.at(cs[0])),
+                                                                                   std::move(nodes.at(cs[1]))});
+        nodes.erase(cs[0]);
+        nodes.erase(cs[1]);
+    }
+
+    return std::move(*nodes.cbegin()->second);
+};
+
+}  // namespace fiction
+
+#endif  // FICTION_SIDB_CLUSTER_HIERARCHY_HPP
---
+++ test/technology/sidb_cluster_hierarchy.cpp
@@ -0,0 +1,111 @@
+//
+// Created by Willem Lambooy on 04/02/2024.
+//
+
+#include <catch2/catch_template_test_macros.hpp>
+
+#include <fiction/layouts/cartesian_layout.hpp>
+#include <fiction/layouts/cell_level_layout.hpp>
+#include <fiction/layouts/clocked_layout.hpp>
+#include <fiction/layouts/coordinates.hpp>
+#include <fiction/technology/cell_technologies.hpp>
+#include <fiction/technology/charge_distribution_surface.hpp>
+#include <fiction/technology/sidb_cluster_hierarchy.hpp>
+// #include <fiction/io/read_sqd_layout.hpp>
+
+#include <cstdint>
+#include <set>
+
+using namespace fiction;
+
+TEMPLATE_TEST_CASE(
+    "SiDB cluster hierarchy of a Y-shape SiDB OR gate with input 01", "[sidb-cluster-hierarchy]",
+    (cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>),
+    (charge_distribution_surface<cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>>))
+{
+    TestType lyt{};
+
+    lyt.assign_cell_type({6, 2, 0}, TestType::cell_type::NORMAL);
+    lyt.assign_cell_type({8, 3, 0}, TestType::cell_type::NORMAL);
+    lyt.assign_cell_type({12, 3, 0}, TestType::cell_type::NORMAL);
+
+    lyt.assign_cell_type({14, 2, 0}, TestType::cell_type::NORMAL);
+    lyt.assign_cell_type({10, 5, 0}, TestType::cell_type::NORMAL);
+
+    lyt.assign_cell_type({10, 6, 1}, TestType::cell_type::NORMAL);
+    lyt.assign_cell_type({10, 8, 1}, TestType::cell_type::NORMAL);
+    lyt.assign_cell_type({16, 1, 0}, TestType::cell_type::NORMAL);
+
+    // check for spooky non-determinism in alglib
+    for (int8_t i = 0; i < 100; ++i)
+    {
+        const sidb_cluster_hierarchy_node& h = sidb_cluster_hierarchy(lyt);
+
+        REQUIRE(h.c.size() == 8);
+        REQUIRE(h.sub[0] != nullptr);
+        REQUIRE(h.sub[1] != nullptr);
+        REQUIRE(h.sub[0]->c.size() == 3);
+        REQUIRE(h.sub[1]->c.size() == 5);
+        CHECK(h.sub[0]->c == std::set<uint64_t>{5, 6, 7});
+        CHECK(h.sub[1]->c == std::set<uint64_t>{0, 1, 2, 3, 4});
+        REQUIRE(h.sub[0]->sub[0] != nullptr);
+        REQUIRE(h.sub[0]->sub[1] != nullptr);
+        REQUIRE(h.sub[1]->sub[0] != nullptr);
+        REQUIRE(h.sub[1]->sub[1] != nullptr);
+        CHECK(h.sub[0]->sub[0]->c == std::set<uint64_t>{7});
+        CHECK(h.sub[0]->sub[1]->c == std::set<uint64_t>{5, 6});
+        CHECK(h.sub[1]->sub[0]->c == std::set<uint64_t>{1, 3});
+        CHECK(h.sub[1]->sub[1]->c == std::set<uint64_t>{0, 2, 4});
+        REQUIRE(h.sub[1]->sub[1]->sub[0] != nullptr);
+        REQUIRE(h.sub[1]->sub[1]->sub[1] != nullptr);
+        CHECK(h.sub[1]->sub[1]->sub[0]->c == std::set<uint64_t>{4});
+        CHECK(h.sub[1]->sub[1]->sub[1]->c == std::set<uint64_t>{0, 2});
+    }
+}
+
+TEMPLATE_TEST_CASE(
+    "SiDB cluster hierarchy of an 8 DB layout with separated groups of SiDBs", "[sidb-cluster-hierarchy]",
+    (cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>),
+    (charge_distribution_surface<cell_level_layout<sidb_technology, clocked_layout<cartesian_layout<siqad::coord_t>>>>))
+{
+    TestType lyt{};
+
+    lyt.assign_cell_type({50, -11, 1}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({54, -9, 0}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({10, -5, 1}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({12, -3, 1}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({-2, -2, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({2, -2, 1}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({53, 10, 1}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({48, 13, 1}, sidb_technology::cell_type::NORMAL);
+
+    const sidb_cluster_hierarchy_node& h = sidb_cluster_hierarchy(lyt);
+    REQUIRE(h.c.size() == 8);
+    REQUIRE(h.sub[0] != nullptr);
+    REQUIRE(h.sub[1] != nullptr);
+    REQUIRE(h.sub[0]->c.size() == 4);
+    REQUIRE(h.sub[1]->c.size() == 4);
+    CHECK(h.sub[0]->c == std::set<uint64_t>{2, 3, 4, 5});
+    CHECK(h.sub[1]->c == std::set<uint64_t>{0, 1, 6, 7});
+    REQUIRE(h.sub[0]->sub[0] != nullptr);
+    REQUIRE(h.sub[0]->sub[1] != nullptr);
+    REQUIRE(h.sub[1]->sub[0] != nullptr);
+    REQUIRE(h.sub[1]->sub[1] != nullptr);
+    CHECK(h.sub[0]->sub[0]->c == std::set<uint64_t>{4, 5});
+    CHECK(h.sub[0]->sub[1]->c == std::set<uint64_t>{2, 3});
+    CHECK(h.sub[1]->sub[0]->c == std::set<uint64_t>{0, 1});
+    CHECK(h.sub[1]->sub[1]->c == std::set<uint64_t>{6, 7});
+}
+
+// TEMPLATE_TEST_CASE("Influence region test", "[sidb-cluster-hierarchy]", (cell_level_layout<sidb_technology,
+// clocked_layout<cartesian_layout<siqad::coord_t>>>))
+//{
+//     TestType lyt = read_sqd_layout<TestType>("influence_region.sqd");
+//
+//     const sidb_cluster_hierarchy_node& h = sidb_cluster_hierarchy(lyt);
+//     REQUIRE(h.c.size() == 52);
+//
+// }
