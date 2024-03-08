df79bfb9bde59b5c7f2ef8ba4632b26d3e5a0192
Willem Lambooy <willem.lambooy@gmail.com>
2024-03-08 18:54:53 +0100

    :green_heart: apply the `FICTION_ALGLIB_ENABLED` switch in `time_to_solution.hpp`

--- include/fiction/algorithms/simulation/sidb/time_to_solution.hpp
+++ include/fiction/algorithms/simulation/sidb/time_to_solution.hpp
@@ -109,12 +109,14 @@ void time_to_solution(Lyt& lyt, const quicksim_params& quicksim_params, const ti
         st.algorithm      = "QuickExact";
         simulation_result = quickexact(lyt, params);
     }
+#if (FICTION_ALGLIB_ENABLED)
     else if (tts_params.engine == exhaustive_sidb_simulation_engine::CLUSTERCOMPLETE)
     {
         const clustercomplete_params params{quicksim_params.phys_params};
         st.algorithm      = "ClusterComplete";
         simulation_result = clustercomplete(lyt, params);
     }
+#endif  // FICTION_ALGLIB_ENABLED
     else
     {
         st.algorithm      = "ExGS";
