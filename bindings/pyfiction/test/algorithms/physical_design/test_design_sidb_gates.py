8b6417cfcb828179626bf462f667a20006212b10
Drewniok <jan.drewniok.research@gmail.com>
2024-03-25 08:08:27 +0100

    :art: use offset instead of siqad coordinates.

--- bindings/pyfiction/test/algorithms/physical_design/test_design_sidb_gates.py
+++ bindings/pyfiction/test/algorithms/physical_design/test_design_sidb_gates.py
@@ -4,30 +4,37 @@ from mnt.pyfiction import *
 class TestDesignSiDBGates(unittest.TestCase):
 
     def test_siqad_and_gate_skeleton(self):
-        layout = sidb_layout((2, 1))
-        layout.assign_cell_type((0, 0, 1), sidb_technology.cell_type.INPUT)
-        layout.assign_cell_type((2, 1, 1), sidb_technology.cell_type.INPUT)
-        layout.assign_cell_type((20, 0, 1), sidb_technology.cell_type.INPUT)
-        layout.assign_cell_type((18, 1, 1), sidb_technology.cell_type.INPUT)
+        layout = sidb_layout((20, 20))
 
-        layout.assign_cell_type((4, 2, 1), sidb_technology.cell_type.NORMAL)
-        layout.assign_cell_type((6, 3, 1), sidb_technology.cell_type.NORMAL)
+        layout.assign_cell_type((0, 1), sidb_technology.cell_type.INPUT)
+        layout.assign_cell_type((2, 3), sidb_technology.cell_type.INPUT)
 
-        layout.assign_cell_type((14, 3, 1), sidb_technology.cell_type.NORMAL)
-        layout.assign_cell_type((16, 2, 1), sidb_technology.cell_type.NORMAL)
+        layout.assign_cell_type((20, 1), sidb_technology.cell_type.INPUT)
+        layout.assign_cell_type((18, 3), sidb_technology.cell_type.INPUT)
 
-        layout.assign_cell_type((10, 6, 0), sidb_technology.cell_type.OUTPUT)
-        layout.assign_cell_type((10, 7, 0), sidb_technology.cell_type.OUTPUT)
+        layout.assign_cell_type((4, 5), sidb_technology.cell_type.NORMAL)
+        layout.assign_cell_type((6, 7), sidb_technology.cell_type.NORMAL)
 
-        layout.assign_cell_type((10, 9, 1), sidb_technology.cell_type.NORMAL)
+        layout.assign_cell_type((14, 7), sidb_technology.cell_type.NORMAL)
+        layout.assign_cell_type((16, 5), sidb_technology.cell_type.NORMAL)
+
+        layout.assign_cell_type((10, 12), sidb_technology.cell_type.OUTPUT)
+        layout.assign_cell_type((10, 14), sidb_technology.cell_type.OUTPUT)
+
+        layout.assign_cell_type((10, 19), sidb_technology.cell_type.NORMAL)
 
         params = design_sidb_gates_params()
         params.simulation_parameters.base = 2
         params.simulation_parameters.mu_minus = -0.28
         params.design_mode = design_sidb_gates_mode.EXHAUSTIVE
-        params.canvas = [(4, 4, 0), (14, 5, 1)]
+        params.canvas = [(4, 8), (14, 11)]
+        params.number_of_sidbs = 1
         params.sim_engine = sidb_simulation_engine.QUICKEXACT
+
         self.assertEqual(params.simulation_parameters.mu_minus, -0.28)
+        self.assertEqual(params.number_of_sidbs, 1)
+        self.assertEqual(params.canvas[0], (4, 8, 0))
+        self.assertEqual(params.canvas[1], (14, 11))
 
         designed_gates = design_sidb_gates(layout, [create_and_tt()], params)
 
