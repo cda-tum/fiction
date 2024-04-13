84e120e0be2f5db5258a99b2fa1a8ad8a96ab1f9
Willem Lambooy <willem.lambooy@gmail.com>
2024-04-13 23:29:07 +0200

    :alembic: Added CC benchmarks

--- test/benchmark/simulation.cpp
+++ test/benchmark/simulation.cpp
@@ -5,10 +5,15 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
+#include <fiction/algorithms/physical_design/apply_gate_library.hpp>
+#include <fiction/algorithms/simulation/sidb/clustercomplete.hpp>
 #include <fiction/algorithms/simulation/sidb/quickexact.hpp>
 #include <fiction/algorithms/simulation/sidb/quicksim.hpp>
+#include <fiction/layouts/gate_level_layout.hpp>
 #include <fiction/technology/cell_technologies.hpp>
+#include <fiction/technology/sidb_bestagon_library.hpp>
 #include <fiction/types.hpp>
+#include <fiction/utils/layout_utils.hpp>
 
 using namespace fiction;
 
@@ -70,6 +75,12 @@ TEST_CASE("Benchmark simulators", "[benchmark]")
         const quicksim_params quicksim_params{sidb_simulation_parameters{2, -0.32}};
         return quicksim<lattice>(lyt, quicksim_params);
     };
+
+    BENCHMARK("ClusterComplete")
+    {
+        const clustercomplete_params<lattice> sim_params{sidb_simulation_parameters{3, -0.32}};
+        return clustercomplete<lattice>(lyt, sim_params);
+    };
 }
 //  Mac M1, Ventura 13.0, Apple clang version 14.0.0 (22.11.23)
 
@@ -98,3 +109,57 @@ TEST_CASE("Benchmark simulators", "[benchmark]")
 //    QuickSim          100             1                   437.706 ms
 //                      4.32637 ms      4.29823 ms          4.358931 ms
 //                      154.258 us      132.162 us          194.857 us
+
+//  Intel Core i6700HQ (8 cores @ 2.60 GHz), Ubuntu 22.04.2 LTS, Ubuntu clang version 14.0.0 (13.04.24)
+
+//
+//    benchmark name          samples       iterations    est run time
+//                            mean          low mean      high mean
+//                            std dev       low std dev   high std dev
+//    -------------------------------------------------------------------------------
+//    QuickExact              100           1             3.95783 m
+//                            2.3555 s      2.33626 s     2.37767 s
+//                            106.153 ms    90.4958 ms    135.246 ms
+//
+//    QuickSim                100           1             1.49393 s
+//                            29.9329 ms    28.5082 ms    31.3671 ms
+//                            7.31298 ms    6.70416 ms    8.12608 ms
+//
+//    ClusterComplete         100           1             17.9353 s
+//                            151.302 ms     145.56 ms    157.719 ms
+//                            30.9984 ms    27.1551 ms    35.8004 ms
+
+TEST_CASE("Benchmark ClusterComplete", "[benchmark]")
+{
+    // number of non-terminating segments of a diagonal wire
+    const uint64_t i = 1;
+
+    hex_odd_row_gate_clk_lyt lyt{{(i + 1) / 2, i + 1}};
+
+    uint64_t signal = lyt.create_pi("a", {0, 0});
+
+    for (uint64_t j = 1; j < i + 1; j++)
+    {
+        signal = lyt.create_buf(signal, {j / 2, j});
+    }
+
+    lyt.create_po(signal, "o", {(i + 1) / 2, i + 1});
+
+    const lattice cl{convert_to_siqad_coordinates(
+        apply_gate_library<sidb_100_cell_clk_lyt, sidb_bestagon_library, hex_odd_row_gate_clk_lyt>(lyt))};
+
+    BENCHMARK("3 Segment Diagonal Bestagon Wire")
+    {
+        const clustercomplete_params<lattice> sim_params{sidb_simulation_parameters{3, -0.32}};
+        return clustercomplete<lattice>(cl, sim_params);
+    };
+}
+//  Intel Core i6700HQ (8 cores @ 2.60 GHz), Ubuntu 22.04.2 LTS, Ubuntu clang version 14.0.0 (13.04.24)
+
+//      benchmark name                          samples       iterations    est run time
+//                                              mean          low mean      high mean
+//                                              std dev       low std dev   high std dev
+//      -------------------------------------------------------------------------------
+//      3 Segment Diagonal Bestagon Wire        100           1             3.99949 m
+//                                              1.77163 s     1.75544 s     1.78914 s
+//                                              85.8704 ms    71.0768 ms    111.453 ms
