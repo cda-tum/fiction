0598e12424f754e0057c24018199c53ad62d2680
Willem Lambooy <willem.lambooy@gmail.com>
2024-04-21 18:31:26 +0200

    :fire: A part of the changes to CDS (Clang-Tidy seems troubled by the CDS diff)

--- include/fiction/technology/charge_distribution_surface.hpp
+++ include/fiction/technology/charge_distribution_surface.hpp
@@ -1522,6 +1522,15 @@ class charge_distribution_surface<Lyt, false> : public Lyt
                std::exp(-distance / defect.lambda_tf) * physical_constants::ELEMENTARY_CHARGE;
     }
     /**
+     * This function can be used to reset all external local electrostatic potentials to 0 Volt. All important
+     * attributes of the charge layout are updated automatically.
+     */
+    void reset_local_external_potentials() noexcept
+    {
+        strg->local_external_pot = {};
+        this->update_after_charge_change();
+    }
+    /**
      * This function can be used to assign an external local electrostatic potential in Volt to the layout, which is
      * added to previously stored values. All important attributes of the charge layout are updated automatically.
      *
