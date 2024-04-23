c3fe3f056d480bf5ce334a4faf3aea7d69639488
Willem Lambooy <willem.lambooy@gmail.com>
2024-04-23 10:26:44 +0200

    :snake: Removed `EXACT` vs `APPROXIMATE` from the bindings

--- bindings/pyfiction/test/algorithms/simulation/sidb/test_critical_temperature.py
+++ bindings/pyfiction/test/algorithms/simulation/sidb/test_critical_temperature.py
@@ -15,7 +15,7 @@ class TestCriticalTemperature(unittest.TestCase):
         layout.assign_cell_type((6, 1), sidb_technology.cell_type.NORMAL)
 
         params = critical_temperature_params()
-        params.engine = simulation_engine.EXACT
+        params.engine = sidb_simulation_engine.QUICKEXACT
 
         stats = critical_temperature_stats()
 
@@ -30,7 +30,7 @@ class TestCriticalTemperature(unittest.TestCase):
         layout = read_sqd_layout_100(dir_path + "/../../../resources/hex_21_inputsdbp_xor_v1.sqd", "xor_gate")
         params = critical_temperature_params()
         params.simulation_parameters.base = 2
-        params.engine = simulation_engine.EXACT
+        params.engine = sidb_simulation_engine.QUICKEXACT
 
         stats = critical_temperature_stats()
 
@@ -46,7 +46,7 @@ class TestCriticalTemperature(unittest.TestCase):
                                  "inverter_input_0")
 
         params = critical_temperature_params()
-        params.engine = simulation_engine.APPROXIMATE
+        params.engine = sidb_simulation_engine.QUICKSIM
 
         stats = critical_temperature_stats()
 
@@ -66,7 +66,7 @@ class TestCriticalTemperature(unittest.TestCase):
         params.simulation_parameters.base = 2
         params.simulation_parameters.mu_minus = -0.2
 
-        params.engine = simulation_engine.EXACT
+        params.engine = sidb_simulation_engine.QUICKEXACT
 
         stats = critical_temperature_stats()
 
