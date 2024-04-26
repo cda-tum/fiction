078665e1345b3dcfa9054a67909054f673c90ab9
Willem Lambooy <willem.lambooy@gmail.com>
2024-04-26 22:40:13 +0200

    :green_heart: one?

--- include/fiction/algorithms/simulation/sidb/exhaustive_ground_state_simulation.hpp
+++ include/fiction/algorithms/simulation/sidb/exhaustive_ground_state_simulation.hpp
@@ -49,7 +49,7 @@ exhaustive_ground_state_simulation(const Lyt&                        lyt,
 
         charge_distribution_surface<Lyt> charge_lyt{lyt};
 
-        charge_lyt.assign_physical_parameters(params);
+        charge_lyt.assign_simulation_parameters(params);
         charge_lyt.assign_all_charge_states(sidb_charge_state::NEGATIVE);
         charge_lyt.update_after_charge_change();
 
