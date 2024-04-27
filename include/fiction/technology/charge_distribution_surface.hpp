102d8d440d30dfcb7853aab14def7a598d6a2e26
Willem Lambooy <willem.lambooy@gmail.com>
2024-04-27 16:11:41 +0200

    :green_heart: the empty function takes 7 minutes

--- include/fiction/technology/charge_distribution_surface.hpp
+++ include/fiction/technology/charge_distribution_surface.hpp
@@ -1528,7 +1528,10 @@ class charge_distribution_surface<Lyt, false> : public Lyt
      * This function can be used to reset all external local electrostatic potentials to 0 Volt. All important
      * attributes of the charge layout are updated automatically.
      */
-    void reset_local_external_potentials() noexcept {}
+    void reset_local_external_potentials() noexcept
+    {
+        this->update_after_charge_change();
+    }
     /**
      * This function can be used to assign an external local electrostatic potential in Volt to the layout, which is
      * added to previously stored values. All important attributes of the charge layout are updated automatically.
