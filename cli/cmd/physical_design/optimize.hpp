a4c4838de706255ec22c3a2a0fd513804c347ca7
simon1hofmann <119581649+simon1hofmann@users.noreply.github.com>
2024-04-28 14:02:44 +0200

    ✨ Wiring reduction (#367)
    * place inputs first
    
    * place outputs last
    
    * wiring reduction algorithm inital version
    
    * add top to bottom wire search
    
    * clang format and removed unused includes
    
    * include wiring_reduction in post_layout_optimization
    
    * clang-format
    
    * clang-tidy suggestions
    
    * clang-format
    
    * override is_adjacent_of function
    
    * disable crossings as they are not needed
    
    * remove excess function
    
    * name testcases
    
    * disable failing testcase
    
    * fix dead node bug
    
    * add static asserts
    
    * update docs
    
    * update docstrings
    
    * increase coverage
    
    * clang-format
    
    * Update test/algorithms/physical_design/wiring_reduction.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fixed inconsistencies
    
    * fix bug and enable all testcases
    
    * delete testcase
    
    * Update test/algorithms/physical_design/post_layout_optimization.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * add more information to experiment
    
    * implement suggestions from code review
    
    * clang-format
    
    * Update include/fiction/algorithms/physical_design/wiring_reduction.hpp
    
    Co-authored-by: Marcel Walter <marcelwalter.mw@gmail.com>
    
    * align docs
    
    * :art: Small consistency changes
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcel Walter <marcelwalter.mw@gmail.com>
    
    * implement Marcel's comments
    
    * test get/set functions
    
    * small refactoring
    
    * fix rare bug
    
    * clang-format
    
    * changed order of statistics
    
    * implement todos
    
    * clang-format
    
    * adapt to new cp_tp api
    
    * use equivalence checking result directly
    
    * align with wiring reduction implementation
    
    * fix todos
    
    * clang-format
    
    * Update wiring_reduction.hpp
    
    * further improvements
    
    * clang-format
    
    * 🎨 Incorporated pre-commit fixes
    
    * apply wiring_reduction before every gate relocation step
    
    * 🎨 Incorporated pre-commit fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcel Walter <marcelwalter.mw@gmail.com>
    
    * 🎨 Incorporated pre-commit fixes
    
    * Marcel's feedback
    
    * 🎨 Incorporated pre-commit fixes
    
    * small fix
    
    * only check clocking scheme in main functions
    
    * another special case and some un-nesting
    
    * scalable post-layout optimization experiment
    
    * only reduce wiring when max_gate_relocations is set to 0
    
    * clang-tidy suggestions
    
    * 🎨 Incorporated pre-commit fixes
    
    * more corner cases covered
    
    * 🎨 Incorporated pre-commit fixes
    
    * expose optimization algorithms
    
    * fix exxlude pattern
    
    * update workflow in own PR
    
    * small fix
    
    * adjust experiment
    
    * final todos
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Marcel Walter <marcelwalter.mw@gmail.com>
    Co-authored-by: Marcel Walter <marcel.walter@tum.de>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- cli/cmd/physical_design/optimize.hpp
+++ cli/cmd/physical_design/optimize.hpp
@@ -6,13 +6,16 @@
 #define FICTION_CMD_OPTIMIZE_HPP
 
 #include <fiction/algorithms/physical_design/post_layout_optimization.hpp>
+#include <fiction/algorithms/physical_design/wiring_reduction.hpp>
 #include <fiction/layouts/clocked_layout.hpp>
 #include <fiction/layouts/gate_level_layout.hpp>
 #include <fiction/traits.hpp>
 #include <fiction/types.hpp>
+#include <fiction/utils/name_utils.hpp>
 
 #include <alice/alice.hpp>
 
+#include <cstdint>
 #include <iostream>
 #include <optional>
 #include <variant>
@@ -34,13 +37,30 @@ class optimize_command : public command
             command(e, "Optimizes a 2DDWave-clocked Cartesian layout with respect to area. It achieves this objective "
                        "by strategically repositioning gates within the layout, removing excess wiring, and "
                        "effectively relocating outputs to more favorable positions.")
-    {}
+    {
+        add_flag("--wiring_reduction_only,-w",
+                 "Do not attempt gate repositioning, but apply wiring reduction "
+                 "exclusively (recommended for logic functions with >200 gates due to scalability reasons).");
+        add_option("--max_gate_relocations,-m", max_gate_relocations,
+                   "Specify the maximum number of relocations to try for each gate (Defaults "
+                   "to the number of tiles in the layout).");
+        add_flag("--verbose,-v", "Be verbose");
+    }
 
   protected:
+    /**
+     * Maximum number of relocations to try for each gate.
+     */
+    uint64_t max_gate_relocations;
+    /**
+     * Parameters.
+     */
+    fiction::post_layout_optimization_params ps{};
     /**
      * Statistics.
      */
     fiction::post_layout_optimization_stats st{};
+    fiction::wiring_reduction_stats         stw{};
 
     /**
      * Optimizes a 2DDWave-clocked Cartesian layout.
@@ -52,48 +72,65 @@ class optimize_command : public command
         // error case: empty gate-level layout store
         if (gls.empty())
         {
-            env->out() << "[w] no gate layout in store" << std::endl;
+            env->out() << "[w] no gate layout in store\n";
+            ps = {};
             return;
         }
 
         const auto& lyt = gls.current();
 
-        const auto check_clocking_scheme = [](auto&& lyt_ptr)
+        const auto is_twoddwave_clocked = [](auto&& lyt_ptr) -> bool
         { return lyt_ptr->is_clocking_scheme(fiction::clock_name::TWODDWAVE); };
 
         // error case: layout is not 2DDWave-clocked
-        if (const auto is_twoddwave_clocked = std::visit(check_clocking_scheme, lyt); !is_twoddwave_clocked)
+        if (!std::visit(is_twoddwave_clocked, lyt))
         {
-            env->out() << "[e] layout has to be 2DDWave-clocked" << std::endl;
+            env->out() << "[e] layout has to be 2DDWave-clocked\n";
+            ps = {};
             return;
         }
 
         const auto apply_optimization = [&](auto&& lyt_ptr)
         {
-            using Lyt               = typename std::decay_t<decltype(lyt_ptr)>::element_type;
-            auto       lyt_copy     = lyt_ptr->clone();
-            const auto lyt_copy_ptr = std::make_shared<Lyt>(lyt_copy);
+            using Lyt = typename std::decay_t<decltype(lyt_ptr)>::element_type;
+
+            auto lyt_copy = lyt_ptr->clone();
 
             if constexpr (fiction::is_cartesian_layout_v<Lyt>)
             {
-                fiction::post_layout_optimization(*lyt_copy_ptr, &st);
-                fiction::restore_names(*lyt_ptr, *lyt_copy_ptr);
-                gls.extend() = lyt_copy_ptr;
+                if (is_set("wiring_reduction_only"))
+                {
+                    fiction::wiring_reduction(lyt_copy, &stw);
+                    if (is_set("verbose"))
+                    {
+                        stw.report(env->out());
+                    }
+                }
+                else
+                {
+                    fiction::post_layout_optimization(lyt_copy, ps, &st);
+                    if (is_set("verbose"))
+                    {
+                        st.report(env->out());
+                    }
+                }
+
+                fiction::restore_names(*lyt_ptr, lyt_copy);
+
+                gls.extend() = std::make_shared<Lyt>(lyt_copy);
             }
             else
             {
-                std::cout << "[e] layout has to be Cartesian" << std::endl;
+                std::cout << "[e] layout has to be Cartesian\n";
             }
         };
 
-        try
+        if (is_set("max_gate_relocations"))
         {
-            std::visit(apply_optimization, lyt);
+            ps.max_gate_relocations = max_gate_relocations;
         }
-        catch (...)
-        {
-            env->out() << "[e] an error occurred while optimizing" << std::endl;
-        }
+        std::visit(apply_optimization, lyt);
+        ps = {};
     }
 };
 
