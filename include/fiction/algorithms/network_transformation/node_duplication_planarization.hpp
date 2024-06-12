9f77edac450d04322d6c264dc65b6b0fc899342d
benjamin <benjamin.hien@web.de>
2024-06-12 17:21:22 +0200

    Compute Delay for H-graph.

--- include/fiction/algorithms/network_transformation/node_duplication_planarization.hpp
+++ include/fiction/algorithms/network_transformation/node_duplication_planarization.hpp
@@ -72,20 +72,132 @@ namespace detail
 {
 
 template <typename Ntk>
+struct node_pair
+{
+    std::pair<mockturtle::node<Ntk>, mockturtle::node<Ntk>> pair;
+    std::shared_ptr<node_pair<Ntk>> fanin_pair;
+    uint64_t delay;
+    // constructor
+    node_pair(mockturtle::node<Ntk> node1, mockturtle::node<Ntk> node2, uint64_t delayValue)
+            : pair(node1, node2), delay(delayValue) {}
+};
+
+template <typename Ntk>
+std::vector<node_pair<Ntk>> calculate_pairs(const std::vector<mockturtle::node<Ntk>>& nodes) {
+    std::vector<node_pair<Ntk>> pairwise_combinations;
+
+    if(nodes.size() == 1) {
+        node_pair<Ntk> pair = {nodes[0], nodes[0], std::numeric_limits<uint64_t>::max()}; // Initialize delay to inf
+        pairwise_combinations.push_back(pair);
+        return pairwise_combinations;
+    }
+
+    for (auto it1 = nodes.begin(); it1 != nodes.end(); ++it1) {
+        for (auto it2 = it1 + 1; it2 != nodes.end(); ++it2) {
+            node_pair<Ntk> pair1 = {*it1, *it2, std::numeric_limits<uint64_t>::max()}; // Initialize delay to inf
+            node_pair<Ntk> pair2 = {*it2, *it1, std::numeric_limits<uint64_t>::max()}; // Initialize delay to inf
+            pairwise_combinations.push_back(pair1);
+            pairwise_combinations.push_back(pair2);
+        }
+    }
+
+    return pairwise_combinations;
+}
+// ToDo: Hande combinations with more than two nodes. Then all the other nodes inbetween can be palced in arbitrary order
+template <typename Ntk>
 class node_duplication_planarization_impl
 {
   public:
     node_duplication_planarization_impl(const Ntk& src, const node_duplication_params p) :
             ntk(mockturtle::rank_view<Ntk>(src))
-    { }
+    {}
+
+    void compute_slice_delays( mockturtle::node<Ntk> po, std::vector<mockturtle::node<Ntk>> next_level )
+    {
+        ntk.foreach_fanin(po, [this, &next_level] (auto fi){
+                              cur_fis.push_back(fi);
+                              if(ntk.visited(fi) == ntk.trav_id()){
+                                  next_level.push_back(fi);
+                                  ntk.set_visited(fi, ntk.trav_id() + 1);
+                              }
+
+                          } );
+        // Compute the combinations in one slice
+        auto combinations = calculate_pairs<Ntk>(cur_fis);
+        if (combinations.empty())
+        {
+            throw std::runtime_error("Combinations are empty. There might be a dangling node");
+        }
+        std::vector<node_pair<Ntk>> combinations_last;
+        if (!cur_lvl_pairs.empty())
+        {
+            combinations_last = cur_lvl_pairs.back();
+            for (auto& node_pair_cur : combinations) {
+                for (auto& node_pair_last : combinations_last) {
+                    // If there is a connection between the two node pairs the delay is calculated like this
+                    if ((node_pair_cur.pair.first == node_pair_last.pair.second && node_pair_last.delay + 1 < node_pair_cur.delay))
+                    {
+                        node_pair_cur.fanin_pair = std::make_shared<node_pair<Ntk>>(node_pair_last);
+                        node_pair_cur.delay = node_pair_last.delay + 1;
+                    }
+                    // If there is no connection between the two node pairs the delay is calculated like this
+                    else if( node_pair_last.delay + 2 < node_pair_cur.delay )
+                    {
+                        node_pair_cur.fanin_pair = std::make_shared<node_pair<Ntk>>(node_pair_last);
+                        node_pair_cur.delay = node_pair_last.delay + 2;
+                    }
+                }
+            }
+        }
+        else
+        {
+            // The delay for the first node in the level is set to 1
+            for (auto& node_pair : combinations) {
+                node_pair.delay = 1;
+            }
+        }
+
+        for (const auto& node_pair : combinations) {
+            std::cout << "Fanin Pair: (" << node_pair.pair.first << ", " << node_pair.pair.second << "), Delay: " << node_pair.delay << std::endl;
+        }
+
+        cur_lvl_pairs.push_back(combinations);
+    }
 
     mockturtle::rank_view<Ntk> run()
     {
+        std::vector<mockturtle::node<Ntk>> next_level; // needed to start the next level
+        ntk.foreach_po([this, &next_level](auto po){
+                           std::cout << ntk.level(po) << std::endl;
+                           /*ntk.set_level(po, ntk.depth());
+                           std::cout << ntk.level(po) << std::endl;*/
+                           cur_fis.clear();
+                           compute_slice_delays(po, next_level);
+        });
+        while (!next_level.empty())
+        {
+            // We need to create a new vector to store the nodes of the next level
+            std::vector<mockturtle::node<Ntk>> new_level;
+            for (const auto& cur_node : next_level)
+            {
+                cur_fis.clear();
+                compute_slice_delays(cur_node, new_level);
+            }
+            // The new level becomes the next level for the next iteration
+            next_level = new_level;
+        }
         // Create random node ranks for POs: maybe here a heuristic or some clustering algorithm can assist
         // Iterate from POs to PIs and watch the current rank plus the fanin nodes at one time
         // A between these two ranks: compute the Graph H and compute the shortest path
         // B this might be simplified by just collecting all fanins also from the node with rank current_rank - 1
         // the order can then has to be applied before going to the next iteration step
+        /*static std::mt19937                     generator(std::random_device{}());
+        std::uniform_int_distribution<uint32_t> distribution(0, ntk.rank_width(r) - 1);*/
+        /*for(const auto node : cur_level)
+        {
+            assert( ntk.is_pi(node) );
+        }*/
+
         std::cout << "width: " <<ntk.width() << std::endl;
         std::cout << "depth: " <<ntk.depth() << std::endl;
         return ntk;
@@ -93,6 +205,9 @@ class node_duplication_planarization_impl
 
   private:
     mockturtle::rank_view<Ntk> ntk;
+    std::vector<std::vector<node_pair<Ntk>>> cur_lvl_pairs;
+    std::vector<mockturtle::node<Ntk>> cur_fis;
+    // std::vector<node_pair<Ntk>> slice_node_pairs;
 
 };
 
@@ -104,7 +219,7 @@ class node_duplication_planarization_impl
 template <typename NtkDest, typename NtkSrc>
 mockturtle::rank_view<NtkDest> node_duplication_planarization(const NtkSrc& ntk_src, node_duplication_params ps = {})
 {
-    /*static_assert(mockturtle::is_network_type_v<NtkSrc>, "NtkSrc is not a network type");
+    static_assert(mockturtle::is_network_type_v<NtkSrc>, "NtkSrc is not a network type");
     static_assert(mockturtle::is_network_type_v<NtkDest>, "NtkDest is not a network type");
 
     static_assert(mockturtle::has_is_constant_v<NtkDest>, "NtkSrc does not implement the is_constant function");
@@ -116,13 +231,19 @@ mockturtle::rank_view<NtkDest> node_duplication_planarization(const NtkSrc& ntk_
     static_assert(mockturtle::has_fanout_size_v<NtkDest>, "NtkDest does not implement the fanout_size function");
     static_assert(mockturtle::has_foreach_gate_v<NtkDest>, "NtkDest does not implement the foreach_gate function");
     static_assert(mockturtle::has_foreach_fanin_v<NtkDest>, "NtkDest does not implement the foreach_fanin function");
-    static_assert(mockturtle::has_foreach_po_v<NtkDest>, "NtkDest does not implement the foreach_po function");*/
+    static_assert(mockturtle::has_foreach_po_v<NtkDest>, "NtkDest does not implement the foreach_po function");
+
+    // check for input degree
+    /*if (has_high_degree_fanin_nodes(ntk_src, 2))
+    {
+        throw high_degree_fanin_exception();
+    }*/
 
     auto t_ntk = convert_network<NtkDest>(ntk_src);
 
-    if (!is_fanout_substituted(t_ntk)) {
+    /*if (!is_fanout_substituted(t_ntk)) {
         t_ntk =  fanout_substitution<NtkDest>(t_ntk);   // Apply fanout substitution if needed
-    }
+    }*/
 
     detail::node_duplication_planarization_impl<NtkDest> p{t_ntk, ps};
 
--- test/algorithms/network_transformation/node_duplication_planarization.cpp
+++ test/algorithms/network_transformation/node_duplication_planarization.cpp
@@ -14,9 +14,55 @@ using namespace fiction;
 
 TEST_CASE("First test", "[node-duplication-planarization]")
 {
-    auto maj = blueprints::maj1_network<mockturtle::aig_network>();
+    technology_network tec{};
 
-    const auto planarized_maj = node_duplication_planarization<technology_network>(maj);
+    CHECK(mockturtle::has_clear_visited_v<technology_network>);
+    CHECK(mockturtle::has_visited_v<technology_network>);
+    CHECK(mockturtle::has_set_visited_v<technology_network>);
+
+    const auto x1 = tec.create_pi();
+    const auto x2 = tec.create_pi();
+    const auto x3 = tec.create_pi();
+
+    const auto f1 = tec.create_and(x1, x2);
+    const auto f2 = tec.create_and(x1, x2);
+    const auto f3 = tec.create_and(x2, x3);
+    const auto f4 = tec.create_not(x3);
+    tec.create_po(f1);
+    tec.create_po(f2);
+    tec.create_po(f3);
+    tec.create_po(f4);
+
+    // auto maj = blueprints::maj1_network<mockturtle::aig_network>();
+
+    const auto planarized_maj = node_duplication_planarization<technology_network>(tec);
+
+    CHECK(1 == 1);
+}
+
+TEST_CASE("Multi Level test", "[node-duplication-planarization]")
+{
+    technology_network tec{};
+
+    CHECK(mockturtle::has_clear_visited_v<technology_network>);
+    CHECK(mockturtle::has_visited_v<technology_network>);
+    CHECK(mockturtle::has_set_visited_v<technology_network>);
+
+    const auto x1 = tec.create_pi();
+    const auto x2 = tec.create_pi();
+    const auto x3 = tec.create_pi();
+    const auto x4 = tec.create_pi();
+    const auto x5 = tec.create_pi();
+    const auto f1 = tec.create_not(x2);
+    const auto f2 = tec.create_nary_and({x1, x2, x3, x4});
+    const auto f3 = tec.create_nary_and({x3, x4, x5});
+    tec.create_po(f1);
+    tec.create_po(f2);
+    tec.create_po(f3);
+
+    // auto maj = blueprints::maj1_network<mockturtle::aig_network>();
+
+    const auto planarized_maj = node_duplication_planarization<technology_network>(tec);
 
     CHECK(1 == 1);
 }
