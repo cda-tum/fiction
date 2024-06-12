1fab81fdb635828bfac1b5ba3cffe6534ede9bdc
benjamin <benjamin.hien@web.de>
2024-06-12 10:59:45 +0200

    Rank Ntk Setup

--- include/fiction/algorithms/network_transformation/node_duplication_planarization.hpp
+++ include/fiction/algorithms/network_transformation/node_duplication_planarization.hpp
@@ -71,31 +71,28 @@ struct node_duplication_params
 namespace detail
 {
 
-template <typename NtkDest, typename NtkSrc>
+template <typename Ntk>
 class node_duplication_planarization_impl
 {
   public:
-    node_duplication_planarization_impl(const NtkSrc& src, const node_duplication_params p) :
-            ntk(convert_network<NtkDest>(src))
-    {
-        if (!is_fanout_substituted(ntk)) {
-            ntk = fanout_substitution<NtkDest>(ntk);   // Apply fanout substitution if needed
-        }
-    }
+    node_duplication_planarization_impl(const Ntk& src, const node_duplication_params p) :
+            ntk(mockturtle::rank_view<Ntk>(src))
+    { }
 
-    NtkDest run()
+    mockturtle::rank_view<Ntk> run()
     {
         // Create random node ranks for POs: maybe here a heuristic or some clustering algorithm can assist
         // Iterate from POs to PIs and watch the current rank plus the fanin nodes at one time
         // A between these two ranks: compute the Graph H and compute the shortest path
         // B this might be simplified by just collecting all fanins also from the node with rank current_rank - 1
         // the order can then has to be applied before going to the next iteration step
-        // std::cout << ntk.width() << std::endl;
+        std::cout << "width: " <<ntk.width() << std::endl;
+        std::cout << "depth: " <<ntk.depth() << std::endl;
         return ntk;
     }
 
   private:
-    NtkDest ntk;
+    mockturtle::rank_view<Ntk> ntk;
 
 };
 
@@ -105,7 +102,7 @@ class node_duplication_planarization_impl
  * ToDo: Description
  */
 template <typename NtkDest, typename NtkSrc>
-NtkDest node_duplication_planarization(const NtkSrc& ntk_src, node_duplication_params ps = {})
+mockturtle::rank_view<NtkDest> node_duplication_planarization(const NtkSrc& ntk_src, node_duplication_params ps = {})
 {
     /*static_assert(mockturtle::is_network_type_v<NtkSrc>, "NtkSrc is not a network type");
     static_assert(mockturtle::is_network_type_v<NtkDest>, "NtkDest is not a network type");
@@ -121,7 +118,13 @@ NtkDest node_duplication_planarization(const NtkSrc& ntk_src, node_duplication_p
     static_assert(mockturtle::has_foreach_fanin_v<NtkDest>, "NtkDest does not implement the foreach_fanin function");
     static_assert(mockturtle::has_foreach_po_v<NtkDest>, "NtkDest does not implement the foreach_po function");*/
 
-    detail::node_duplication_planarization_impl<NtkDest, NtkSrc> p{ntk_src, ps};
+    auto t_ntk = convert_network<NtkDest>(ntk_src);
+
+    if (!is_fanout_substituted(t_ntk)) {
+        t_ntk =  fanout_substitution<NtkDest>(t_ntk);   // Apply fanout substitution if needed
+    }
+
+    detail::node_duplication_planarization_impl<NtkDest> p{t_ntk, ps};
 
     auto result = p.run();
 
