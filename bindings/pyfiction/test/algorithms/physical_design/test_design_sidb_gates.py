4c2b3351fd294530a90f53e710ca76c98ae71822
Drewniok <jan.drewniok.research@gmail.com>
2024-06-20 09:38:47 +0200

    :green_heart: try to fix wheel test issue on windows.

--- bindings/pyfiction/test/algorithms/physical_design/test_design_sidb_gates.py
+++ bindings/pyfiction/test/algorithms/physical_design/test_design_sidb_gates.py
@@ -81,7 +81,7 @@ class TestDesignSiDBGates(unittest.TestCase):
 
         self.assertEqual(params.simulation_parameters.mu_minus, -0.32)
         self.assertEqual(params.number_of_sidbs, 3)
-        self.assertEqual(params.canvas[0], (10, 22, 0))
+        self.assertEqual(params.canvas[0], (10, 22))
         self.assertEqual(params.canvas[1], (14, 34))
 
         designed_gates = design_sidb_gates(layout, [create_nor_tt()], params)
