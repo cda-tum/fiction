443c1d612400d9901a311363f0a91fd7e0a7d1fe
Drewniok <jan.drewniok.research@gmail.com>
2024-07-17 13:31:20 +0200

    :art: delete DOC.

--- bindings/pyfiction/include/pyfiction/layouts/cell_level_layout.hpp
+++ bindings/pyfiction/include/pyfiction/layouts/cell_level_layout.hpp
@@ -70,7 +70,7 @@ void fcn_technology_cell_level_layout(pybind11::module& m)
     }
     else if constexpr (std::is_same_v<Technology, fiction::sidb_technology>)
     {
-        cell_type.value("LOGIC", Technology::cell_type::LOGIC, DOC(fiction_sidb_technology_cell_type_logic));
+        cell_type.value("LOGIC", Technology::cell_type::LOGIC);
     }
     // NOTE: more technologies go here
 
