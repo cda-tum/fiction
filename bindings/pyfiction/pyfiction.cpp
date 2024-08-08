aac6f12d2b29a66806d7b662f3c48d5271db0892
Jan Drewniok <97012901+Drewniok@users.noreply.github.com>
2024-08-08 15:23:03 +0200

    ✨ Displacement Robustness Domain Simulation (#416)
    * :art: print pis and pos of layout.
    
    * :art: add optional obstructed cell.
    
    * :construction: function to assess the displacement robustness.
    
    * :art: try to add a hash fucntion for the cell-level layout.
    
    * :art: use vector instead of a map.
    
    * :art: small changes.
    
    * :art: small change.
    
    * :art: small changes.
    
    * :art: still with bug.
    
    * :art: add comments.
    
    * :art: small change.
    
    * :sparkles: add ``determine_sidb_gate_robustness_domain_p_error_rate`` function.
    
    * :bug: Fixed conversion of cube coordinate with negative y-value to siqad coordinate.
    
    * :art: small fix.
    
    * :white_check_mark: add unit test.
    
    * :art: function for p=0.0 still needs to be fixed.
    
    * :thread: implement multithreading.
    
    * :art: add SiQAD AND gate to blueprints.
    
    * :art: add SiQAD AND gate to blueprints.
    
    * :memo: add docu.
    
    * :art: add exceptions.
    
    * :white_check_mark: update unit tests.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :memo: small fix.
    
    * :twisted_rightwards_arrows: merge.
    
    * :art: small fix.
    
    * :art: implement Marcel's comments.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: small fix in docu.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: small fix in docu.
    
    * :art: implement Marcel's comments (1. batch)
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: integrate Marcel's feedback.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :thread: update.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :snake: add python bindings
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :snake: add python bindings.
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: specify namespace to avoid build issues.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :art: add fiction namespace
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: integrate Marcel's feedback.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: use an underscore _ as a placeholder.
    
    ---------
    
    Signed-off-by: GitHub Actions <actions@github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: GitHub Actions <actions@github.com>

--- bindings/pyfiction/pyfiction.cpp
+++ bindings/pyfiction/pyfiction.cpp
@@ -38,6 +38,7 @@
 #include "pyfiction/algorithms/simulation/sidb/check_simulation_results_for_equivalence.hpp"
 #include "pyfiction/algorithms/simulation/sidb/convert_potential_to_distance.hpp"
 #include "pyfiction/algorithms/simulation/sidb/determine_groundstate_from_simulation_results.hpp"
+#include "pyfiction/algorithms/simulation/sidb/displacement_robustness_domain.hpp"
 #include "pyfiction/algorithms/simulation/sidb/minimum_energy.hpp"
 #include "pyfiction/algorithms/simulation/sidb/occupation_probability_of_excited_states.hpp"
 #include "pyfiction/algorithms/simulation/sidb/operational_domain.hpp"
@@ -140,6 +141,7 @@ PYBIND11_MODULE(pyfiction, m)
     pyfiction::determine_groundstate_from_simulation_results(m);
     pyfiction::check_simulation_results_for_equivalence(m);
     pyfiction::determine_physically_valid_parameters(m);
+    pyfiction::determine_displacement_robustness_domain(m);
     /**
      * Logic
      */
