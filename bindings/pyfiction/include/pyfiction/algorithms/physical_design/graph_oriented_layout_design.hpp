d999c8f30596633099c57a2e956268e8016d3fac
Simon Hofmann <simon.t.hofmann@tum.de>
2024-09-09 14:38:16 +0200

    add bindings

--- bindings/pyfiction/include/pyfiction/algorithms/physical_design/graph_oriented_layout_design.hpp
+++ bindings/pyfiction/include/pyfiction/algorithms/physical_design/graph_oriented_layout_design.hpp
@@ -26,6 +26,32 @@ inline void graph_oriented_layout_design(pybind11::module& m)
     namespace py = pybind11;
     using namespace pybind11::literals;
 
+    py::enum_<fiction::graph_oriented_layout_design_params::effort_mode>(
+        m, "effort_mode", DOC(fiction_graph_oriented_layout_design_params_effort_mode))
+        .value("LOW_EFFORT", fiction::graph_oriented_layout_design_params::effort_mode::LOW_EFFORT,
+               DOC(fiction_graph_oriented_layout_design_params_effort_mode_LOW_EFFORT))
+        .value("HIGH_EFFORT", fiction::graph_oriented_layout_design_params::effort_mode::HIGH_EFFORT,
+               DOC(fiction_graph_oriented_layout_design_params_effort_mode_HIGH_EFFORT))
+        .value("HIGHEST_EFFORT", fiction::graph_oriented_layout_design_params::effort_mode::HIGHEST_EFFORT,
+               DOC(fiction_graph_oriented_layout_design_params_effort_mode_HIGHEST_EFFORT))
+
+        ;
+
+    py::enum_<fiction::graph_oriented_layout_design_params::cost_objective>(
+        m, "cost_objective", DOC(fiction_graph_oriented_layout_design_params_cost_objective))
+        .value("AREA", fiction::graph_oriented_layout_design_params::cost_objective::AREA,
+               DOC(fiction_graph_oriented_layout_design_params_cost_objective_AREA))
+        .value("WIRES", fiction::graph_oriented_layout_design_params::cost_objective::WIRES,
+               DOC(fiction_graph_oriented_layout_design_params_cost_objective_WIRES))
+        .value("CROSSINGS", fiction::graph_oriented_layout_design_params::cost_objective::CROSSINGS,
+               DOC(fiction_graph_oriented_layout_design_params_cost_objective_CROSSINGS))
+        .value("ACP", fiction::graph_oriented_layout_design_params::cost_objective::ACP,
+               DOC(fiction_graph_oriented_layout_design_params_cost_objective_ACP))
+        .value("CUSTOM", fiction::graph_oriented_layout_design_params::cost_objective::CUSTOM,
+               DOC(fiction_graph_oriented_layout_design_params_cost_objective_CUSTOM))
+
+        ;
+
     py::class_<fiction::graph_oriented_layout_design_params>(m, "graph_oriented_layout_design_params",
                                                              DOC(fiction_graph_oriented_layout_design_params))
         .def(py::init<>())
@@ -35,8 +61,10 @@ inline void graph_oriented_layout_design(pybind11::module& m)
                        DOC(fiction_graph_oriented_layout_design_params_num_vertex_expansions))
         .def_readwrite("verbose", &fiction::graph_oriented_layout_design_params::verbose,
                        DOC(fiction_graph_oriented_layout_design_params_verbose))
-        .def_readwrite("high_effort_mode", &fiction::graph_oriented_layout_design_params::high_effort_mode,
-                       DOC(fiction_graph_oriented_layout_design_params_high_effort_mode))
+        .def_readwrite("mode", &fiction::graph_oriented_layout_design_params::mode,
+                       DOC(fiction_graph_oriented_layout_design_params_mode))
+        .def_readwrite("cost", &fiction::graph_oriented_layout_design_params::cost,
+                       DOC(fiction_graph_oriented_layout_design_params_cost))
         .def_readwrite("return_first", &fiction::graph_oriented_layout_design_params::return_first,
                        DOC(fiction_graph_oriented_layout_design_params_return_first))
         .def_readwrite("planar", &fiction::graph_oriented_layout_design_params::planar,
