e256c4c2fb848a226ae6122fc6d1fce6736c70fe
wlambooy <willem.lambooy@gmail.com>
2024-10-11 11:48:09 +0200

    :art: Some Clang-Tidy suggestions

--- test/algorithms/simulation/sidb/ground_state_space.cpp
+++ test/algorithms/simulation/sidb/ground_state_space.cpp
@@ -157,9 +157,10 @@ TEMPLATE_TEST_CASE("Ground State Space construction of a 7 DB layout", "[ground-
 
         for (uint64_t sidb_ix = 0; sidb_ix < 7; ++sidb_ix)
         {
-            CHECK_THAT(gss_res.top_cluster->charge_space.cbegin()
-                    ->compositions.cbegin()
-                    ->pot_bounds.get<bound_direction::LOWER>(sidb_ix) -
+            CHECK_THAT(
+                gss_res.top_cluster->charge_space.cbegin()
+                        ->compositions.cbegin()
+                        ->pot_bounds.get<bound_direction::LOWER>(sidb_ix) -
                 gss_res.top_cluster->charge_space.cbegin()
                         ->compositions.cbegin()
                         ->proj_states.cbegin()
@@ -177,11 +178,12 @@ TEMPLATE_TEST_CASE("Ground State Space construction of a 7 DB layout", "[ground-
                                 gss_res.top_cluster->charge_space.cbegin()->compositions.cbegin()->proj_states.cbegin(),
                                 1)
                                 ->multiset_conf)
-                        .pot_val, Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
+                        .pot_val,
+                Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
             CHECK_THAT(
                 gss_res.top_cluster->charge_space.cbegin()
-                    ->compositions.cbegin()
-                    ->pot_bounds.get<bound_direction::UPPER>(sidb_ix) -
+                        ->compositions.cbegin()
+                        ->pot_bounds.get<bound_direction::UPPER>(sidb_ix) -
                 gss_res.top_cluster->charge_space.cbegin()
                         ->compositions.cbegin()
                         ->proj_states.cbegin()
@@ -199,7 +201,8 @@ TEMPLATE_TEST_CASE("Ground State Space construction of a 7 DB layout", "[ground-
                                 gss_res.top_cluster->charge_space.cbegin()->compositions.cbegin()->proj_states.cbegin(),
                                 1)
                                 ->multiset_conf)
-                        .pot_val, Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
+                        .pot_val,
+                Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
         }
     }
     else
@@ -226,8 +229,8 @@ TEMPLATE_TEST_CASE("Ground State Space construction of a 7 DB layout", "[ground-
         {
             CHECK_THAT(
                 gss_res.top_cluster->charge_space.cbegin()
-                    ->compositions.cbegin()
-                    ->pot_bounds.get<bound_direction::LOWER>(sidb_ix) -
+                        ->compositions.cbegin()
+                        ->pot_bounds.get<bound_direction::LOWER>(sidb_ix) -
                 gss_res.top_cluster->charge_space.cbegin()
                         ->compositions.cbegin()
                         ->proj_states.cbegin()
@@ -245,10 +248,11 @@ TEMPLATE_TEST_CASE("Ground State Space construction of a 7 DB layout", "[ground-
                                 gss_res.top_cluster->charge_space.cbegin()->compositions.cbegin()->proj_states.cbegin(),
                                 1)
                                 ->multiset_conf)
-                        .pot_val, Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
+                        .pot_val,
+                Catch::Matchers::WithinAbs(0.0, std::numeric_limits<double>::epsilon()));
             CHECK_THAT(
                 gss_res.top_cluster->charge_space.cbegin()
-                    ->compositions.cbegin()
+                        ->compositions.cbegin()
                     ->pot_bounds.get<bound_direction::UPPER>(sidb_ix) -
                 gss_res.top_cluster->charge_space.cbegin()
                         ->compositions.cbegin()
