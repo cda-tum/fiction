7eeee971a7d167d4cb90860cb9f06c64a3f4b5a7
Drewniok <jan.drewniok.research@gmail.com>
2024-11-06 15:09:32 +0100

    :alembic: add clustercomplete to runtime experiment.

--- experiments/sidb_simulation/electrostatic_ground_state/runtime_analysis_bestagon_gates.cpp
+++ experiments/sidb_simulation/electrostatic_ground_state/runtime_analysis_bestagon_gates.cpp
@@ -5,6 +5,7 @@
 #include "fiction_experiments.hpp"
 
 #include <fiction/algorithms/iter/bdl_input_iterator.hpp>
+#include <fiction/algorithms/simulation/sidb/clustercomplete.hpp>
 #include <fiction/algorithms/simulation/sidb/exhaustive_ground_state_simulation.hpp>
 #include <fiction/algorithms/simulation/sidb/quickexact.hpp>
 #include <fiction/algorithms/simulation/sidb/quicksim.hpp>
@@ -29,12 +30,13 @@ using namespace fiction;
 
 int main()  // NOLINT
 {
-    experiments::experiment<std::string, std::size_t, double, double, double, double, double> simulation_exp{
+    experiments::experiment<std::string, std::size_t, double, double, double, double, double, double> simulation_exp{
         "Benchmark",
         "Gate Name",
         "#Instances",
         "Exhaustive Runtime [s]",
         "QuickExact Runtime [s]",
+        "QuickComplete Runtime [s]",
         "Average QuickSim Accuracy",
         "QuickSim Single Runtime [s]",
         "Total QuickSim TTS [s]"};
@@ -55,10 +57,11 @@ int main()  // NOLINT
         std::make_pair("inv", std::vector<tt>{create_not_tt()}),
         std::make_pair("wire", std::vector<tt>{create_id_tt()})};
 
-    const sidb_simulation_parameters        sim_params{2, -0.32};
-    const quicksim_params                   qs_params{sim_params};
-    const quickexact_params<siqad::coord_t> qe_params{sim_params};
-    const time_to_solution_params           tts_params{};
+    const sidb_simulation_parameters             sim_params{2, -0.32};
+    const quicksim_params                        qs_params{sim_params};
+    const quickexact_params<siqad::coord_t>      qe_params{sim_params};
+    const clustercomplete_params<siqad::coord_t> clustering_params{sim_params};
+    const time_to_solution_params                tts_params{};
 
     for (const auto& [gate, truth_table] : gates)
     {
@@ -68,6 +71,7 @@ int main()  // NOLINT
 
             double      runtime_exhaustive      = 0;
             double      runtime_quickexact      = 0;
+            double      runtime_clustercomplete = 0;
             double      tts_quicksim            = 0;
             std::size_t instances               = 0;
             double      quicksim_accuracy_mean  = 0.0;
@@ -77,10 +81,13 @@ int main()  // NOLINT
             const auto             exhaustive_results_layout = exhaustive_ground_state_simulation(layout, sim_params);
             time_to_solution_stats stats{};
             time_to_solution(layout, qs_params, tts_params, &stats);
-            const auto quickexact_results_layout = quickexact(layout, qe_params);
+            const auto quickexact_results_layout    = quickexact(layout, qe_params);
+            const auto quickcomplete_results_layout = clustercomplete(layout, clustering_params);
 
             runtime_exhaustive += mockturtle::to_seconds(exhaustive_results_layout.simulation_runtime);
             runtime_quickexact += mockturtle::to_seconds(quickexact_results_layout.simulation_runtime);
+            runtime_clustercomplete += mockturtle::to_seconds(quickcomplete_results_layout.simulation_runtime);
+
             tts_quicksim += stats.time_to_solution;
             instances += 1;
             quicksim_accuracy_mean += stats.acc;
@@ -95,10 +102,13 @@ int main()  // NOLINT
                 const auto             exhaustive_results = exhaustive_ground_state_simulation(*bii, sim_params);
                 time_to_solution_stats tts_stats{};
                 time_to_solution(*bii, qs_params, tts_params, &tts_stats);
-                const auto quickexact_results = quickexact(*bii, qe_params);
+                const auto quickexact_results    = quickexact(*bii, qe_params);
+                const auto quickcomplete_results = clustercomplete(*bii, clustering_params);
 
                 runtime_exhaustive += mockturtle::to_seconds(exhaustive_results.simulation_runtime);
                 runtime_quickexact += mockturtle::to_seconds(quickexact_results.simulation_runtime);
+                runtime_clustercomplete += mockturtle::to_seconds(quickcomplete_results.simulation_runtime);
+
                 tts_quicksim += stats.time_to_solution;
 
                 instances += 1;
@@ -106,8 +116,8 @@ int main()  // NOLINT
                 quicksim_single_runtime += stats.mean_single_runtime;
             }
             quicksim_accuracy_mean = quicksim_accuracy_mean / static_cast<double>(instances);
-            simulation_exp(gate, instances, runtime_exhaustive, runtime_quickexact, quicksim_accuracy_mean,
-                           quicksim_single_runtime, tts_quicksim);
+            simulation_exp(gate, instances, runtime_exhaustive, runtime_quickexact, runtime_clustercomplete,
+                           quicksim_accuracy_mean, quicksim_single_runtime, tts_quicksim);
             simulation_exp.save();
             simulation_exp.table();
         }
