b88408e0299e5f47c884d9e61f64dc709aeb427e
Drewniok <jan.drewniok.research@gmail.com>
2024-11-11 18:48:43 +0100

    :art: fix typo.

--- cli/stores.hpp
+++ cli/stores.hpp
@@ -465,7 +465,7 @@ void show<fiction::cell_layout_t>(std::ostream& os, const fiction::cell_layout_t
 
         if constexpr (!fiction::has_qca_technology_v<Lyt> && !fiction::has_sidb_technology_v<Lyt>)
         {
-            cmd.env->out() << fmt::format("[e] {} is either a QCA layout nor an SiDB layout", lyt_ptr->get_layout_name()) << std::endl;
+            cmd.env->out() << fmt::format("[e] {} is neither a QCA layout nor an SiDB layout", lyt_ptr->get_layout_name()) << std::endl;
         }
         else if constexpr (!std::is_same_v<fiction::coordinate<Lyt>, fiction::offset::ucoord_t>)
         {
