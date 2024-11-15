0cd91231c00e5610b70dc42ea084462a368bb6ce
Jan Drewniok <97012901+Drewniok@users.noreply.github.com>
2024-11-15 22:09:37 +0100

    ✨ Added function to print SiDB layout to svg  (#565)
    * :sparkles: first working version.
    
    * :sparkles: function to plot cell-level and cds to svg.
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: add fiction namespace tag.
    
    * :art: add fiction namespace tag.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :white_check_mark: update tests.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :memo: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * :art: remove ``const``
    
    * :snake: use normalize function to ensure equality
    
    * 🎨 Incorporated pre-commit fixes
    
    * :white_check_mark: test 111-lattice behavior.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: implement Marcel's feedback.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: small fix.
    
    * :art: small fix.
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: use lower case.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: change format.
    
    * :art: small fix.
    
    * :art: integrate Marcel's comments and add to store.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: fix typo.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :art: restructure.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :art: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :memo: small fix.
    
    * 🎨 Incorporated pre-commit fixes
    
    * :memo: Update pyfiction docstrings
    
    Signed-off-by: GitHub Actions <actions@github.com>
    
    * :memo: small fix.
    
    * :memo: small fix.
    
    ---------
    
    Signed-off-by: GitHub Actions <actions@github.com>
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Marcel Walter <marcel.walter@tum.de>

--- bindings/pyfiction/include/pyfiction/inout/write_svg_layout.hpp
+++ bindings/pyfiction/include/pyfiction/inout/write_svg_layout.hpp
@@ -17,25 +17,89 @@
 namespace pyfiction
 {
 
-inline void write_svg_layout(pybind11::module& m)
+namespace detail
+{
+template <typename Lyt>
+inline void write_sidb_layout_svg_impl(pybind11::module& m)
+{
+    using namespace pybind11::literals;
+
+    // Pointers to the original functions
+    void (*write_sidb_layout_svg_pointer)(const Lyt&, const std::string_view&,
+                                          const fiction::write_sidb_layout_svg_params&) =
+        &fiction::write_sidb_layout_svg<Lyt>;
+
+    // SiDB plot
+    m.def("write_sidb_layout_svg", write_sidb_layout_svg_pointer, "layout"_a, "filename"_a,
+          "ps"_a = fiction::write_sidb_layout_svg_params{}, DOC(fiction_write_sidb_layout_svg));
+
+    // Register the function to return an SVG as a string
+    m.def(
+        "write_sidb_layout_svg_to_string",
+        [](const Lyt& layout, const fiction::write_sidb_layout_svg_params& params) -> std::string
+        {
+            std::ostringstream oss;                               // Create an in-memory output stream
+            fiction::write_sidb_layout_svg(layout, oss, params);  // Write to the stream
+            return oss.str();                                     // Return the string content
+        },
+        "layout"_a, "ps"_a = fiction::write_sidb_layout_svg_params{}, DOC(fiction_write_sidb_layout_svg));
+}
+
+template <typename Lyt>
+inline void write_qca_layout_svg_impl(pybind11::module& m)
+{
+    using namespace pybind11::literals;
+
+    // QCA plot
+    void (*write_qca_layout_svg_pointer)(const py_qca_layout&, const std::string_view&,
+                                         const fiction::write_qca_layout_svg_params&) =
+        &fiction::write_qca_layout_svg<py_qca_layout>;
+
+    m.def("write_qca_layout_svg", write_qca_layout_svg_pointer, "layout"_a, "filename"_a,
+          "params"_a = fiction::write_qca_layout_svg_params{}, DOC(fiction_write_qca_layout_svg));
+}
+
+}  // namespace detail
+
+void write_svg_layout(pybind11::module& m)
 {
     namespace py = pybind11;
     using namespace pybind11::literals;
+
+    py::enum_<fiction::write_sidb_layout_svg_params::color_mode>(m, "color_mode",
+                                                                 DOC(fiction_write_sidb_layout_svg_params_color_mode))
+        .value("LIGHT", fiction::write_sidb_layout_svg_params::color_mode::LIGHT,
+               DOC(fiction_write_sidb_layout_svg_params_color_mode_LIGHT))
+        .value("DARK", fiction::write_sidb_layout_svg_params::color_mode::DARK,
+               DOC(fiction_write_sidb_layout_svg_params_color_mode_DARK));
+
+    py::class_<fiction::write_sidb_layout_svg_params>(m, "write_sidb_layout_svg_params",
+                                                      DOC(fiction_write_sidb_layout_svg_params))
+        .def(py::init<>())
+        .def_readwrite("lattice_point_size", &fiction::write_sidb_layout_svg_params::lattice_point_size,
+                       DOC(fiction_write_sidb_layout_svg_params_lattice_point_size))
+        .def_readwrite("sidb_size", &fiction::write_sidb_layout_svg_params::sidb_size,
+                       DOC(fiction_write_sidb_layout_svg_params_sidb_size))
+        .def_readwrite("sidb_border_width", &fiction::write_sidb_layout_svg_params::sidb_border_width,
+                       DOC(fiction_write_sidb_layout_svg_params_sidb_border_width))
+        .def_readwrite("color_background", &fiction::write_sidb_layout_svg_params::color_background,
+                       DOC(fiction_write_sidb_layout_svg_params_color_background));
 
     py::class_<fiction::write_qca_layout_svg_params>(m, "write_qca_layout_svg_params",
                                                      DOC(fiction_write_qca_layout_svg_params))
         .def(py::init<>())
         .def_readwrite("simple", &fiction::write_qca_layout_svg_params::simple,
-                       DOC(fiction_write_qca_layout_svg_params_simple))
+                       DOC(fiction_write_qca_layout_svg_params_simple));
+    ;
 
-        ;
+    detail::write_sidb_layout_svg_impl<py_charge_distribution_surface_111>(m);
+    detail::write_sidb_layout_svg_impl<py_charge_distribution_surface_100>(m);
+    detail::write_sidb_layout_svg_impl<py_charge_distribution_surface>(m);
 
-    void (*write_svg_layout_function_pointer)(const py_qca_layout&, const std::string_view&,
-                                              const fiction::write_qca_layout_svg_params&) =
-        &fiction::write_qca_layout_svg<py_qca_layout>;
-
-    m.def("write_qca_layout_svg", write_svg_layout_function_pointer, "layout"_a, "filename"_a,
-          "params"_a = fiction::write_qca_layout_svg_params{}, DOC(fiction_write_qca_layout_svg));
+    detail::write_sidb_layout_svg_impl<py_sidb_111_lattice>(m);
+    detail::write_sidb_layout_svg_impl<py_sidb_100_lattice>(m);
+    detail::write_sidb_layout_svg_impl<py_sidb_layout>(m);
+    detail::write_qca_layout_svg_impl<py_sidb_layout>(m);
 }
 
 }  // namespace pyfiction
