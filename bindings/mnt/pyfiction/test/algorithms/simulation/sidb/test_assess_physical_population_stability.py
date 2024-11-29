11eacbed7d613dadd1f7680550f927cf0aeb0617
Drewniok <jan.drewniok.research@gmail.com>
2024-11-29 11:46:29 +0100

    :snake: small fix.

--- bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_assess_physical_population_stability.py
+++ bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_assess_physical_population_stability.py
@@ -12,14 +12,13 @@ class TestAssessPhysicalPopulationStability(unittest.TestCase):
         layout.assign_cell_type((1, 1), sidb_technology.cell_type.NORMAL)
         params = assess_physical_population_stability_params()
         params.simulation_parameters.mu_minus = -0.25
-        cds = charge_distribution_surface_100(layout)
         result = assess_physical_population_stability_100(layout, params)
         self.assertEqual(len(result), 5)
         self.assertLessEqual(result[0].system_energy, result[1].system_energy)
         self.assertLessEqual(result[1].system_energy, result[2].system_energy)
 
         params.simulation_parameters.mu_minus = -0.32
-        result = assess_physical_population_stability_100(cds, params)
+        result = assess_physical_population_stability_100(layout, params)
         self.assertEqual(len(result), 1)
 
     def test_three_DBs_111_lattice(self):
@@ -29,14 +28,13 @@ class TestAssessPhysicalPopulationStability(unittest.TestCase):
         layout.assign_cell_type((1, 1), sidb_technology.cell_type.NORMAL)
         params = assess_physical_population_stability_params()
         params.simulation_parameters.mu_minus = -0.25
-        cds = charge_distribution_surface_111(layout)
         result = assess_physical_population_stability_111(layout, params)
         self.assertEqual(len(result), 5)
         self.assertLessEqual(result[0].system_energy, result[1].system_energy)
         self.assertLessEqual(result[1].system_energy, result[2].system_energy)
 
         params.simulation_parameters.mu_minus = -0.32
-        result = assess_physical_population_stability_111(cds, params)
+        result = assess_physical_population_stability_111(layout, params)
         self.assertEqual(len(result), 2)
 
 if __name__ == '__main__':
