55e1d81a4e6989eedd67d5062b65dd581530436e
Simon Hofmann <simon.t.hofmann@tum.de>
2024-12-04 21:00:14 +0100

    :snake: expose hexagonalization stats

--- bindings/mnt/pyfiction/include/pyfiction/algorithms/physical_design/hexagonalization.hpp
+++ bindings/mnt/pyfiction/include/pyfiction/algorithms/physical_design/hexagonalization.hpp
@@ -11,29 +11,31 @@
 #include <fiction/types.hpp>
 
 #include <pybind11/pybind11.h>
+#include <sstream>
 
 namespace pyfiction
 {
 
-namespace detail
-{
-
-template <typename Lyt>
-void hexagonalization(pybind11::module& m)
+inline void hexagonalization(pybind11::module& m)
 {
     namespace py = pybind11;
 
-    m.def(
-        "hexagonalization", [](const Lyt& lyt) -> py_hexagonal_gate_layout
-        { return fiction::hexagonalization<py_hexagonal_gate_layout, Lyt>(lyt); }, py::arg("layout"),
-        DOC(fiction_hexagonalization));
-}
+    py::class_<fiction::hexagonalization_stats>(m, "hexagonalization_stats",
+                                                DOC(fiction_hexagonalization_stats))
+            .def(py::init<>())
+            .def("__repr__",
+                 [](const fiction::hexagonalization_stats &stats) {
+                     std::stringstream stream{};
+                     stats.report(stream);
+                     return stream.str();
+                 })
+            .def_readonly("time_total", &fiction::hexagonalization_stats::time_total,
+                          DOC(fiction_hexagonalization_stats_duration));
 
-}  // namespace detail
+    m.def("hexagonalization",
+          &fiction::hexagonalization<py_hexagonal_gate_layout, py_cartesian_gate_layout>, py::arg("layout"),
+          py::arg("statistics") = nullptr, DOC(fiction_hexagonalization));
 
-inline void hexagonalization(pybind11::module& m)
-{
-    detail::hexagonalization<py_cartesian_gate_layout>(m);
 }
 
 }  // namespace pyfiction
