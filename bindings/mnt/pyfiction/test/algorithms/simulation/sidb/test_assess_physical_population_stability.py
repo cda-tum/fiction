54d7a6cec253ad37b8864a3df70dc8152ad2959e
Drewniok <jan.drewniok.research@gmail.com>
2024-12-09 10:48:12 +0100

    :snake: small fix.

--- bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_assess_physical_population_stability.py
+++ bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_assess_physical_population_stability.py
@@ -1,7 +1,8 @@
 import unittest
 
 from mnt.pyfiction import (
-    assess_physical_population_stability,
+    assess_physical_population_stability_100,
+    assess_physical_population_stability_111,
     assess_physical_population_stability_params,
     sidb_100_lattice,
     sidb_111_lattice,
@@ -17,13 +18,13 @@ class TestAssessPhysicalPopulationStability(unittest.TestCase):
         layout.assign_cell_type((1, 1), sidb_technology.cell_type.NORMAL)
         params = assess_physical_population_stability_params()
         params.simulation_parameters.mu_minus = -0.25
-        result = assess_physical_population_stability(layout, params)
+        result = assess_physical_population_stability_100(layout, params)
         self.assertEqual(len(result), 5)
         self.assertLessEqual(result[0].system_energy, result[1].system_energy)
         self.assertLessEqual(result[1].system_energy, result[2].system_energy)
 
         params.simulation_parameters.mu_minus = -0.32
-        result = assess_physical_population_stability(layout, params)
+        result = assess_physical_population_stability_100(layout, params)
         self.assertEqual(len(result), 1)
 
     def test_three_DBs_111_lattice(self):
@@ -33,13 +34,13 @@ class TestAssessPhysicalPopulationStability(unittest.TestCase):
         layout.assign_cell_type((1, 1), sidb_technology.cell_type.NORMAL)
         params = assess_physical_population_stability_params()
         params.simulation_parameters.mu_minus = -0.25
-        result = assess_physical_population_stability(layout, params)
+        result = assess_physical_population_stability_111(layout, params)
         self.assertEqual(len(result), 5)
         self.assertLessEqual(result[0].system_energy, result[1].system_energy)
         self.assertLessEqual(result[1].system_energy, result[2].system_energy)
 
         params.simulation_parameters.mu_minus = -0.32
-        result = assess_physical_population_stability(layout, params)
+        result = assess_physical_population_stability_111(layout, params)
         self.assertEqual(len(result), 2)
 
 
