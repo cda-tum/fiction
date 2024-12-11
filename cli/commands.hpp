fcbb8036ffa4e4f2e929f24e0bd6af9e61e87259
Marcel Walter <marcel.walter@tum.de>
2024-12-11 14:42:27 +0100

    :building_construction: Move `equiv` command to the `logic` CLI category

--- cli/commands.hpp
+++ cli/commands.hpp
@@ -33,6 +33,7 @@
 #include "cmd/logic/miginvprop.hpp"
 #include "cmd/logic/random.hpp"
 #include "cmd/logic/simulate.hpp"
+#include "cmd/verification/equiv.hpp"
 #endif
 
 // physical design and validation commands
@@ -47,7 +48,6 @@
 #include "cmd/technology/cell.hpp"
 #include "cmd/technology/energy.hpp"
 #include "cmd/verification/check.hpp"
-#include "cmd/verification/equiv.hpp"
 #endif
 
 // physical simulation commands
