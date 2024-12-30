8c9d1efbb5bd60f746596e90bb757f8266ca0efd
Drewniok <jan.drewniok.research@gmail.com>
2024-12-30 11:58:58 +0100

    :memo: small fix.

--- bindings/mnt/pyfiction/include/pyfiction/algorithms/simulation/sidb/operational_domain_ratio.hpp
+++ bindings/mnt/pyfiction/include/pyfiction/algorithms/simulation/sidb/operational_domain_ratio.hpp
@@ -27,7 +27,7 @@ void compute_operational_ratio(pybind11::module& m)
 
     m.def("operational_domain_ratio", &fiction::operational_domain_ratio<Lyt, py_tt>, py::arg("lyt"), py::arg("spec"),
           py::arg("pp"), py::arg("params") = fiction::operational_domain_ratio_params{},
-          DOC(fiction_compute_operational_ratio));
+          DOC(fiction_operational_domain_ratio));
 }
 
 }  // namespace detail
@@ -37,10 +37,10 @@ inline void compute_operational_ratio(pybind11::module& m)
     namespace py = pybind11;
 
     py::class_<fiction::operational_domain_ratio_params>(m, "operational_domain_ratio_params",
-                                                         DOC(fiction_compute_operational_ratio_params))
+                                                         DOC(fiction_operational_domain_ratio_params))
         .def(py::init<>())
         .def_readwrite("op_domain_params", &fiction::operational_domain_ratio_params::op_domain_params,
-                       DOC(fiction_compute_operational_ratio_params_op_domain_params));
+                       DOC(fiction_operational_domain_ratio_params_op_domain_params));
 
     // NOTE be careful with the order of the following calls! Python will resolve the first matching overload!
 
