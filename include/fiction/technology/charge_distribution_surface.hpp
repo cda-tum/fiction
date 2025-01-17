c6f316e6c0c482f46b90f02aa612db7a2d405f49
Marcel Walter <marcel.walter@tum.de>
2025-01-17 18:29:39 +0100

    :green_heart: Attempt to fix compiler error on ARM Linux

--- include/fiction/technology/charge_distribution_surface.hpp
+++ include/fiction/technology/charge_distribution_surface.hpp
@@ -174,7 +174,7 @@ class charge_distribution_surface<Lyt, false> : public Lyt
     {
       private:
         /**
-         * The distance matrix is a vector of vectors storing the euclidean distance in nm.
+         * The distance matrix is a vector of vectors storing the Euclidean distance in nm.
          */
         using distance_matrix = std::vector<std::vector<double>>;
         /**
@@ -383,7 +383,7 @@ class charge_distribution_surface<Lyt, false> : public Lyt
         for (const auto& c : strg->sidb_order)
         {
             auto pos = sidb_nm_position<Lyt>(Lyt{}, c);
-            positions.push_back(std::make_pair(pos.first, pos.second));
+            positions.emplace_back(pos.first, pos.second);
         }
 
         return positions;
