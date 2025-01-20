9a1031c7f8cba96188773eef9107d21d26f4a7cb
wlambooy <willem.lambooy@gmail.com>
2025-01-20 10:50:11 +0100

    :memo: Updated benchmarks (with jemalloc)

--- test/benchmark/simulation.cpp
+++ test/benchmark/simulation.cpp
@@ -130,7 +130,7 @@ TEST_CASE("Benchmark simulators", "[benchmark]")
 //                          2.00015 ms          1.97047 ms              2.05573 ms
 //                          199.837 us          129.863 us              357.338 us
 //
-//      After PR #390:
+//      After PR #390 (without jemalloc):
 //      benchmark name      samples             iterations              est run time
 //                          mean                low mean                high mean
 //                          std dev             low std dev             high std dev
@@ -146,6 +146,23 @@ TEST_CASE("Benchmark simulators", "[benchmark]")
 //      ClusterComplete     100                 1                       633.861 ms
 //                          20.5306 ms          16.53 ms                26.2279 ms
 //                          24.2954 ms          18.5671 ms              31.7567 ms
+//
+//      After PR #390 (with jemalloc):
+//      benchmark name      samples             iterations              est run time
+//                          mean                low mean                high mean
+//                          std dev             low std dev             high std dev
+//      -----------------------------------------------------------------------------
+//      QuickExact          100                 1                       1.71685 m
+//                          1.05373 s           1.05177 s               1.05608 s
+//                          10.8409 ms          9.05006 ms              13.7004 ms
+//
+//      QuickSim            100                 1                       269.791 ms
+//                          2.73239 ms          2.69164 ms              2.83162 ms
+//                          299.43 us           79.7179 us              533.137 us
+//
+//      ClusterComplete     100                 1                       1.25202 s
+//                          8.38247 ms          6.48053 ms              13.3237 ms
+//                          14.5352 ms          5.93141 ms              29.8427 ms
 
 #if (FICTION_ALGLIB_ENABLED)
 TEST_CASE("Benchmark ClusterComplete", "[benchmark]")
@@ -184,3 +201,12 @@ TEST_CASE("Benchmark ClusterComplete", "[benchmark]")
 //      4 Segment Diagonal Bestagon Wire    100                 1                       2.39773 m
 //                                          1.44927 s           1.44573 s               1.45397 s
 //                                          20.5985 ms          16.1537 ms              32.1081 ms
+//
+//      With jemalloc:
+//      benchmark name                      samples             iterations              est run time
+//                                          mean                low mean                high mean
+//                                          std dev             low std dev             high std dev
+//      ---------------------------------------------------------------------------------------------
+//      4 Segment Diagonal Bestagon Wire    100                 1                       1.39364 m
+//                                          835.42 ms           833.679 ms              837.076 ms
+//                                          8.68306 ms          7.61487 ms              10.1187 ms
