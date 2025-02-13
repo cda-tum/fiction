8a1e61b7653bb89873d18c1b15701d107260a0fc
Willem Lambooy <35612535+wlambooy@users.noreply.github.com>
2025-02-13 21:58:58 +0100

    :adhesive_bandage: Renaming artifact

--- experiments/quickcell/quickcell_rectangular_gate_library.cpp
+++ experiments/quickcell/quickcell_rectangular_gate_library.cpp
@@ -95,7 +95,7 @@ int main()  // NOLINT
     design_sidb_gates_params<sidb_100_cell_clk_lyt_siqad> params{
         is_operational_params{sidb_simulation_parameters{2, -0.32}, sidb_simulation_engine::QUICKEXACT,
                               bdl_input_iterator_params{{3}},
-                              is_operational_params::operational_condition::REJECT_KINKS},
+                              is_operational_params::operational_condition_kinks::REJECT_KINKS},
         design_sidb_gates_params<sidb_100_cell_clk_lyt_siqad>::design_sidb_gates_mode::QUICKCELL,
         {{18, 9, 0}, {26, 13, 0}},
         num_canvas_sidbs};
