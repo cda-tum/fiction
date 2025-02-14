d7c14bda09947e8dffe3454db506fcf9cedb612d
Willem Lambooy <35612535+wlambooy@users.noreply.github.com>
2025-02-14 01:16:13 +0100

    :adhesive_bandage: Renaming artifact

--- test/algorithms/simulation/sidb/operational_domain.cpp
+++ test/algorithms/simulation/sidb/operational_domain.cpp
@@ -1343,7 +1343,7 @@ TEMPLATE_TEST_CASE("AND gate with Bestagon shape and kink states at default phys
 
     SECTION("grid_search, reject kinks")
     {
-        op_domain_params.operational_params.op_condition = is_operational_params::operational_condition::REJECT_KINKS;
+        op_domain_params.operational_params.op_condition = is_operational_params::operational_condition_kinks::REJECT_KINKS;
 
         const auto op_domain = operational_domain_grid_search(layout, std::vector<tt>{create_and_tt()},
                                                               op_domain_params, &op_domain_stats);
@@ -1372,7 +1372,7 @@ TEMPLATE_TEST_CASE("Grid search to determine the operational domain. The operati
     op_domain_params.sweep_dimensions                         = {{sweep_parameter::EPSILON_R, 4.0, 6.0, 0.4},
                                                                  {sweep_parameter::LAMBDA_TF, 4.0, 6.0, 0.4}};
 
-    op_domain_params.operational_params.op_condition = is_operational_params::operational_condition::REJECT_KINKS;
+    op_domain_params.operational_params.op_condition = is_operational_params::operational_condition_kinks::REJECT_KINKS;
 
     operational_domain_stats op_domain_stats{};
 
