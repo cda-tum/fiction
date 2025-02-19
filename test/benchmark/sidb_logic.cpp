a2dd43ea09490e8bdabdc47e43ed188cca97db31
wlambooy <willem.lambooy@gmail.com>
2025-02-19 16:16:32 +0100

    :alembic: Added benchmark for `is_operational`

---
+++ test/benchmark/sidb_logic.cpp
@@ -0,0 +1,76 @@
+//
+// Created by Willem Lambooy on 19/02/2025.
+//
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include <fiction/algorithms/simulation/sidb/is_operational.hpp>
+#include <fiction/types.hpp>
+
+using namespace fiction;
+
+using lattice       = sidb_100_cell_clk_lyt;
+using lattice_siqad = sidb_100_cell_clk_lyt_siqad;
+
+TEST_CASE("Benchmark exact operational assessment", "[benchmark]")
+{
+    // crossing bestagon gate
+    lattice_siqad lyt{};
+
+    lyt.assign_cell_type({36, 1, 0}, sidb_technology::cell_type::INPUT);
+    lyt.assign_cell_type({2, 1, 0}, sidb_technology::cell_type::INPUT);
+
+    lyt.assign_cell_type({0, 0, 0}, sidb_technology::cell_type::INPUT);
+    lyt.assign_cell_type({38, 0, 0}, sidb_technology::cell_type::INPUT);
+
+    lyt.assign_cell_type({6, 2, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({20, 12, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({8, 3, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({14, 5, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({14, 11, 1}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({12, 4, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({14, 15, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({26, 4, 0}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({14, 9, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({24, 15, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({12, 16, 0}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({18, 9, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({26, 16, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({24, 13, 1}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({24, 5, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({30, 3, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({16, 13, 1}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({32, 2, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({20, 8, 0}, sidb_technology::cell_type::NORMAL);
+
+    lyt.assign_cell_type({30, 17, 0}, sidb_technology::cell_type::OUTPUT);
+    lyt.assign_cell_type({6, 18, 0}, sidb_technology::cell_type::OUTPUT);
+
+    lyt.assign_cell_type({32, 18, 0}, sidb_technology::cell_type::OUTPUT);
+    lyt.assign_cell_type({8, 17, 0}, sidb_technology::cell_type::OUTPUT);
+
+    lyt.assign_cell_type({2, 19, 0}, sidb_technology::cell_type::NORMAL);
+    lyt.assign_cell_type({36, 19, 0}, sidb_technology::cell_type::NORMAL);
+
+    BENCHMARK("QuickExact")
+    {
+        is_operational_params params{};
+        params.sim_engine = sidb_simulation_engine::QUICKEXACT;
+        return is_operational(lyt, create_crossing_wire_tt());
+    };
+
+#if (FICTION_ALGLIB_ENABLED)
+    BENCHMARK("ClusterComplete")
+    {
+        is_operational_params params{};
+        params.sim_engine = sidb_simulation_engine::CLUSTERCOMPLETE;
+        return is_operational(lyt, create_crossing_wire_tt());
+    };
+#endif  // FICTION_ALGLIB_ENABLED
+}
