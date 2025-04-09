b4935496fb234d1c3c22cc1feb07d7126bcf499d
Simon Hofmann <simon.t.hofmann@tum.de>
2025-04-09 13:25:13 +0200

    :snake: Fix errors after merge

--- bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_energy_distribution.py
+++ bindings/mnt/pyfiction/test/algorithms/simulation/sidb/test_energy_distribution.py
@@ -2,11 +2,9 @@ import unittest
 
 from mnt.pyfiction import (
     calculate_energy_distribution,
-    charge_distribution_surface_100,
-    charge_distribution_surface_111,
-    sidb_100_lattice,
-    sidb_111_lattice,
+    charge_distribution_surface,
     sidb_charge_state,
+    sidb_lattice,
     sidb_simulation_parameters,
     sidb_technology,
 )
@@ -14,20 +12,20 @@ from mnt.pyfiction import (
 
 class TestEnergyDistribution(unittest.TestCase):
     def test_three_DBs_100_lattice(self):
-        layout = sidb_100_lattice((10, 10))
+        layout = sidb_lattice((10, 10))
 
         layout.assign_cell_type((0, 1), sidb_technology.cell_type.NORMAL)
         layout.assign_cell_type((4, 1), sidb_technology.cell_type.NORMAL)
         layout.assign_cell_type((6, 1), sidb_technology.cell_type.NORMAL)
 
-        cds1 = charge_distribution_surface_100(layout)  # all negative
-        cds2 = charge_distribution_surface_100(
+        cds1 = charge_distribution_surface(layout)  # all negative
+        cds2 = charge_distribution_surface(
             layout, sidb_simulation_parameters(), sidb_charge_state.NEUTRAL
         )  # all neutral
-        cds3 = charge_distribution_surface_100(layout)
+        cds3 = charge_distribution_surface(layout)
         cds3.assign_charge_state((6, 1), sidb_charge_state.NEUTRAL)  # only two SiDBs are negative
 
-        cds4 = charge_distribution_surface_100(layout)
+        cds4 = charge_distribution_surface(layout)
         cds4.assign_charge_state((0, 1), sidb_charge_state.NEUTRAL)
         cds4.assign_charge_state((4, 1), sidb_charge_state.POSITIVE)
         cds4.assign_charge_state((6, 1), sidb_charge_state.NEGATIVE)
@@ -47,20 +45,20 @@ class TestEnergyDistribution(unittest.TestCase):
         self.assertLessEqual(distribution.get_nth_state(1).degeneracy, 3)
 
     def test_three_DBs_111_lattice(self):
-        layout = sidb_111_lattice((10, 10))
+        layout = sidb_lattice((10, 10), orientation="111")
 
         layout.assign_cell_type((0, 1), sidb_technology.cell_type.NORMAL)
         layout.assign_cell_type((4, 1), sidb_technology.cell_type.NORMAL)
         layout.assign_cell_type((6, 1), sidb_technology.cell_type.NORMAL)
 
-        cds1 = charge_distribution_surface_111(layout)  # all negative
-        cds2 = charge_distribution_surface_111(
+        cds1 = charge_distribution_surface(layout)  # all negative
+        cds2 = charge_distribution_surface(
             layout, sidb_simulation_parameters(), sidb_charge_state.NEUTRAL
         )  # all neutral
-        cds3 = charge_distribution_surface_111(layout)
+        cds3 = charge_distribution_surface(layout)
         cds3.assign_charge_state((6, 1), sidb_charge_state.NEUTRAL)  # only two SiDBs are negative
 
-        cds4 = charge_distribution_surface_111(layout)
+        cds4 = charge_distribution_surface(layout)
         cds4.assign_charge_state((0, 1), sidb_charge_state.NEUTRAL)
         cds4.assign_charge_state((4, 1), sidb_charge_state.POSITIVE)
         cds4.assign_charge_state((6, 1), sidb_charge_state.NEGATIVE)
