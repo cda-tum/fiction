7bdd2ec319f9b69b6fd0edb44ef36c4dc94fa402
wlambooy <willem.lambooy@gmail.com>
2025-04-10 14:20:33 +0200

    :memo: Added docstrings

--- include/fiction/algorithms/simulation/sidb/compare_by_ground_state_isolation.hpp
+++ include/fiction/algorithms/simulation/sidb/compare_by_ground_state_isolation.hpp
@@ -23,6 +23,15 @@ namespace fiction
 namespace detail
 {
 
+/**
+ * Obtains the energetic ground state isolation; i.e., the energetic difference between the ground state and the first
+ * excited state.
+ *
+ * @tparam Lyt SiDB cell-level layout type.
+ * @param sim_res Simulation results as a vector of physically valid charge distributions, sorted by energy such that
+ * the first charge distribution corresponds is the ground state.
+ * @return The energetic difference between the ground state and the first excited state.
+ */
 template <typename Lyt>
 [[nodiscard]] double get_ground_state_isolation(const std::vector<charge_distribution_surface<Lyt>>& sim_res) noexcept
 {
@@ -43,23 +52,47 @@ template <typename Lyt>
 
 }  // namespace detail
 
+/**
+ * This comparator for designed SiDB gates implements comparison by minimum ground state isolation over all input
+ * combinations.
+ *
+ * @tparam Lyt SiDB cell-level layout type.
+ */
 template <typename Lyt>
 class compare_by_minimum_ground_state_isolation final : public designed_sidb_gate_comparator<Lyt>
 {
   public:
     using typename designed_sidb_gate_comparator<Lyt>::sidb_gate_design;
-
+    /**
+     * Standard constructor.
+     *
+     * @param sens The sensitivity parameter determines when an energetic difference it big enough to judge the
+     * left-hand right-hand side as not equal.
+     */
     explicit compare_by_minimum_ground_state_isolation(
         const double sens = std::numeric_limits<double>::epsilon()) noexcept :
             designed_sidb_gate_comparator<Lyt>{sens}
     {}
-
+    /**
+     * Implements strict comparison by minimum ground state isolation.
+     *
+     * @param lhs Left-hand side.
+     * @param rhs Right-hand side.
+     * @return True if and only if the left-hand side is strictly below the right-hand side.
+     */
     [[nodiscard]] bool operator()(const sidb_gate_design& lhs, const sidb_gate_design& rhs) const noexcept override
     {
         return minimum_ground_state_isolation_over_all_inputs(lhs) <
                minimum_ground_state_isolation_over_all_inputs(rhs);
     }
-
+    /**
+     * Implements equality comparison for minimum ground state isolation.
+     *
+     * @param lhs Left-hand side.
+     * @param rhs Right-hand side.
+     * @return True if and only if the left-hand side is equal to the right-hand side as determined by the sensitivity
+     * parameter.
+     */
     [[nodiscard]] bool equals(const sidb_gate_design& lhs, const sidb_gate_design& rhs) const noexcept override
     {
         return std::abs(minimum_ground_state_isolation_over_all_inputs(lhs) -
@@ -67,6 +100,13 @@ class compare_by_minimum_ground_state_isolation final : public designed_sidb_gat
     }
 
   private:
+    /**
+     * Obtains the minimum ground state isolation over all input for the given SiDB gate design.
+     *
+     * @param gate_design The SiDB gate design object with simulation results for each input to obtain the minimum
+     * ground state isolation over all inputs of.
+     * @return The minimum ground state isolation over all inputs.
+     */
     [[nodiscard]] double
     minimum_ground_state_isolation_over_all_inputs(const sidb_gate_design& gate_design) const noexcept
     {
@@ -80,24 +120,47 @@ class compare_by_minimum_ground_state_isolation final : public designed_sidb_gat
         return *std::min_element(ground_state_isolations.cbegin(), ground_state_isolations.cend());
     }
 };
-
+/**
+ * This comparator for designed SiDB gates implements comparison by average ground state isolation over all input
+ * combinations.
+ *
+ * @tparam Lyt SiDB cell-level layout type.
+ */
 template <typename Lyt>
 class compare_by_average_ground_state_isolation final : public designed_sidb_gate_comparator<Lyt>
 {
   public:
     using typename designed_sidb_gate_comparator<Lyt>::sidb_gate_design;
-
+    /**
+     * Standard constructor.
+     *
+     * @param sens The sensitivity parameter determines when an energetic difference it big enough to judge the
+     * left-hand right-hand side as not equal.
+     */
     explicit compare_by_average_ground_state_isolation(
         const double sens = std::numeric_limits<double>::epsilon()) noexcept :
             designed_sidb_gate_comparator<Lyt>{sens}
     {}
-
+    /**
+     * Implements strict comparison by average ground state isolation.
+     *
+     * @param lhs Left-hand side.
+     * @param rhs Right-hand side.
+     * @return True if and only if the left-hand side is strictly below the right-hand side.
+     */
     [[nodiscard]] bool operator()(const sidb_gate_design& lhs, const sidb_gate_design& rhs) const noexcept override
     {
         return average_ground_state_isolation_over_all_inputs(lhs) <
                average_ground_state_isolation_over_all_inputs(rhs);
     }
-
+    /**
+     * Implements equality comparison for average ground state isolation.
+     *
+     * @param lhs Left-hand side.
+     * @param rhs Right-hand side.
+     * @return True if and only if the left-hand side is equal to the right-hand side as determined by the sensitivity
+     * parameter.
+     */
     [[nodiscard]] bool equals(const sidb_gate_design& lhs, const sidb_gate_design& rhs) const noexcept override
     {
         return std::abs(average_ground_state_isolation_over_all_inputs(lhs) -
@@ -105,6 +168,13 @@ class compare_by_average_ground_state_isolation final : public designed_sidb_gat
     }
 
   private:
+    /**
+     * Obtains the average ground state isolation over all input for the given SiDB gate design.
+     *
+     * @param gate_design The SiDB gate design object with simulation results for each input to obtain the average
+     * ground state isolation over all inputs of.
+     * @return The average ground state isolation over all inputs.
+     */
     [[nodiscard]] double
     average_ground_state_isolation_over_all_inputs(const sidb_gate_design& gate_design) const noexcept
     {
@@ -131,109 +201,3 @@ class compare_by_average_ground_state_isolation final : public designed_sidb_gat
 }  // namespace fiction
 
 #endif  // COMPARE_BY_GROUND_STATE_ISOLATION
-
-//
-// /**
-//      * Performs a sorting operation on the designed gate layouts, putting those in front for which the energetic gap
-//      * between the ground state and the first excited state is larger. For each designed gate layout, the minimum
-//      * energetic gap is taken over each input. When the minima are equal for two designed gate layouts, the average
-//      * energetic gap over each input is taken as a tiebreaker.
-//      *
-//      * @param designed_gate_layouts A vector of designed gate layouts to sort in place.
-//      * @param sim_results_per_input_for_each_gate_design The simulation results for each input of each designed gate
-//      * layout.
-//      */
-//     void sort_designed_gate_layouts_by_ground_state_isolation(
-//         std::vector<Lyt>& designed_gate_layouts,
-//         std::vector<std::vector<std::vector<charge_distribution_surface<Lyt>>>>
-//                                                      sim_results_per_input_for_each_gate_design) const noexcept
-//     {
-//         // pair the two input vectors
-//         std::vector<std::pair<Lyt, std::vector<std::vector<charge_distribution_surface<Lyt>>>>> pairs{};
-//         pairs.reserve(designed_gate_layouts.size());
-//
-//         for (uint64_t i = 0; i < designed_gate_layouts.size(); ++i)
-//         {
-//             pairs.emplace_back(std::move(designed_gate_layouts.at(i)),
-//                                std::move(sim_results_per_input_for_each_gate_design.at(i)));
-//         }
-//
-//         // clear the designed_gate_layouts vector so that we may reenter the elements in order later
-//         designed_gate_layouts.clear();
-//
-//         // sort all individual simulation results by system energy
-//         for (auto& pair : pairs)
-//         {
-//             for (std::vector<charge_distribution_surface<Lyt>>& sim_res : pair.second)
-//             {
-//                 std::sort(sim_res.begin(), sim_res.end(), [](const auto& cds1, const auto& cds2) noexcept
-//                           { return cds1.get_electrostatic_potential_energy() <
-//                           cds2.get_electrostatic_potential_energy(); });
-//             }
-//         }
-//
-//         const auto get_ground_state_isolation =
-//             [&](const std::vector<charge_distribution_surface<Lyt>>& sim_res) noexcept
-//         {
-//             return sim_res.size() == 1 ? std::numeric_limits<double>::infinity() :
-//                                          sim_res.at(1).get_electrostatic_potential_energy() -
-//                                          sim_res.at(0).get_electrostatic_potential_energy();
-//         };
-//
-//         const auto minimum_ground_state_isolation_for_all_inputs =
-//             [&get_ground_state_isolation](
-//                 const std::vector<std::vector<charge_distribution_surface<Lyt>>>& res_per_input) noexcept
-//         {
-//             std::vector<double> ground_state_isolations{};
-//             std::transform(res_per_input.cbegin(), res_per_input.cend(), std::back_inserter(ground_state_isolations),
-//                            get_ground_state_isolation);
-//             return *std::min_element(ground_state_isolations.cbegin(), ground_state_isolations.cend());
-//         };
-//
-//         const auto average_ground_state_isolation_for_all_inputs =
-//             [&get_ground_state_isolation](
-//                 const std::vector<std::vector<charge_distribution_surface<Lyt>>>& res_per_input) noexcept
-//         {
-//             uint64_t count = 0;
-//
-//             double accumulated_ground_state_isolation = 0.0;
-//
-//             for (const auto& sim_res : res_per_input)
-//             {
-//                 if (sim_res.size() == 1)
-//                 {
-//                     continue;
-//                 }
-//
-//                 accumulated_ground_state_isolation += get_ground_state_isolation(sim_res);
-//
-//                 ++count;
-//             }
-//
-//             return accumulated_ground_state_isolation / static_cast<double>(count);
-//         };
-//
-//         // sort the pairs by minimum ground state isolation for each input
-//         std::sort(pairs.begin(), pairs.end(),
-//                   [&minimum_ground_state_isolation_for_all_inputs,
-//                    &average_ground_state_isolation_for_all_inputs](const auto& lhs, const auto& rhs) noexcept
-//                   {
-//                       const double diff = minimum_ground_state_isolation_for_all_inputs(lhs.second) -
-//                                           minimum_ground_state_isolation_for_all_inputs(rhs.second);
-//
-//                       // when minima are equal, take the average
-//                       if (std::abs(diff) < std::numeric_limits<double>::epsilon())
-//                       {
-//                           return average_ground_state_isolation_for_all_inputs(lhs.second) >
-//                                  average_ground_state_isolation_for_all_inputs(rhs.second);
-//                       }
-//
-//                       return diff > 0.0;
-//                   });
-//
-//         // put the designed gate layouts back in the sorted order
-//         for (auto& pair : pairs)
-//         {
-//             designed_gate_layouts.emplace_back(pair.first);
-//         }
-//     }
