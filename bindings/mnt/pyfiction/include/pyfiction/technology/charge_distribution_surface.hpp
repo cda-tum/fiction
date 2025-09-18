9c8b0189e20ae7d6554f897d7ba0e559ce8c4807
wlambooy <willem.lambooy@gmail.com>
2025-09-18 15:07:01 +0200

    :snake: trigger wheels build

--- bindings/mnt/pyfiction/include/pyfiction/technology/charge_distribution_surface.hpp
+++ bindings/mnt/pyfiction/include/pyfiction/technology/charge_distribution_surface.hpp
@@ -29,7 +29,7 @@ namespace detail
 {
 
 template <typename Lyt>
-void charge_distribution_surface_layout(pybind11::module& m, const std::string& lattice = "")
+void charge_distribution_surface_layout(pybind11::module& m, const std::string& lattice = "TRIGGER")
 {
     namespace py = pybind11;
 
