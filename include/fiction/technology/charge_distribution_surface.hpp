4b0f6639d10b1cf1f8dc4ca72a7df93c82ff1c45
Marcel Walter <marcel.walter@tum.de>
2026-01-27 14:03:40 +0100

    :green_heart: Fix build errors

--- include/fiction/technology/charge_distribution_surface.hpp
+++ include/fiction/technology/charge_distribution_surface.hpp
@@ -390,17 +390,7 @@ class charge_distribution_surface<Lyt, false> : public Lyt
      *
      * @param cds Other `charge_distribution_surface`.
      */
-    charge_distribution_surface(const charge_distribution_surface& cds) :
-            Lyt(cds),
-            strg{std::make_shared<charge_distribution_storage>(*cds.strg)}
-    {}
-    /**
-     * Conversion constructor from the generic charge_distribution_surface.
-     *
-     * @param cds Other `charge_distribution_surface`.
-     */
-    explicit charge_distribution_surface(
-        const charge_distribution_surface<Lyt>& cds) :  // NOLINT(google-explicit-constructor)
+    charge_distribution_surface(const charge_distribution_surface<Lyt>& cds) :
             Lyt(cds),
             strg{std::make_shared<charge_distribution_storage>(*cds.strg)}
     {}
